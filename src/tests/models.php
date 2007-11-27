21f5658afa96824ab09b7397dae7b60c2402ff2a
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2007-11-27 23:07:00 +0000

    testing fez etd

--- src/tests/models.php
+++ src/tests/models.php
@@ -7,6 +7,7 @@ class ModelGroupTest extends GroupTest {
     $this->GroupTest('Model tests');
     $this->addTestFile('models/etdTest.php');
     $this->addTestFile('models/etd_htmlTest.php');
+    $this->addTestFile('models/FezEtdTest.php');
   }
 }
 
