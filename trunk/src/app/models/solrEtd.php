60f3742c043b977150a8cd54880d53ce33ed2196
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2007-12-07 23:32:23 +0000

    mock-etd object initialized from solr for display (needs refining-- interface?)

---
+++ trunk/src/app/models/solrEtd.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once("abstractEtd.php");
+
+class solrEtd implements abstractEtd {
+
+  public function __construct(array $values) {
+
+    // map lucene fields to etd fields - should be named consistently
+    foreach ($values as $key =>  $value) {
+      $this->$key = $values[$key];
+    }
+    // note that facet fields will be ignored here
+  }
+
+  public function pid() { return $this->PID; }
+  public function title() { return $this->title; }
+  public function author() { return $this->author; }
+  public function program() { return $this->program; }
+  public function advisor() { return $this->advisor; }
+  public function committee() { return $this->committee; }
+			  // how to handle non-emory committee?
+  	// dissertation/thesis/etc
+  public function document_type() { return $this->document_type; }
+  public function language() { return $this->language; }
+  public function year() { return $this->year; }
+  public function _abstract() { return $this->abstract; }
+  public function tableOfContents() { return $this->tableOfContents; }
+  public function num_pages() { return $this->num_pages; }
+  
+  public function keywords() { return  $this->keyword; }	//array
+  public function researchfields() { return $this->subject; } 	//array
+
+}
+
+?>
