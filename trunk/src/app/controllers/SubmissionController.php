c4ed9b14aa60e0477d8d9b46c54b9cc9ede090ae
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2007-12-13 15:58:31 +0000

    beginning of submission workflow actions

---
+++ trunk/src/app/controllers/SubmissionController.php
@@ -0,0 +1,42 @@
+<?php
+/** Zend_Controller_Action */
+
+/* Require models */
+require_once("models/etd.php");
+require_once("helpers/ProcessPDF.php");
+
+
+class SubmissionController extends Zend_Controller_Action {
+
+  public function indexAction() {
+    $this->_forward("start");
+    // maybe this should be a summary/status page to check on the submission...
+  }
+   
+  public function startAction() {
+    // any other info needed here?
+    $this->view->title = "Begin Submission";
+  }
+
+  // fixme: better action name? (preliminary?)
+  public function processPDFAction() {
+    Zend_Controller_Action_HelperBroker::addPrefix('Etd_Controller_Action_Helper');
+    //     Zend_Controller_Action_HelperBroker::addPath('Controllers/Helpers', 'Helper');
+    
+    print "<pre>";
+    print_r($_FILES);
+    print "</pre>";
+    
+    //     $pdf = Zend_Controller_Action_HelperBroker::getStaticHelper('processPDF');
+    $this->view->messages = array();
+    
+    $this->view->etd_info = $this->_helper->processPDF($_FILES['pdf']);
+
+    $this->view->messages = $this->_helper->flashMessenger->getCurrentMessages();
+    //$this->_helper->flashMessenger->clearMessages();
+  }
+
+  //  other actions:
+  // review, submit, addfile (upload/enter info)
+   
+}
