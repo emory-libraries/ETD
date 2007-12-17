07baaf9eda8597dc4dbb6019171c6b7cbc5b859f
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2007-12-17 23:12:57 +0000

    purge etd, attempt to update file (not working?)

--- trunk/src/app/models/etdfile.php
+++ trunk/src/app/models/etdfile.php
@@ -3,14 +3,36 @@
 require_once("XmlObject.class.php");
 require_once("models/foxml.php");
 require_once("api/fedora.php");
+require_once("etd_rels.php");
 
 class etd_file extends foxml {
 
-  public function __construct($pid = null) {
+  public $parent;
+  protected $type;
+  
+  public function __construct($pid = null, etd $parent = null) {
     parent::__construct($pid);
 
     if (is_null($pid)) 
       $this->cmodel = "etdFile";
+
+
+    // determine what type of etd file this is
+    if (isset($this->rels_ext->pdfOf)) {
+      $this->type = "pdf";
+    } elseif (isset($this->rels_ext->originalOf)) {
+      $this->type = "original";
+    } elseif (isset($this->rels_ext->supplementOf)) {
+      $this->type = "supplement";
+    }
+
+    // based on file type, get parent pid and initialize parent object (if not set and if not a new object)
+    if (is_null($parent) && !is_null($pid)) {
+      $parent_rel = $this->type . "Of";
+      $this->parent = new etd($this->rels_ext->$parent_rel);
+    } else {
+      $this->parent = $parent;
+    }
   }
 
 
@@ -23,6 +45,28 @@ class etd_file extends foxml {
     // add mappings for xmlobject
     $this->xmlconfig["file"] = array("xpath" => "foxml:datastream[@ID='FILE']",
 				     "class_name" => "file_for_ingest");
+
+    // just override one portion of the rels configuration
+    $this->xmlconfig["rels_ext"]["class_name"] = "etd_rels";
+  }
+
+
+  // remove from parent etd record, THEN purge from fedora
+  public function purge($message) {
+    $rel = "rel:has" . ucfirst($this->type);	// FIXME: won't work for pdf...
+    // maybe add removePdf, removeSupplement, etc. functions for etd_rels ?
+
+    // remove relation stored in parent etd record, save parent etd
+    $this->parent->rels_ext->removeRelation($rel, $this->pid);
+    $this->parent->save("removed relation $rel to " . $this->pid);
+
+    // run default foxml purge
+    return parent::purge($message);
+  }
+
+  public function updateFile($filename, $mimetype, $message) {
+    $upload_id = fedora::upload($filename);
+    return fedora::modifyBinaryDatastream($this->pid, "FILE", "Binary File", $mimetype, $upload_id, $message);
   }
 
 }  
