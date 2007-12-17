0c637c639a1da00df0880cec9ef7a20e37b9f67c
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2007-12-17 23:07:41 +0000

    split out file logic into separate controller

--- src/app/controllers/EditController.php
+++ src/app/controllers/EditController.php
@@ -3,22 +3,30 @@
 
 /* Require models */
 require_once("models/etd.php");
+require_once("helpers/FileUpload.php");
 
 class EditController extends Zend_Controller_Action {
 
+
+  public function postDispatch() {
+    $this->view->messages = $this->_helper->flashMessenger->getCurrentMessages();
+
+    $env = Zend_Registry::get('env-config');
+    $this->view->site_mode = $env->mode;	// better name for this? (test/dev/prod)
+  }
+  
   // edit main record metadata
   // better name?
   public function modsAction() {
-     $pid = $this->_getParam("pid");
-     $etd = new etd($pid);
-
-     $env = Zend_Registry::get('env-config');
-     $this->view->site_mode = $env->mode;	// better name for this? (test/dev/prod)
-
-     $this->view->title = "Edit Record Information";
+    $pid = $this->_getParam("pid");
+    $etd = new etd($pid);
+    
+    $this->view->title = "Edit Record Information";
     $this->view->etd = $etd;
     // xforms setting - so layout can include needed code in the header
     $this->view->xforms = true;
+    $this->view->xforms_bind_script = "edit/_mods_bind.phtml";
+    $this->view->namespaces = array("mods" => "http://www.loc.gov/mods/v3");
     //    $this->view->xforms_model_xml = $etd->mods->saveXML();
     // link to xml rather than embedding directly in the page
     $this->view->xforms_model_uri = $this->view->url(array("controller" => "view",
@@ -43,31 +51,82 @@ class EditController extends Zend_Controller_Action {
 
   // edit formatted fields
   public function htmlAction() {
-     $pid = $this->_getParam("pid");
-     $mode = $this->_getParam("mode", "title");
-     $etd = new etd($pid);
+    $pid = $this->_getParam("pid");
+    $mode = $this->_getParam("mode", "title");
+    $etd = new etd($pid);
 
-     $this->view->mode = $mode;
-     $this->view->etd_title = $etd->mods->title;
-     $this->view->edit_content = $etd->html->{$mode};
-     $this->view->title = "edit $mode";
-   }
+    $this->view->mode = $mode;
+    $this->view->etd_title = $etd->mods->title;
+    $this->view->edit_content = $etd->html->{$mode};
+    $this->view->title = "edit $mode";
+  }
 
 
-  // fixme: not actually saving anything yet...  
+  public function saveTitleAction() {
+    $this->_setParam("mode", "title");
+    $this->_forward("saveHtml");
+  }
+  
+  public function saveAbstractAction() {
+    $this->_setParam("mode", "abstract");
+    $this->_forward("saveHtml");
+  }
+
+  public function saveContentsAction() {
+    $this->_setParam("mode", "contents");
+    $this->_forward("saveHtml");
+  }
+
+  
+  // save formatted fields
+  public function saveHtmlAction() {
+    $pid = $this->_getParam("pid");
+    $mode = $this->_getParam("mode");
+    $content = $this->_getParam("edit_content");
+
+    // FIXME: FCKeditor is adding divs around the edges, sometimes paragraphs...
+    // this clean-up should probably be in etd_html class, not here
+    $content = preg_replace("|^\s*<div>\s*(.*)\s*</div>\s*$|", "$1", $content);
+    if ($mode == "title") {
+      $content = preg_replace("|^\s*<p>\s*(.*)\s*</p>\s*$|", "$1", $content);
+    }
+    
+    $etd = new etd($pid);
+
+    // title/abstract/contents - special fields that set values in html, mods and dublin core
+    $etd->$mode = $content;
+
+    $this->view->save_result = $etd->save("modified $mode");
+    $this->_helper->flashMessenger->addMessage("Saved changes to $mode");
+
+    $this->view->pid = $pid;
+    $this->view->mode = $mode;
+    $this->view->title = "save $mode";
+  }
+
+
    public function savemodsAction() {
+    $pid = $this->_getParam("pid");
+    $etd = new etd($pid);
+    
      global $HTTP_RAW_POST_DATA;
      $xml = $HTTP_RAW_POST_DATA;
     if ($xml == "") {
       // if no xml is submitted, don't modify 
       // forward to a different view?
       $this->view->noxml = true;
+    } else {
+      $etd->mods->updateXML($xml);
+      $this->view->save_result = $etd->save("edited record information");
+      $this->_helper->flashMessenger->addMessage("Saved changes");
     }
-    $this->view->xml = $xml;
 
-    $this->view->title = "edit mods";
-    
+
+    $this->view->pid = $pid;
+    $this->view->xml = $xml;
+    $this->view->title = "save mods";
    }
+
 
 
 
