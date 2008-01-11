05dd9ddfacb40db1015272310e5c5ac55ba7eaf9
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2008-01-11 15:40:24 +0000

    unit tests for etd_mods, specifically keyword/research field functions

--- trunk/src/tests/models.php
+++ trunk/src/tests/models.php
@@ -4,9 +4,10 @@ require_once("bootstrap.php");
 
 class ModelGroupTest extends GroupTest {
   function ModelGroupTest() {
-    $this->GroupTest('Model tests');
+    $this->GroupTest('ETD Model tests');
     $this->addTestFile('models/etdTest.php');
     $this->addTestFile('models/etd_htmlTest.php');
+    $this->addTestFile('models/etdModsTest.php');
     $this->addTestFile('models/FezEtdTest.php');
   }
 }
