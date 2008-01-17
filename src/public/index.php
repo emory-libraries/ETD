34328ae0394d5e69a8f998e8937469b901434211
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2008-01-17 21:59:14 +0000

    find current user if logged in [#98]; also preliminary acl, solr

--- src/public/index.php
+++ src/public/index.php
@@ -17,6 +17,7 @@ ini_set("include_path", ini_get("include_path") . ":../app/::../app/models:../ap
 require("Zend/Loader.php");
 Zend_Loader::registerAutoload();
 
+
 //Load Configuration
 $env_config = new Zend_Config_Xml("../config/environment.xml", "environment");
 Zend_Registry::set('env-config', $env_config);
@@ -26,6 +27,45 @@ Zend_Registry::set('fedora-config',
 
 //set default timezone
 date_default_timezone_set($config->timezone);
+
+
+// set up connection to solr for search & browse
+require_once("solr.php");
+$solr_config = new Zend_Config_Xml("../config/solr.xml", $env_config->mode);
+$solr = new solr($solr_config->server, $solr_config->port);
+$solr->addFacets(explode(',', $solr_config->facets)); 	// array of default facet terms
+Zend_Registry::set('solr', $solr);
+
+
+// set up access controls
+$acl = new Zend_Acl();
+
+//roles
+$guest = new Zend_Acl_Role('guest');
+$acl->addRole($guest);
+$author = new Zend_Acl_Role('author');
+$acl->addRole($author);
+$committee = new Zend_Acl_Role('committee');
+$acl->addRole($committee);
+$admin = new Zend_Acl_Role('admin');
+$acl->addRole($admin);
+
+//resources
+$acl->add(new Zend_Acl_Resource("etd"));
+$acl->add(new Zend_Acl_Resource("published etd"), "etd");
+$acl->add(new Zend_Acl_Resource("draft etd"), "etd");
+
+//privileges
+$acl->allow("guest", "published etd", "view metadata");
+
+$acl->allow("author", "etd", array("view metadata", "view history", "view status",
+				   "download", "edit metadata", "add file"));
+
+$acl->allow("committee", "etd", array("view metadata", "view history", "view status", "download"));
+$acl->allow("admin", "etd", array("view metadata", "view history", "view status", "download",
+				  "edit history", "edit status"));
+
+Zend_Registry::set('acl', $acl);
 
 //Setup Controller
 $front = Zend_Controller_Front::getInstance();
@@ -43,6 +83,18 @@ Xend_Layout::setDefaultLayoutName('site');
 $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
 $viewRenderer->initView();
 $viewRenderer->view->addHelperPath('Emory/View/Helper', 'Emory_View_Helper');
+
+
+$auth = Zend_Auth::getInstance();
+if ($auth->hasIdentity()) {
+  $viewRenderer->view->current_user = user::find_by_username($auth->getIdentity());
+}
+
+
+
+Zend_Controller_Action_HelperBroker::addPath('Emory/Controller/Action/Helper',
+					     'Emory_Controller_Action_Helper');
+
 
 //set internal error handler
 $front->throwExceptions((boolean)$env_config->display_exception);
