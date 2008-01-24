765c3b1635fd39af13227e6354703d8d26a8677c
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2008-01-24 19:26:20 +0000

    display static pages; imported content from fez etds [#36]

---
+++ src/app/controllers/DocsController.php
@@ -0,0 +1,12 @@
+<?php
+
+class DocsController extends Zend_Controller_Action {
+
+  public function aboutAction() {}
+  public function faqAction() {}
+  public function ipAction() {}
+  public function policiesAction() {}
+  public function boundcopiesAction() {}
+     
+
+}
