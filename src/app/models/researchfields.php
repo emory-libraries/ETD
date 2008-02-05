0a19449f21eaa6a46f638c963cddf257bb6840d3
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2008-02-05 15:53:50 +0000

    fine-tuning collection model, added functionality for totals - ticket:19

--- src/app/models/researchfields.php
+++ src/app/models/researchfields.php
@@ -5,5 +5,7 @@ require_once("skosCollection.php");
 class researchfields extends collectionHierarchy  {
   public function __construct($dom, $id = "#researchfields") {
     parent::__construct($dom, $id);
+
+    $this->index_field = "subject_facet";
   }
 }
