145afca12ab0104d7f7fc9a12f86617193ede104
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2008-02-06 22:56:55 +0000

    check if field is set before returning it - ticket:122

--- src/app/models/solrEtd.php
+++ src/app/models/solrEtd.php
@@ -16,26 +16,35 @@ class solrEtd implements etdInterface {
 
   // fixme: handle fields that could be empty?
 
-  public function pid() { return $this->PID; }
-  public function status() { return $this->status; }
-  public function title() { return $this->title; }
-  public function author() { return $this->author; }
-  public function program() { return $this->program; }
-  public function subfield() { return isset($this->subfield) ? $this->subfield : ""; }
-  public function advisor() { return $this->advisor; }
-  public function committee() { return $this->committee; }
+  public function pid() { return $this->getField("PID"); }
+  public function status() { return $this->getField("status"); }
+  public function title() { return $this->getField("title"); }
+  public function author() { return $this->getField("author"); }
+  public function program() { return $this->getField("program"); }
+  public function subfield() { return $this->getField("subfield"); }
+  public function advisor() { return $this->getField("advisor"); }
+  public function committee() { return $this->getField("committee", true); }
 			  // how to handle non-emory committee?
   	// dissertation/thesis/etc
-  public function document_type() { return $this->document_type; }
-  public function language() { return $this->language; }
-  public function year() { return $this->year; }
-  public function _abstract() { return $this->abstract; }
-  public function tableOfContents() { return $this->tableOfContents; }
-  public function num_pages() { return $this->num_pages; }
+  public function document_type() { return $this->getField("document_type"); }
+  public function language() { return $this->getField("language"); }
+  public function year() { return $this->getField("year"); }
+  public function _abstract() { return $this->getField("abstract"); }
+  public function tableOfContents() { return $this->getField("tableOfContents"); }
+  public function num_pages() { return $this->getField("num_pages"); }
   
-  public function keywords() { return  $this->keyword; }	//array
-  public function researchfields() { return $this->subject; } 	//array
+  public function keywords() { return $this->getField("keyword", true); }	//array
+  public function researchfields() { return $this->getField("subject", true); } 	//array
 
+
+  private function getField($name, $array = false) {
+    if (isset($this->$name))
+      return $this->$name;
+    else if ($array)
+      return array();
+    else
+      return "";
+  }
 
   // for Zend ACL Resource
   public function getResourceId() {
