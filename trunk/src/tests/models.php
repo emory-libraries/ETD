d11d2cc2aea227af82090a121c95a9ee3292fc2c
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2008-02-06 20:46:24 +0000

    added policy/xacml object test

--- trunk/src/tests/models.php
+++ trunk/src/tests/models.php
@@ -10,11 +10,15 @@ class ModelGroupTest extends GroupTest {
     $this->addTestFile('models/etdModsTest.php');
     $this->addTestFile('models/FezEtdTest.php');
     $this->addTestFile('models/premisTest.php');
+    $this->addTestFile('models/policyTest.php');
 
+    
     $this->addTestFile('models/userTest.php');
     $this->addTestFile('models/madsTest.php');
 
     $this->addTestFile('models/skosCollectionTest.php');
+
+    
   }
 }
 
