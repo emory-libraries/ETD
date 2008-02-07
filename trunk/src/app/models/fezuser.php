e38161e244a45edfaa803d7cd215606c073766c8
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2008-02-07 18:23:05 +0000

    added fez user-lookup to get netids, etc - progress for ticket:63

---
+++ trunk/src/app/models/fezuser.php
@@ -0,0 +1,26 @@
+<?php
+
+  /* simple database object to retrieve user information from Fez database */
+
+class FezUser extends Zend_Db_Table_Row {
+
+  public function __get($name) {
+    // all fields in fez table are prefixed with 'usr_'
+    // adding the prefix here so code can be more readable
+    if (strpos($name, "usr_") !== 0) {
+      $name = "usr_$name";
+    }
+    return parent::__get($name);
+  }
+  
+  
+}
+
+class FezUsers extends Zend_Db_Table_Rowset {}
+
+class FezUserObject extends Emory_Db_Table {
+  protected $_name           = 'fez_user';
+  protected $_rowsetClass    = 'FezUsers';  
+  protected $_rowClass       = 'FezUser';  
+  protected $_primary        = 'usr_id';    
+}
