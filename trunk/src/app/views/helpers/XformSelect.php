a834d14064f1fd492de325641ef4dc9f39f916f7
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2008-02-18 16:38:10 +0000

    appearance for checkboxes

--- trunk/src/app/views/helpers/XformSelect.php
+++ trunk/src/app/views/helpers/XformSelect.php
@@ -2,20 +2,25 @@
 
 class Zend_View_Helper_xformSelect {
 
-  public function xformSelect($label, $nodeset, $options, $multiple = false) {
+  public function xformSelect($label, $nodeset, $select_options, $options = null) {
+    //$multiple = false,
+    //			      $appearance = null) {
     // make sure nodeset expression uses single quotes (nested inside double quotes)
     $nodeset = str_replace('"', "'", $nodeset);
 
     // multiple or single select
-    if ($multiple)
+    if (isset($options['multiple']) && $options['multiple'])
       $select = "select";
     else $select = "select1";
     
-    $xform = '<xf:' . $select . ' ref="' . $nodeset . '">
+    $xform = '<xf:' . $select . ' ref="' . $nodeset . '"';
+    if (isset($options['appearance'])) 
+      $xform .= ' appearance="' . $options['appearance'] . '" ';
+    $xform .= '>
    <xf:label>' . $label . '</xf:label>
 ';
 
-    foreach ($options as $opt_value => $opt_label) {
+    foreach ($select_options as $opt_value => $opt_label) {
       $xform .= "<xf:item>
   <xf:label>" . $opt_label . "</xf:label>
   <xf:value>" . $opt_value . "</xf:value>
