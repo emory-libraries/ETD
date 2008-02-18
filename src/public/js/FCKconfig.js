cd08bc88836bbd9ed0d60ad2ac0c77bbff8d8259
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2008-02-18 16:23:15 +0000

    FCKEditor config file with ETD toolbar

---
+++ src/public/js/FCKconfig.js
@@ -0,0 +1,14 @@
+// FIXME: universal key doesn't exist anymore?
+
+FCKConfig.ToolbarSets["etd-title"] = [
+      ['Preview','-', 'Bold','Italic','Underline','-','Subscript','Superscript','-','Link','Unlink',
+	   '-', 'SpecialChar', '-', 'SpellCheck', 'About']
+];
+
+FCKConfig.ToolbarSets["etd"] = [
+	['Preview','-', 'Bold','Italic','Underline','-','Subscript','Superscript','-','Link','Unlink',
+	   '-', 'SpecialChar', '-', 'SpellCheck', 'About'],
+	  '/',
+	  ['OrderedList','UnorderedList','-','Outdent','Indent','Blockquote']
+  ];
+
