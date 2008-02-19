532e089f0f1f3b8f3f3c9f4a328828ee47747352
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2008-02-19 22:47:38 +0000

    moved setFedoraAccount function into test group for use by all xacml tests

--- src/tests/xacml.php
+++ src/tests/xacml.php
@@ -2,10 +2,12 @@
 
 require_once("bootstrap.php");
 
+
 class XacmlGroupTest extends GroupTest {
   function XacmlGroupTest() {
     $this->GroupTest('ETD XACML tests');
-    $this->addTestFile('xacml/etdXacmlTest.php');
+    //    $this->addTestFile('xacml/etdXacmlTest.php');
+    $this->addTestFile('xacml/userXacmlTest.php');
   }
 }
 
@@ -14,4 +16,20 @@ if (! defined('RUNNER')) {
   $test = &new XacmlGroupTest();
   $test->run(new HtmlReporter());
 }
+
+
+
+// utility function used by xacml tests
+function setFedoraAccount($user) {
+  $fedora_cfg = Zend_Registry::get('fedora-config');
+  
+  // create a new fedora connection with configured port & server, specified password
+  $fedora = new FedoraConnection($user, $user,	// for test accounts, username = password
+				 $fedora_cfg->server, $fedora_cfg->port);
+  
+  // note: needs to be in registry because this is what the etd object will use
+  Zend_Registry::set('fedora', $fedora);
+}
+
+
 ?>
