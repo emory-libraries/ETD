97ada2107b07a6be19e728d8b59ef59b7266cb73
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2008-02-21 18:12:50 +0000

    fix remaining missing conversion steps - ticket:63

--- trunk/src/scripts/compound2atomistic.php
+++ trunk/src/scripts/compound2atomistic.php
@@ -12,7 +12,8 @@ require_once("api/FedoraConnection.php");
 $opts = new Zend_Console_Getopt(
   array(
 	'noact|n'	=> "no action: don't save to fedora",
-	'pid|p=s'		=> "fedora pid of the fez etd to convert",
+	'pid|p=s'	=> "fedora pid of the fez etd to convert",
+	'urls|u=s'	=> "filename where old and new urls should go",
   )
 );
 
@@ -23,9 +24,12 @@ try {
   exit;
 }
 $pid = $opts->pid;
+$rewrite_file = $opts->urls;
+$rewrite = "";
 
 
 $env_config = new Zend_Config_Xml("../config/environment.xml", "environment");
+Zend_Registry::set('env-config', $env_config);
 
 $config = new Zend_Config_Xml('../config/fedora.xml', $env_config->mode);
 Zend_Registry::set('fedora-config', $config);
@@ -67,7 +71,7 @@ require_once("etdfile.php");
 
 /** convert a compound (fez) emory etd object into new atomist model  */
 
-$etd = new FezEtd($pid);
+$fezetd = new FezEtd($pid);
 
 // map vcard to new user object
 
@@ -75,20 +79,20 @@ $etd = new FezEtd($pid);
 $user = new user();
 
 // map vcard to mads
-$user->mads->name->first = $etd->mods->author->first;	// first & middle - split these out?
-$user->mads->name->last = $etd->mods->author->last;
-$user->mads->permanent->email = $etd->vcard->permanent_email;
-$user->mads->permanent->address->street = $etd->vcard->street;
-$user->mads->permanent->address->city = $etd->vcard->city;
-$user->mads->permanent->address->state = $etd->vcard->state;
-$user->mads->permanent->address->postcode = $etd->vcard->zip;
-$user->mads->permanent->address->country = $etd->vcard->country;
-if (isset($etd->vcard->telephone))
-    $user->mads->permanent->phone = $etd->vcard->telephone;
+$user->mads->name->first = $fezetd->mods->author->first;	// first & middle - split these out?
+$user->mads->name->last = $fezetd->mods->author->last;
+$user->mads->permanent->email = $fezetd->vcard->permanent_email;
+$user->mads->permanent->address->street = $fezetd->vcard->street;
+$user->mads->permanent->address->city = $fezetd->vcard->city;
+$user->mads->permanent->address->state = $fezetd->vcard->state;
+$user->mads->permanent->address->postcode = $fezetd->vcard->zip;
+$user->mads->permanent->address->country = $fezetd->vcard->country;
+if (isset($fezetd->vcard->telephone))
+    $user->mads->permanent->phone = $fezetd->vcard->telephone;
 
 // pull some information from Fez DB (things not stored in vcard)
 $fezUserObject = new FezUserObject();
-$fezuser = $fezUserObject->findByUsrId($etd->vcard->uid);
+$fezuser = $fezUserObject->findByUsrId($fezetd->vcard->uid);
 if ($fezuser) {  // in case user is not in the Fez DB
   $ownerid = $fezuser->username;
   print "found fezuser... username is " . $ownerid . "\n";
@@ -99,21 +103,12 @@ if ($fezuser) {  // in case user is not in the Fez DB
   // emory email
   $user->mads->current->email = $fezuser->email;
 } else {
-  trigger_error("Cannot find user information in Fez Db for author (fez id=" . $etd->vcard->uid . ")", E_USER_WARNING);
+  trigger_error("Cannot find user information in Fez Db for author (fez id=" . $fezetd->vcard->uid . ")", E_USER_WARNING);
 }
 // set dc:title, and foxml label 
-$user->name = $etd->mods->author->full;
+$user->name = $fezetd->mods->author->full;
 
-
-// finished setting values for user object - save
-if ($opts->noact) {
-  print "saving user xml to tmp file: tmp/user.xml\n";
-  $user->saveXMLtoFile("tmp/user.xml");
-  $user->pid = "test:user1";	// set test pid in order to simulate setting rels
-} else {
-  $user_pid = $user->save("migrating to atomistic content model from $pid");
-  print "user object saved as $user_pid\n";
-}
+// finished setting values for user object - except for relation to etd
 
 
 $newetd = new etd();
@@ -124,39 +119,39 @@ if (isset($ownerid)) {
  }
   
 // set formatted fields in html & clean versions in mods
-$newetd->title = $etd->mods->title;
-$newetd->abstract = $etd->mods->abstract;
-$newetd->contents = $etd->mods->tableOfContents;
+$newetd->title = $fezetd->mods->title;
+$newetd->abstract = $fezetd->mods->abstract;
+$newetd->contents = $fezetd->mods->tableOfContents;
 
 // set other dublin core fields
-$newetd->dc->creator = $etd->mods->author->full;
-$newetd->dc->contributor = $etd->mods->advisor->full;
-$newetd->dc->language = $etd->mods->language->text;
-$newetd->dc->date = $etd->mods->date;	// should pick up key date
+$newetd->dc->creator = $fezetd->mods->author->full;
+$newetd->dc->contributor = $fezetd->mods->advisor->full;
+$newetd->dc->language = $fezetd->mods->language->text;
+$newetd->dc->date = $fezetd->mods->date;	// should pick up key date
 
 // set author information
 
 $name_fields = array("full", "first", "last", "affiliation");
 foreach ($name_fields as $field) {
-  $newetd->mods->author->$field = $etd->mods->author->$field;
+  $newetd->mods->author->$field = $fezetd->mods->author->$field;
 }
 
 // set author's department in mods & in view policy
-$newetd->department = $etd->mods->department;
+$newetd->department = $fezetd->mods->department;
 
 // retrieve netid and use to set values in new etd record
-$fezAdvisor = $fezUserObject->findByUsrId($etd->mods->advisor->id);
+$fezAdvisor = $fezUserObject->findByUsrId($fezetd->mods->advisor->id);
 if ($fezAdvisor) {
   $newetd->setAdvisor($fezAdvisor->username);
   // FIXME: possibly need error handling if user is not found in ESD? (unlikely...)
 } else {
   trigger_error("Cannot find user information in Fez Db for advisor (fez id=" .
-		$etd->mods->advisor->id . ")", E_USER_WARNING);
+		$fezetd->mods->advisor->id . ")", E_USER_WARNING);
 }
 
 // set committee
 $committee_ids = array();
-foreach ($etd->mods->committee as $cm) {
+foreach ($fezetd->mods->committee as $cm) {
   if (!isset($cm->full) || ($cm->full == "")) continue;	// skip blank entries
   $fezCommittee = $fezUserObject->findByUsrId($cm->id);
   if ($fezCommittee) 
@@ -173,7 +168,7 @@ if (count($committee_ids)) {	// don't set if none were found
 // copy non-Emory committee from old etd
 // 	clear out non-emory so if there are none we won't get blank xml for that
 $newetd->mods->clearNonEmoryCommittee();
-foreach ($etd->mods->nonemory_committee as $cm) {
+foreach ($fezetd->mods->nonemory_committee as $cm) {
   // skip blank entries (added by Fez)
   if (!isset($cm->full) || ($cm->full == "")) continue;
   $newetd->mods->addCommitteeMember($cm->last,
@@ -182,23 +177,21 @@ foreach ($etd->mods->nonemory_committee as $cm) {
 				    $cm->affiliation);
 }
 
-// FIXME: need to retrieve committee member & advisor netids and add them as relations
 
-
-$newetd->mods->genre = $etd->mods->genre;
-$newetd->mods->language->text = $etd->mods->language->text;
+$newetd->mods->genre = $fezetd->mods->genre;
+$newetd->mods->language->text = $fezetd->mods->language->text;
 // set code for english (?)
 
 // dates 	FIXME: adjust date format? 
-$newetd->mods->originInfo->issued = $etd->mods->originInfo->issued;
+$newetd->mods->originInfo->issued = $fezetd->mods->originInfo->issued;
 
 // in fez etds, yes/no on copyright request was temporarily stored in copyright date
 // 	FIXME: make sure this works correctly for published records 
-if (is_numeric($etd->mods->originInfo->copyright) ||
-    $etd->mods->originInfo->copyright == "Yes")
+if (is_numeric($fezetd->mods->originInfo->copyright) ||
+    $fezetd->mods->originInfo->copyright == "Yes")
   $copyright = "yes";
-elseif ($etd->mods->originInfo->copyright == "" ||
-	$etd->mods->originInfo->copyright == "No")
+elseif ($fezetd->mods->originInfo->copyright == "" ||
+	$fezetd->mods->originInfo->copyright == "No")
   $copyright = "no";
 if ($copyright)
   $newetd->mods->copyright = $copyright;
@@ -207,11 +200,11 @@ else
 
 // in fez etds, yes/no on embargo request was temporarily stored in dateOther
 
-$embargo_duration = str_replace("Embargoed for ", "", $etd->mods->embargo_note);
+$embargo_duration = str_replace("Embargoed for ", "", $fezetd->mods->embargo_note);
 
-if ($etd->mods->embargo_end == "No" || $embargo_duration == "0 days")
+if ($fezetd->mods->embargo_end == "No" || $embargo_duration == "0 days")
   $embargo_request = "no";
-elseif ($etd->mods->embargo_end == "Yes" || $embargo_duration != "")
+elseif ($fezetd->mods->embargo_end == "Yes" || $embargo_duration != "")
   $embargo_request = "yes";
 if ($embargo_request)
   $newetd->mods->embargo_request = $embargo_request;
@@ -223,9 +216,9 @@ if ($embargo_duration)
   $newetd->mods->embargo = $embargo_duration;
 
 // if embargo end date has already been calculated, store it
-if ($embargo_request == "yes" && $etd->mods->embargo_end != "Yes") {
-  $newetd->mods->embargo_end = $etd->mods->embargo_end;
-  $embargo_end = $etd->mods->embargo_end;
+if ($embargo_request == "yes" && $fezetd->mods->embargo_end != "Yes") {
+  $newetd->mods->embargo_end = $fezetd->mods->embargo_end;
+  $embargo_end = $fezetd->mods->embargo_end;
 }
 // store embargo information - may be needed for etdfile xacml policy rules
 if ($embargo_request == "yes") $embargo_requested = true;
@@ -235,15 +228,15 @@ if ($embargo_request == "yes") $embargo_requested = true;
 
 
 // fezetd record created/updated mapped to a new (proper) place in mods
-$newetd->mods->recordInfo->created = $etd->mods->originInfo->created;
-$newetd->mods->recordInfo->modified = $etd->mods->originInfo->modified;
+$newetd->mods->recordInfo->created = $fezetd->mods->originInfo->created;
+$newetd->mods->recordInfo->modified = $fezetd->mods->originInfo->modified;
 
 
 
 //  researchfields
 $fields = array();	// construct an array of id => text name
-for ($i= 0; $i < count($etd->mods->researchfields); $i++) {
-  $fields["id" . $etd->mods->researchfields[$i]->id] = $etd->mods->researchfields[$i]->topic;
+for ($i= 0; $i < count($fezetd->mods->researchfields); $i++) {
+  $fields[$fezetd->mods->researchfields[$i]->id] = $fezetd->mods->researchfields[$i]->topic;
  }
 // set all the fields at once
 $newetd->mods->setResearchFields($fields);
@@ -254,56 +247,86 @@ $newetd->dc->subject = $newetd->mods->researchfields[0]->topic;
 
 
 // copy keywords (no id attributes)
-for ($i= 0; $i < count($etd->mods->keywords); $i++) {
+for ($i= 0; $i < count($fezetd->mods->keywords); $i++) {
   if (array_key_exists($i, $newetd->mods->keywords)) 
-    $newetd->mods->keywords[$i]->topic = $etd->mods->keywords[$i]->topic;
+    $newetd->mods->keywords[$i]->topic = $fezetd->mods->keywords[$i]->topic;
   else 
-    $newetd->mods->addKeyword($etd->mods->keywords[$i]->topic);
+    $newetd->mods->addKeyword($fezetd->mods->keywords[$i]->topic);
 }
 
 // copy number of pages
-if (isset($etd->mods->pages))
-  $newetd->mods->pages = $etd->mods->pages;
+if (isset($fezetd->mods->pages))
+  $newetd->mods->pages = $fezetd->mods->pages;
 else 
   trigger_error("Record does not seem to have page count", E_USER_WARNING);
 
-if ($etd->mods->genre == "Dissertation") {
+if ($fezetd->mods->genre == "Dissertation") {
   $newetd->mods->degree->level = "Doctoral";
   $newetd->mods->degree->name = "Ph.D.";	// safe to assume for limited pilot participants?
 } else { // check genre again? should be one or the other
   $newetd->mods->degree->level = "Masters";
   // pull degree name from vcard (value from alumni feed stored on publication)
-  $newetd->mods->degree->name = $etd->vcard->degree;
+  $newetd->mods->degree->name = $fezetd->vcard->degree;
 }
 // note: discipline is being used for program subfield
 //   - no subfields in pilot departments, so ignoring
 
-// add relations to user
-//$newetd->rels_ext->addRelationToResource("rel:owner", $user->pid);
 
 
 // set status & load appropriate policies
-$newetd->setStatus($etd->status);
-if ($embargo_requested && $embargo_end) {
-  // fixme : set embargo end date in published policy
- }
+$newetd->setStatus($fezetd->status);
+if ($newetd->status() == "published" &&
+    $embargo_requested && $embargo_end) { 
+  // set embargo end date as condition in published policy rule
+  $newetd->policy->published->condition->date = $embargo_end;
+}
 
 if ($opts->noact) {
   print "saving etd foxml to tmp file: tmp/etd.xml\n";
-  $newetd->saveXMLtoFile("tmp/etd.xml");
   $newetd->pid = "test:etd1";	// set test pid in order to simulate setting rels, premis, etc
+  $newetd->saveXMLtoFile("tmp/etd.xml");
 } else {
   $newid = $newetd->save("migrating to atomistic content model from $pid");
   print "etd object saved as $newid\n";
+  
+  // need to reinitialize from fedora so datastreams can be saved separately
+  $newetd = new etd($newid);
+}
+
+if (isset($newetd->mods->ark) && $newetd->mods->ark != "")
+  $newurl = $newetd->mods->ark;		// should be set if not in noact mode
+else
+  $newurl = "view/record/pid/" . $newetd->pid;
+
+$rewrite .= "# etd record - $ownerid
+view.php?pid={$fezetd->pid}	$newurl
+";
+
+
+// NOTE: in noact mode, temporary pids are set in order to test setting rels & premis 
+
+
+// add etd relation to user object
+$user->rels_ext->addRelationToResource("rel:authorInfoFor", $newetd->pid);
+
+if ($opts->noact) {
+  print "saving user xml to tmp file: tmp/user.xml\n";
+  $user->pid = "test:user1";	// set test pid in order to simulate setting rels
+  $user->saveXMLtoFile("tmp/user.xml");
+} else {
+  $user_pid = $user->save("migrating to atomistic content model from $pid");
+  print "user object saved as $user_pid\n";
 }
 
 
+// add author info relation to etd object
+$newetd->rels_ext->addRelationToResource("rel:hasAuthorInfo", $user->pid);
 
-// FIXME: in noact mode set temporary pids to test setting rels & premis stuff...
 
 
+$filecount = 0;
 // generate etdFile objects based on these
-foreach ($etd->files as $file) {
+foreach ($fezetd->files as $file) {
   switch($file->label) {
   case "Dissertation":
     //    print "Dissertation: ID=" . $file->ID . ", mimetype=" . $file->MIMEType . "\n";
@@ -322,51 +345,61 @@ foreach ($etd->files as $file) {
   // let etdfile handle getting new pids (using arks)
   $etdfile->label = $file->label;
   if (isset($ownerid)) $etdfile->owner = $ownerid;
-  $etdfile->file->url = "http://" . $config->server . ":" . $config->port
-    . "/fedora/get/" . $pid . "/" . $file->ID;
+
   $etdfile->file->label = $file->label;
   $etdfile->file->mimetype = $etdfile->dc->type = $file->MIMEType;
   $etdfile->dc->description = $file->ID;
   // fixme: any other settings we can do?
 
-  // NOTE: can't add relations until after objects are ingested and they have pids
   // add relations between objects
-  //  $etdfile->rels_ext->addRelationToResource("rel:owner", $user->pid);
   $etdfile->rels_ext->addRelationToResource("rel:is" . $reltype . "Of", $newetd->pid);
-  $newetd->rels_ext->addRelationToResource("rel:has" . $reltype, $etdfile->pid);
   
   
   // ingest into fedora
   if ($opts->noact) {
     print "saving etdfile " . $file->label . " to tmp file: tmp/" . $file->ID . ".xml\n";
+    $etdfile->pid = "test:etdfile" . ++$filecount;   
     $etdfile->saveXMLtoFile("tmp/" . $file->ID . ".xml");
   } else {
+    //if we just refer to the files at the public url, they may be inaccessible/embargoed
+
+    // should access as fedoraAdmin so the file can be retrieved, then re-upload
+    $filename = "/tmp/" . $file->ID;
+    file_put_contents($filename, $fedora->getDatastream($fezetd->pid, $file->ID));
+    $etdfile->file->url = $fedora->upload($filename);
+    
     $fileid = $etdfile->save("migrating to atomistic content model from $pid");
     print "etd file object saved as $fileid\n";
   }
+
+  if ($reltype != "Original") {		// no redirects for archive copy
+    // print out old and new urls to be used in creating rewrite rules
+    
+    /*	fixme: identifier not currently stored in etdfile object
+     if (isset($etdfile->mods->ark) && $newetd->mods->ark != "")
+      $newurl = $newetd->mods->ark;		// should be set if not in noact mode
+      else*/
+      $newurl = "view/record/pid/" . $newetd->pid;
+    
+    $rewrite .= "# etd file ($reltype) 
+eserv.php?pid={$fezetd->pid}&dsID={$file->ID} 	file/view/pid/{$etdfile->pid}
+";
+// FIXME: new file url? will this change?
+
+  }
+
+  // add relation to newetd
+  $newetd->rels_ext->addRelationToResource("rel:has" . $reltype, $etdfile->pid);
 }
 
 
-/* FIXME: still needs to be included in the migration:
-    - set permissions for embargo, policy status, etc.
-*/
-
-
-// remaining actions depend on etd object having been created in fedora
-if ($opts->noact) exit;
- 
-
 // copy premis events, updating to new format
 
-// need to reinitialize from fedora so datastreams can be saved separately
-$newetd = new etd($newid);
+// note: premis should only be updated after object has a pid
+$newetd->premis->object->identifier->value = $newetd->pid;
 
-// note: premis must be done after object has a pid
-$newetd->premis->object->identifier->value = $newid;
-
-//FIXME: need to set object id in premis before adding events (event ids based on object id)
 // fez lists most recent events first; we want the history in chronological order
-foreach (array_reverse($etd->premis->event) as $event) {
+foreach (array_reverse($fezetd->premis->event) as $event) {
   // note: agent value is returning fez userid (numeric database id)
 
   preg_match("/(^.*) by (.*)$/", $event->detail, $matches);
@@ -418,9 +451,21 @@ foreach (array_reverse($etd->premis->event) as $event) {
 			    $agent, $event->date);
 }
 
-$result = $newetd->save("migrated record history");
-print "added premis actions and saved at $result\n";
+if ($opts->noact) {
+  print "updating etd foxml in tmp file with rels & premis: tmp/etd.xml\n";
+  $newetd->saveXMLtoFile("tmp/etd.xml");
+} else {
+  $result = $newetd->save("migrated record history");
+  print "updated etd with rels & premis actions; saved at $result\n";
+}
 
+
+// save old and new urls based on old & new pids for all objects/files
+// (basis for rewrite rules)
+if ($rewrite_file) {
+  print "saving old and new urls for rewrite rules in $rewrite_file\n";
+  file_put_contents($rewrite_file, $rewrite, FILE_APPEND);
+}
 
 
 
