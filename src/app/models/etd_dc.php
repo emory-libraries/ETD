db338a21b118726d89ec5faa36f4f5be7364eb34
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2008-02-22 20:04:57 +0000

    fix for setting ark; mapping/config for page number - ticket:90

--- src/app/models/etd_dc.php
+++ src/app/models/etd_dc.php
@@ -21,10 +21,11 @@ class etd_dc extends dublin_core {
     // fixme: will these xpaths work?
     $this->xmlconfig["mimetype"] = array("xpath" => "dc:format[contains(., '/')]");	
     $this->xmlconfig["filesize"] = array("xpath" => "dc:format[not(contains(., '/'))]");
+    $this->xmlconfig["pages"] = array("xpath" => "dc:format[contains(., ' p.')]");
 
     
 
-    $this->additional_fields = array("ark", "mimetype", "filesize");
+    $this->additional_fields = array("ark", "mimetype", "filesize", "pages");
   }
 
   // simple check - should only be dc terms
@@ -33,7 +34,36 @@ class etd_dc extends dublin_core {
     return (in_array($name, $this->fields) || in_array($name, $this->additional_fields));
   }
 
-  
+
+  public function setArk($ark) {
+    if (isset($this->ark)) {
+      // is this an error? ark shouldn't change
+      $this->ark = $ark;
+    } else {
+      $this->identifier->append($ark);
+      $this->update();	// update so the 'ark' mapping will get picked up
+    }
+  }
+
+  public function setPages($num) {
+    if (isset($this->pages)) {
+      $this->pages = "$num p.";
+    } else {
+      $this->format->append("$num p.");
+      $this->update();
+    }
+  }
+
+
+  public function __get($name) {
+    $value = parent::__get($name);
+    switch ($name) {
+    case "pages":
+      $value = str_replace(" p.", "", $value);	// return just the number
+      break;
+    }
+    return $value;
+  }
 
 
 }
