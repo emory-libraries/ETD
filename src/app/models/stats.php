3165194b02af3b1b012ca48695cd8a9eb8e5b311
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2008-02-28 20:18:24 +0000

    stats by country (incomplete)

--- src/app/models/stats.php
+++ src/app/models/stats.php
@@ -29,8 +29,79 @@ class StatObject extends Stats_Db_Table {
     if (! $this->_db) 
       $this->_db = Zend_Registry::get("stat-db");
   }
+
+  public function count(array $pids) {
+    $select = $this->_db->select();	// newer versions of Zend?   $select = $this->select();
+    $select->from($this->_name, array('COUNT(*) as count', 'type'));	// type ?
+
+    for ($i = 0;  $i < count($pids); $i++) {
+      if ($i == 0) $select->where('pid = ?', $pids[$i]);
+      else $select->orWhere('pid = ?', $pids[$i]);
+    }
+    $select->group("type");
+
+    $stmt = $this->_db->query($select);
+    $result = $stmt->fetchAll();
+
+    $total = array("abstract" => 0, "file" => 0);
+    foreach ($result as $row) {
+      $total[$row['type']] = $row['count'];
+    }
+    return $total;
+  }
+
+  // Fez code is getting two counts and then merging the arrays... there must be a better way!?!
+  public function countByCountry(array $pids) {
+    $abs_sel = $this->selectTypeByCountry($pids, "abstract");
+    $file_sel = $this->selectTypeByCountry($pids, "file");
+    $sql = "SELECT a.c as country, abstract, file FROM (" . $abs_sel->__toString() . ") as a
+	    LEFT OUTER JOIN (" . $file_sel->__toString() . ") AS f ON a.c = f.c
+    	    ORDER BY (a.abstract + f.file) DESC;";
+
+    //    print "<pre>" . $sql . "</pre>";
+    $stmt = $this->_db->query($sql);
+    $result = $stmt->fetchAll();
+    //    print "<pre>"; print_r($result); print "</pre>";
+    
+    return $result;
+    /* select a.country, a.abstract, f.file from (select count(*) as abstract, country
+    from stats where type='abstract' group by country) as a
+   left join (select count(*) as file, country from stats where type='file' group by country)
+   as f on a.country = f.country order by (a.abstract + f.file) DESC;*/
+
+    
+  }
+
+  private function selectTypeByCountry(array $pids, $type) {
+    $select = $this->_db->select();
+    $select->from($this->_name, array("COUNT(*) as $type", "country as c"));
+    		// note: have to rename country because field wasn't being found on the join
+
+    for ($i = 0;  $i < count($pids); $i++) {
+      if ($i == 0) $select->where('pid = ?', $pids[$i]);
+      else $select->orWhere('pid = ?', $pids[$i]);
+    }
+
+    //FIXME: need (1 or 2) and 3 NOT 1 or 2 nd 3
+    $select->where('type = ?', $type);
+    $select->group("country");
+//    $select->order("$type DESC");
+
+    return $select;
+
+    $stmt = $this->_db->query($select);
+    $result = $stmt->fetchAll();
+    //    print "<pre>"; print_r($result); print "</pre>";
+    return $result;
+  }
+
+  
+
+  // need another count totals - by month/year (how to do?)
 }
 
+
+/* info to keep track of known robots */
 class Bot extends Zend_Db_Table_Row {}
 
 class Bots extends Zend_Db_Table_Rowset {}
@@ -43,6 +114,7 @@ class BotObject extends Stats_Db_Table {
 
 }
 
+/* info to keep track of when the script was run */
 class LastRun extends Zend_Db_Table_Row {}
 
 class LastRuns extends Zend_Db_Table_Rowset {}
