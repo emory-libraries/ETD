876d09dc010706f94c471278e6532e395ae51030
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2008-02-29 18:23:43 +0000

    fix query, added functions to count by month+year - part of ticket:26

--- trunk/src/app/models/stats.php
+++ trunk/src/app/models/stats.php
@@ -58,11 +58,15 @@ class StatObject extends Stats_Db_Table {
 	    LEFT OUTER JOIN (" . $file_sel->__toString() . ") AS f ON a.c = f.c
     	    ORDER BY (a.abstract + f.file) DESC;";
 
-    //    print "<pre>" . $sql . "</pre>";
+    //        print "<pre>" . $sql . "</pre>";
     $stmt = $this->_db->query($sql);
     $result = $stmt->fetchAll();
-    //    print "<pre>"; print_r($result); print "</pre>";
-    
+
+    // convert nulls to zeroes to simplify display logic
+    for ($i = 0; $i < count($result); $i++){
+      if (is_null($result[$i]['file'])) $result[$i]['file'] = 0;
+      if (is_null($result[$i]['abstract'])) $result[$i]['abstract'] = 0;
+    }
     return $result;
     /* select a.country, a.abstract, f.file from (select count(*) as abstract, country
     from stats where type='abstract' group by country) as a
@@ -72,28 +76,71 @@ class StatObject extends Stats_Db_Table {
     
   }
 
+  // build a select statement to be used as a subselect
   private function selectTypeByCountry(array $pids, $type) {
     $select = $this->_db->select();
     $select->from($this->_name, array("COUNT(*) as $type", "country as c"));
     		// note: have to rename country because field wasn't being found on the join
 
+    $pid_filter = array();
+    //          $sql .= $this->getAdapter()->quoteInto($where_sql, $uname);
     for ($i = 0;  $i < count($pids); $i++) {
-      if ($i == 0) $select->where('pid = ?', $pids[$i]);
-      else $select->orWhere('pid = ?', $pids[$i]);
+      $pid_filter[] = $this->getAdapter()->quoteInto("pid = ?", $pids[$i]);
     }
 
-    //FIXME: need (1 or 2) and 3 NOT 1 or 2 nd 3
-    $select->where('type = ?', $type);
+    // need to group (pid1 or pid2) and type, NOT pid1 or pid2 and type
+    $where = "(" . implode(" OR ", $pid_filter) . ") AND type = '$type' ";
+    
+    $select->where($where);
     $select->group("country");
-//    $select->order("$type DESC");
 
     return $select;
+  }
 
-    $stmt = $this->_db->query($select);
+
+  public function countByMonthYear(array $pids) {
+    $abs_sel = $this->selectTypeByMonthYear($pids, "abstract");
+    $file_sel = $this->selectTypeByMonthYear($pids, "file");
+    $sql = "SELECT a.month as month, abstract, file FROM (" . $abs_sel->__toString() . ") as a
+	    LEFT OUTER JOIN (" . $file_sel->__toString() . ") AS f ON a.month = f.month
+    	    ORDER BY a.month;";
+
+    //        print "<pre>" . $sql . "</pre>";
+    $stmt = $this->_db->query($sql);
     $result = $stmt->fetchAll();
+
+    // convert nulls to zeroes to simplify display logic
+    for ($i = 0; $i < count($result); $i++){
+      if (is_null($result[$i]['file'])) $result[$i]['file'] = 0;
+      if (is_null($result[$i]['abstract'])) $result[$i]['abstract'] = 0;
+    }
     //    print "<pre>"; print_r($result); print "</pre>";
     return $result;
   }
+
+    // build a select statement to be used as a subselect
+  private function selectTypeByMonthYear(array $pids, $type) {
+    $select = $this->_db->select();
+    $select->from($this->_name, array("COUNT(*) as $type", "strftime('%m-%Y', date) as month"));
+    // in mysql: date_format(date, '%b %Y')
+    		// note: have to rename country because field wasn't being found on the join
+
+    $pid_filter = array();
+    //          $sql .= $this->getAdapter()->quoteInto($where_sql, $uname);
+    for ($i = 0;  $i < count($pids); $i++) {
+      $pid_filter[] = $this->getAdapter()->quoteInto("pid = ?", $pids[$i]);
+    }
+
+    // need to group (pid1 or pid2) and type, NOT pid1 or pid2 and type
+    $where = "(" . implode(" OR ", $pid_filter) . ") AND type = '$type' ";
+    
+    $select->where($where);
+    $select->group("month");
+
+    return $select;
+  }
+
+
 
   
 
