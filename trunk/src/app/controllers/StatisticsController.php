e2cea1a643f4cd5c7159077edb19b2b9c09dae65
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2008-03-03 17:02:02 +0000

    actions for country/month stats for all etds - ticket:26

--- trunk/src/app/controllers/StatisticsController.php
+++ trunk/src/app/controllers/StatisticsController.php
@@ -17,6 +17,22 @@ class StatisticsController extends Zend_Controller_Action {
      $this->view->messages = $this->_helper->flashMessenger->getMessages();
    }
 
+   // stats for all etds, broken down by month/year
+   public function countryAction() {
+     $stats = new StatObject();
+     $this->view->country = $stats->countByCountry();
+     $this->view->countries = new CountryNames;
+     $this->view->title = "Access Statistics by Country";
+   }
+    
+   
+   // stats for all etds, broken down by month/year
+   public function monthlyAction() {
+     $stats = new StatObject();
+     $this->view->month = $stats->countByMonthYear();
+     $this->view->title = "Access Statistics by Month";
+   }
+
    // statistics for a single record
    public function recordAction() {
      // fixme: error handling ?
@@ -32,6 +48,7 @@ class StatisticsController extends Zend_Controller_Action {
      $this->view->country = $stats->countByCountry($pids);
      $this->view->countries = new CountryNames;
 
+     $this->view->month = $stats->countByMonthYear($pids);
      
      $this->view->etd = $etd;
      $this->view->title = "Access Statistics for " . $etd->label;
