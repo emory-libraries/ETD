e7a0261af0e7920164e6809d74b538d9627c1933
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2008-03-05 20:57:40 +0000

    fix for user not logged in

--- trunk/src/lib/Etd/Controller/Action.php
+++ trunk/src/lib/Etd/Controller/Action.php
@@ -21,13 +21,15 @@ abstract class Etd_Controller_Action extends Zend_Controller_Action {
     $this->acl = Zend_Registry::get('acl');
     if (Zend_Registry::isRegistered('current_user'))
       $this->current_user = Zend_Registry::get('current_user');
-    else $this->current_user = "guest";		// no user currently logged in (guest?)
+    // no user currently logged in -- leave current_user unset
+    //        else $this->current_user = "guest";		
+
     $this->env = Zend_Registry::get('environment');
 
     
     // these variables are also needed in the view
     $this->view->acl = $this->acl;
-    $this->view->current_user = $this->current_user;
+    if (isset($this->current_user)) $this->view->current_user = $this->current_user;
     $this->view->env = $this->env;
 
     // store controller/action  name in view (needed for certain pages)
