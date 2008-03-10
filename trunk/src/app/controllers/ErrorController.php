c9182b914bb88f2cc0070866f6e79a59e07fffa1
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2008-03-10 15:22:08 +0000

    nicer error handling / user-friendly display for production

--- trunk/src/app/controllers/ErrorController.php
+++ trunk/src/app/controllers/ErrorController.php
@@ -7,10 +7,22 @@ class ErrorController extends Etd_Controller_Action {
    }
   
   public function errorAction() {
-    $this->view->backtrace = debug_backtrace();
+    // $this->view->backtrace = debug_backtrace();
+    $errorHandler = $this->_getParam("error_handler");
+    $this->view->exception = $errorHandler->exception;
+
+    switch ($errorHandler->type) {
+    case "EXCEPTION_NO_ACTION":
+    case "EXCEPTION_NO_CONTROLLER":
+      $this->view->error = "Document not Found"; break;
+    case "EXCEPTION_OTHER":
+    default:
+      $this->view->error = "Unknown Error"; break;
+    }
   }
 
 
+  // when Fedora is not accessible, the Etd Controller forwards to this pagemessage
   public function fedoraUnavailableAction() {
     $this->view->service = "Repository";
     $this->_helper->viewRenderer->setScriptAction("unavailable");
