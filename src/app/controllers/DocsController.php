b08b2e88cf01019e57d371b33d231016e583aa18
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2008-03-10 20:14:15 +0000

    added titles for html header

--- src/app/controllers/DocsController.php
+++ src/app/controllers/DocsController.php
@@ -2,12 +2,24 @@
 
 class DocsController extends Etd_Controller_Action {
 
-  public function aboutAction() {}
-  public function faqAction() {}
-  public function ipAction() {}
-  public function policiesAction() {}
-  public function boundcopiesAction() {}
-  public function instructionsAction() {}
+  public function aboutAction() {
+    $this->view->title = "About Emory's ETD Repository";
+  }
+  public function faqAction() {
+    $this->view->title = "Frequently Asked Questions";
+  }
+  public function ipAction() {
+    $this->view->title = "Intellectual Property";
+  }
+  public function policiesAction() {
+    $this->view->title = "Policies & Procedures";
+  }
+  public function boundcopiesAction() {
+    $this->view->title = "Bound Copies";
+  }
+  public function instructionsAction() {
+    $this->view->title = "Submission Instructions";
+  }
      
 
 }
