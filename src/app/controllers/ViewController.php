87189de9329ee89a11d3c114c0c774b4a71273aa
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2008-03-13 16:17:57 +0000

    much more complete DC header for etds - ticket:230

--- src/app/controllers/ViewController.php
+++ src/app/controllers/ViewController.php
@@ -14,7 +14,7 @@ class ViewController extends Etd_Controller_Action {
      if (!$this->_helper->access->allowedOnEtd("view metadata", $etd)) return;
      $this->view->etd = $etd;
      $this->view->title = $etd->label;
-     $this->view->dc = $etd->dc;
+     //$this->view->dc = $etd->dc;		/* DC not as detailed as MODS; using etd DC header template */
      $this->view->messages = $this->_helper->flashMessenger->getMessages();
    }
 
