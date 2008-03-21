3b2a82c3cca150f604d724a67bc3a7090c047b53
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2008-03-21 21:08:49 +0000

    preliminary recently published list stuff - ticket:32

--- src/app/controllers/IndexController.php
+++ src/app/controllers/IndexController.php
@@ -4,8 +4,11 @@ class IndexController extends Etd_Controller_Action {
 
   
   public function indexAction() {	
-		$this->view->assign("title", "Welcome");
-	}
+    $this->view->assign("title", "Welcome");
+
+    $this->view->feed = new Zend_Feed_Atom("http://wilson.library.emory.edu/~rsutton/etd/browse/recentFeed");
+    
+  }
 
 	public function listAction() {
 	}
