3d8c195a14e197a89c46539d01f04934a97a8a89
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2008-03-26 20:31:50 +0000

    switch from atom to rss, add more information (author, categories), simplify feed creation - ticket:32

--- trunk/src/app/models/etd_feed.php
+++ trunk/src/app/models/etd_feed.php
@@ -8,7 +8,7 @@ class Etd_Feed {
   
   public function __construct($title, $url, $etds) {
     $builder = new Etd_Feed_Builder($title, $url, $etds);
-    $this->zfeed = Zend_Feed::importBuilder($builder);
+    $this->zfeed = Zend_Feed::importBuilder($builder, 'rss');
   }
 
   public function saveXml() { return $this->zfeed->saveXml(); }
@@ -24,15 +24,36 @@ class Etd_Feed_Builder implements Zend_Feed_Builder_Interface {
   
   public function __construct($title, $link, array $etds) {
     $this->header = new Zend_Feed_Builder_Header($title, $link);
-    
+
+
+    $front  = Zend_Controller_Front::getInstance();
+    $request = $front->getRequest();
+    $baseurl = ($request->getServer("HTTPS") == "") ? "http://" : "https://";
+    $baseurl .=  $request->getServer("SERVER_NAME") . $request->getBaseUrl() .
+      "/view/record/pid/";	// FIXME: use a helper somehow?
+
+
     $this->entries = array();
-    foreach ($etds as $etd) {
-      $entry = new Zend_Feed_Builder_Entry($etd->label,		// title
-					   $etd->mods->ark,		// link
+    foreach ($etds as $etd) { 
+      $link = $baseurl . $etd->pid;
+      $entry = new Zend_Feed_Builder_Entry($etd->label,			// plain-text title
+					   $link,			// link within site
 					   $etd->dc->description	// brief non-html description
 					   );
+      $entry->guid = $etd->mods->ark;	// globally unique identifier (ark)	no way to set isPermaLink="true" ?
       $entry->setLastUpdate(strtotime($etd->mods->originInfo->issued));
-      //       $entry->author->name = $etd->mods->author->full;	// adds but doesn't output
+      $entry->author = $etd->mods->author->full;	// adds but doesn't output
+
+      // add program, research fields, and keywords as categories (appropriate/useful?)
+      $entry->addCategory(array("term" => $etd->program(),
+				"scheme" => "Program"));
+      foreach (array_merge($etd->mods->researchfields, $etd->mods->keywords) as $subject) {
+	$entry->addCategory(array("term" => $subject->topic,
+				  "scheme" => $subject->authority));
+      }
+
+      // add pdf ark as an enclosure ? (if available) -- needs url, length in bytes, and mime type
+      
       // Zend_Atom_Feed doesn't output author?
       $this->entries[] = $entry;
     }
