5f5c46014a5a6c3269ce615d36ef45a59881a177
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2008-03-26 20:31:14 +0000

    new feed url, new absolute url helper to build it - ticket:32

--- trunk/src/app/controllers/IndexController.php
+++ trunk/src/app/controllers/IndexController.php
@@ -6,8 +6,8 @@ class IndexController extends Etd_Controller_Action {
   public function indexAction() {	
     $this->view->assign("title", "Welcome");
 
-    $this->view->feed = new Zend_Feed_Atom("http://wilson.library.emory.edu/~rsutton/etd/browse/recentFeed");
-    
+    // rss feed of recently published ETD records - for display on sidebar
+    $this->view->feed = new Zend_Feed_Rss($this->_helper->absoluteUrl('recent', 'feeds'));
   }
 
 	public function listAction() {
