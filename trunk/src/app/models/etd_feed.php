30a567dd04129162278b227d6587989ac9f9d93c
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2008-03-28 18:39:58 +0000

    set program, PQ fields, keywords as categories

--- trunk/src/app/models/etd_feed.php
+++ trunk/src/app/models/etd_feed.php
@@ -6,8 +6,8 @@ class Etd_Feed {
 
   private $zfeed;
   
-  public function __construct($title, $url, $etds) {
-    $builder = new Etd_Feed_Builder($title, $url, $etds);
+  public function __construct($title, $url, $etds, $description = null) {
+    $builder = new Etd_Feed_Builder($title, $url, $etds, $description);
     $this->zfeed = Zend_Feed::importBuilder($builder, 'rss');
   }
 
@@ -22,9 +22,10 @@ class Etd_Feed_Builder implements Zend_Feed_Builder_Interface {
   protected $header;
   protected $entries;
   
-  public function __construct($title, $link, array $etds) {
+  public function __construct($title, $link, array $etds, $description = null) {
     $this->header = new Zend_Feed_Builder_Header($title, $link);
-
+    if ($description) $this->header->description = $description;
+      
 
     $front  = Zend_Controller_Front::getInstance();
     $request = $front->getRequest();
@@ -35,21 +36,26 @@ class Etd_Feed_Builder implements Zend_Feed_Builder_Interface {
 
     $this->entries = array();
     foreach ($etds as $etd) { 
-      $link = $baseurl . $etd->pid;
-      $entry = new Zend_Feed_Builder_Entry($etd->label,			// plain-text title
+      $link = $baseurl . $etd->pid();
+      $entry = new Zend_Feed_Builder_Entry($etd->title(),			// plain-text title
 					   $link,			// link within site
-					   $etd->dc->description	// brief non-html description
+					   $etd->_abstract()	// brief non-html description
 					   );
-      $entry->guid = $etd->mods->ark;	// globally unique identifier (ark)	no way to set isPermaLink="true" ?
-      $entry->setLastUpdate(strtotime($etd->mods->originInfo->issued));
-      $entry->author = $etd->mods->author->full;	// adds but doesn't output
+      $entry->guid = $etd->ark;		// globally unique identifier (ark)	no way to set isPermaLink="true" ?
+      $entry->setLastUpdate(strtotime($etd->dateIssued));
+      $entry->author = $etd->author();	// adds but doesn't output
 
       // add program, research fields, and keywords as categories (appropriate/useful?)
-      $entry->addCategory(array("term" => $etd->program(),
-				"scheme" => "Program"));
-      foreach (array_merge($etd->mods->researchfields, $etd->mods->keywords) as $subject) {
-	$entry->addCategory(array("term" => $subject->topic,
-				  "scheme" => $subject->authority));
+      if ($etd->program())
+	$entry->addCategory(array("term" => $etd->program(),
+				  "scheme" => "Program"));
+      foreach ($etd->researchfields() as $subject) {
+	$entry->addCategory(array("term" => $subject,
+				  "scheme" => "ProQuest Research Field"));
+      }
+      foreach ($etd->keywords() as $subject) {
+	$entry->addCategory(array("term" => $subject,
+				  "scheme" => "keyword"));
       }
 
       // add pdf ark as an enclosure ? (if available) -- needs url, length in bytes, and mime type
