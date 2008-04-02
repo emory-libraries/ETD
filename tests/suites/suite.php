dd26211a138ef15ff011dd2f83dc78628e62697f
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2008-04-02 20:00:00 +0000

    rearranging unit tests - ticket:285

---
+++ tests/suites/suite.php
@@ -0,0 +1,22 @@
+<?php
+define('RUNNER', true);
+
+require_once("bootstrap.php");
+
+// test groups
+require_once('models.php');
+require_once('controllers.php');
+require_once('lib.php');
+
+$suite = new TestSuite('All Tests');
+
+// models
+$suite->addTestCase(new ModelGroupTest());
+
+// controllers
+$suite->addTestCase(new ControllerGroupTest());
+
+$suite->run(new HtmlReporter());
+
+
+?>
