e427681e0479ebca3df8b6ec2f97885cdad20d51
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2008-07-22 15:31:37 +0000

    use Tidy to clean up html formatting - preserve indentation formatting (#329), convert special characters to numeric entities so fedora can handle them (#328)

--- trunk/src/app/models/etd_html.php
+++ trunk/src/app/models/etd_html.php
@@ -17,8 +17,8 @@ class etd_html extends foxmlDatastreamAbstract {
     parent::__construct($dom, $config);
   }
 
-  /* FIXME: does this need to be valid html? or can we just use tags
-   from the namespace in a different container?
+  /* Note: this is not a full, valid html document with head & body;
+     simply using html tags for formatting.
   */
   public static function getTemplate() {
     return '<html>
@@ -36,16 +36,18 @@ class etd_html extends foxmlDatastreamAbstract {
     return etd_html::dslabel;
   }
 
-
   public function __set($name, $value) {
     switch ($name) {
     case "title":	
       /* don't wrap title in a paragraph tag (added by FCKeditor) */
-      $value = preg_replace("|^\s*<p>\s*(.*)\s*</p>\s*$|", "$1", $value);
+      // FIXME: is this fix still needed ? handle paragraphs with styles instead?
+      //      $value = preg_replace("|^\s*<p>\s*(.*)\s*</p>\s*$|", "$1", $value);
     case "abstract":
     case "contents":
       parent::__set($name, etd_html::cleanEntities($value));
-      return $this->map{$name} = etd_html::tags_to_nodes($this->map{$name});
+      $this->map{$name} = etd_html::tags_to_nodes($this->map{$name});
+      $this->update();
+      return $this->map{$name};
     default:
       return parent::__set($name, $value);
     }
@@ -70,27 +72,31 @@ class etd_html extends foxmlDatastreamAbstract {
     case "title":
     case "abstract":
     case "contents":
-      $div = $this->map{$name}->ownerDocument->saveXML($this->map{$name});
-      $div = preg_replace("|<div[^>]*>(.*)</div>$|s", "$1", $div);	// treat as a single line
-      return $div;
+      return etd_html::getContentXML($this->map{$name});
     default:
       return parent::__get($name);
     }
   }
 
+  // return the content of the node *without* the containing node tag 
+  public static function getContentXml($node) {
+    $content = "";
+    if ($node->hasChildNodes()) {
+      for ($i = 0; $i < $node->childNodes->length; $i++) {
+	$child = $node->childNodes->item($i);
+	$content .= $node->ownerDocument->saveXML($child);
+      }
+    } else {
+      $content = $node->nodeValue;
+    }
+    return $content;
+  }
+
   // utility functions for handling html-related of tasks
-  
+
+  // clean up entities, convert named entities into numeric ones that fedora can handle
   public static function cleanEntities($string, $clean_whitespace = true) {
     // convert tags to a more easily matchable form, remove unneeded formatting
-
-    /* FIXME: make better use of htmlentities ?
-     * can specify quote_style, character set (UTF-8), double_encode...
-     */
-
-    
-    // convert any non-entities into entities so they can be cleaned up
-    //  $string = htmlentities($string);	 // is this needed?
-    
     $string = str_replace("&amp;", "&", $string);
     
     $search = array("&lt;", "&gt;", "&rsquo;", "&ldquo;", "&rdquo;", "&quot;", "&ndash;", "&mdash;", "&shy;");
@@ -101,35 +107,28 @@ class etd_html extends foxmlDatastreamAbstract {
       $string = str_replace("&nbsp;", " ", $string);
     }
     
-
-    // replace unicode characters 
-    $search = array("&alpha;", "&beta;", "&gamma;", "&Gamma;","&delta;","&Delta;","&zeta;","&eta;",
-		    "&iota;","&kappa;", "&lambda;","&Lambda;","&mu;","&nu;","&xi;","&Xi;","&pi;","&Pi;",
-		    "&rho;","&sigma;","&Sigma;","&tau;","&Phi;","&chi;","&psi;","&Psi;","&omega;","&Omega;",
-		    "&szlig;","&ccedil;",);
-    	 
-    
-    $replace = array("&#x03b1;", "&#x03b2;", "&#x03B3;", "&#x0393;", "&#x03B4;","&#x0394;","&#x03B6;",
-		     "&#x03B7;","&#x03B9;","&#x03BA;","&#x03BB;","&#x039B;","&#x03BC;","&#x03BD;","&#x03BE;",
-		     "&#x039E;","&#x03C0;","&#x03A0;","&#x03C1;","&#x03C3;","&#x03A3;","&#x03C4;",
-		     "&#x03A6;","&#x03C7;","&#x03C8;","&#x03A8;","&#x03C9;","&#x03A9;",
-		     "&#x00DF;","&#x00E7;",);
-    
-    $string = str_replace($search, $replace, $string);
-
-    // remove classnames (MSONORMAL, etc.) or any other formatting inside of tags
-    $string = preg_replace("|<([a-z]+)\s+[^>/]+(/?)>|", "<$1$2>", $string);	// (don't mess up empty tags)
-
-    return $string;
+    // use Tidy to clean up the formatting, tags, etc.
+    $tidy_opts = array("output-xhtml" => true,
+		       "drop-font-tags" => true,
+		       "drop-proprietary-attributes" => true,
+		       "join-styles" => false,
+		       "numeric-entities" => true,
+		       "show-body-only" => true,
+		       );
+    /** NOTE: cannot use  option "clean" because it converts inline styles to
+        css styles, which is much harder to manage & display correctly.
+     */
+    return tidy_repair_string($string, $tidy_opts);
   }
 
 
   
   // utility functions for handling html-related of tasks
   public static function cleanTags($string, $keep_breaks = false) {
+
     // convert tags to a more easily matchable form, remove unneeded formatting
     $string = etd_html::cleanEntities($string);
-    
+
     // remove extra spaces, unused tags, clean up break tags; also remove empty tags
     $search = array("|<br\s+/?>|", "|</?i/?>|", "|</?b/?>|", "|</?em/?>|", "|</?strong/?>|",
 		    "|</?sup/?>|", "|</?sub/?>|", "|</?span/?>|", "|</?st1:[^>]+/?>|", "|</?o:p/?>|", "|</?p>|");
@@ -181,63 +180,32 @@ class etd_html extends foxmlDatastreamAbstract {
 
 
 
-  // convert text tags into proper xml nodes (so < & > won't be escaped as characters)
-  public static function tags_to_nodes($node) {
-    if (! $node instanceof DOMNode)
-      print "Error! tags_to_nodes argument should be a DOMNode\n";
-    $nodetext = $node->nodeValue;
-    // blank out current text so it can be replaced with text & new nodes
-    $node->nodeValue = "";	
-
-    //* do some minimal clean-up to remove formatting that shouldn't be even in the HTML
-
-    // remove classnames (MSONORMAL, etc.) or any other formatting inside of tags
-    $nodetext = preg_replace("|<([a-z]+)\s+[^>/]+>|", "<$1>", $nodetext);
-    	// (don't mess up empty tags)
-  
-    // remove unwanted tags
-    $search = array("|</?span>|", "|</?st1:[^>]+>|", "|</?o:p>|");
-    $replace = array();  // standardize break tags to simplify split pattern later
-    $nodetext = preg_replace($search, $replace, $nodetext);
-   
-    // remove unwanted empty tags (sometimes made empty by removing unwanted tags)
-    $search = array("|<i/>|", "|<i>\s*</i>|", "|<b/>|", "|<b>\s*</b>|",
-		    "|<em/>|", "|<em>\s*</em>|", "|<strong/>|", "|<strong>\s*</strong>|",
-		    "|<sup/>|","|<sup>\s*</sup>|", "|<sub/>|", "|<sub>\s*</sub>|",
-		    "|<span/>|", "|<span>\s*</span>|", "|<p/>|", "|<p>\s*</p>|",);
-    $nodetext = preg_replace($search, array(), $nodetext);
-
-
-    // stack of nodes so new elements & text nodes can be added at the appropriate level
-    $current_node = array();
-    array_unshift($current_node, $node);
+  /**
+    * convert text tags contained in node into proper xml nodes
+    * (so < & * > don't get escaped as characters)
     
-    // split the text on any tags 
-    $chunks = preg_split("|(</?[a-z]+\s*/?>)|", $nodetext, null,
-			 PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
-    for ($j = 0; $j < count($chunks); $j++) {
-      if (preg_match("|<([a-z]+)>|", $chunks[$j], $matches)) {
-	// open tag -- create node with the next section as text content
-	$tagname = $matches[1];
-	// don't add text to node here-- next chunk could be another tag
-	$newnode = $node->ownerDocument->createElement($tagname);
-	$current_node[0]->appendChild($newnode);
-	// make new node the current one that subsequent elements & text are added to
-	array_unshift($current_node, $newnode);
-      } elseif (preg_match("|<([a-z]+)\s*/>|", $chunks[$j], $matches)) {
-	// empty tag -- add to document with no text content
-	$tagname = $matches[1];
-	$current_node[0]->appendChild($node->ownerDocument->createElement($tagname));
-      } elseif (preg_match("|</([a-z]+)>|", $chunks[$j], $matches)) {
-	$tagname = $matches[1];
-	// close tag -- remove node from current node stack, but ONLY if tagname matches
-	if ($tagname == $current_node[0]->nodeName)
-	  array_shift($current_node);
-      } else {
-	// regular text
-	$current_node[0]->appendChild($node->ownerDocument->createTextNode($chunks[$j]));
+    * @param DOMNode $node
+    * @return converted node
+    */
+  public static function tags_to_nodes(DOMNode $node) {
+
+    // save the text xml currently in the node (to be converted)
+    $nodetext = $node->nodeValue;
+    // blank out current text so it can be replaced with the new nodes
+    $node->nodeValue = "";
+
+    // load the xml into a temporary DOM
+    $tmpdoc = new DOMDocument();
+    $tmpdoc->loadXML("<doc>" . $nodetext . "</doc>");
+
+    // append all nodes under top-level doc to the node being converted
+    if ($tmpdoc->documentElement->hasChildNodes()) {
+      for ($i = 0; $i < $tmpdoc->documentElement->childNodes->length; $i++) {
+	$newnode = $node->ownerDocument->importNode($tmpdoc->documentElement->childNodes->item($i), true);
+	$node->appendChild($newnode);
       }
     }
+    
     return $node;
   }
 
