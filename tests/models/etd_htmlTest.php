5e1e9d0fa33ac753b719325515f034da3b422c31
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2008-07-22 15:32:32 +0000

    generalized tests to work with new dom/tidy html clean-up; #328, #329

--- tests/models/etd_htmlTest.php
+++ tests/models/etd_htmlTest.php
@@ -29,23 +29,21 @@ class TestEtdHtml extends UnitTestCase {
 
   function testCleanTags() {
     // unnecessary entities
-    $this->assertEqual("\"they're great! -\"",
+    $this->assertPattern("|\s*\"they're great! -\"|",
 		       etd_html::cleanTags("&lt;b&gt;&ldquo;they&rsquo;re great!&nbsp;&ndash;&rdquo;"));
-    $this->assertEqual("text",
-		       etd_html::cleanTags("<p class='MSONORMAL'>text</p>"));
+    //    $this->assertEqual("text",
+    //		       etd_html::cleanTags("<p class='MSONORMAL'>text</p>"));
 
-    $this->assertEqual("first line second line",
-		       etd_html::cleanTags("first line<br/> second line"));
-    $this->assertEqual("first line<br/> second line",
-		       etd_html::cleanTags("first line<br/> second line", true));
+    $this->assertPattern("|first line\s*second line|",
+			 etd_html::cleanTags("first line<br/> second line"));
+    $this->assertPattern("|first line\s*<br/>\s*second line|",
+			 etd_html::cleanTags("first line<br/> second line", true));
 
-    $this->assertEqual("first line<br/> second line",
-		       etd_html::cleanTags("first line<br clear='all'/> second line", true));
-
+    $this->assertPattern("|first line\s*<br clear=['\"]all['\"]\s*/>\s*second line|",
+			 etd_html::cleanTags("first line<br clear='all'/> second line", true));
     
     $this->assertEqual("some text",
 		       etd_html::cleanTags("<strong/>some text", true));
-    
   }
 
   function testRemoveTags() {
@@ -83,25 +81,25 @@ class TestEtdHtml extends UnitTestCase {
 
     // tags all at same level
     $this->etd_html->contents = "<b>chapter 1</b> <br/> <i>chapter 2</i>";
-    $this->assertEqual("<b>chapter 1</b> <br/> <i>chapter 2</i>", $this->etd_html->contents);
+    $this->assertPattern("|\s*<b>chapter 1</b>\s*<br/>\s*<i>chapter 2</i>|", $this->etd_html->contents);
 
     // one tag inside another 
     $this->etd_html->contents = "<p>chapter 1 <br/> chapter 2</p>";
-    $this->assertEqual("<p>chapter 1 <br/> chapter 2</p>", $this->etd_html->contents);
+    $this->assertPattern("|\s*<p>chapter 1\s*<br/>\s*chapter 2</p>|", $this->etd_html->contents);
 
     // tags nested two levels deep
     $this->etd_html->contents = "<p>chapter <font>1 <br/> ch</font>apter 2</p>";
-    $this->assertEqual("<p>chapter <font>1 <br/> ch</font>apter 2</p>", $this->etd_html->contents);
+    $this->assertPattern("|\s*<p>chapter 1\s*<br/>\s*chapter 2</p>|", $this->etd_html->contents);
 
     // bad xml: close tag without open tag (should be ignored)
     $this->etd_html->abstract = "<p>here is my </em> whoops</p>";
-    $this->assertEqual("<p>here is my  whoops</p>", $this->etd_html->abstract);
+    $this->assertPattern("|<p>here is my\s+whoops</p>|", $this->etd_html->abstract);
 
     // node following directly after another one (ticket:102)
     $this->etd_html->abstract = '<div><b>Background:</b> lots of text here.</div>';
-    $this->assertEqual('<div><b>Background:</b> lots of text here.</div>', $this->etd_html->abstract);
+    $this->assertPattern('|<div>\s*<b>Background:</b> lots of text here.</div>|', $this->etd_html->abstract);
  
-    // quote entities (also ticket:102)
+    // quote entities (see ticket:102)
     $this->etd_html->abstract = 'I have some &quot;text&quot; in quotes.';
     $this->assertEqual('I have some "text" in quotes.', $this->etd_html->abstract);
  
@@ -117,6 +115,18 @@ class TestEtdHtml extends UnitTestCase {
     // losing Greek characters
     $this->etd_html->abstract = 'character &beta; mid-sentence';
     $this->assertEqual('character &#x3B2; mid-sentence', $this->etd_html->abstract);
+
+    // named entities can't be saved - should be converted to numeric
+    $this->etd_html->abstract = 'named entity &eacute; character';
+    $this->assertEqual('named entity &#xE9; character', $this->etd_html->abstract);
+
+    
+    // indentation getting stripped out
+    $this->etd_html->contents = "<p style='margin-left:40px;'>indented paragraph</p><p>second paragraph</p>";
+    $this->assertPattern("|<p style=['\"]margin-left:40px;['\"]>indented paragraph</p>|", $this->etd_html->contents); 
+
+
+    
   }
 
   public function testSetContentsWithWhitespace() {
