0eb1db0f55102f7811a930dc866a4bfb9e26e4aa
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2008-07-24 21:17:26 +0000

    restrict indexed fields to elements with PQ numeric ids  #236

--- trunk/src/app/models/researchfields.php
+++ trunk/src/app/models/researchfields.php
@@ -3,6 +3,8 @@
 require_once("skosCollection.php");
 
 class researchfields extends collectionHierarchy  {
+  protected $collection_class = "researchfieldCollection";
+    
   public function __construct($id = "#researchfields") {
     $xml = file_get_contents("umi-researchfields.xml", FILE_USE_INCLUDE_PATH); 
     $dom = new DOMDocument();
@@ -11,4 +13,25 @@ class researchfields extends collectionHierarchy  {
 
     $this->index_field = "subject_facet";
   }
+}
+
+/* Using custom researchfield collection & member classes in order to
+   customize indexed function used to retrieve all indexed fields for
+   generating Solr query
+*/
+
+class researchfieldCollection extends skosCollection {
+  protected $member_class = "researchfieldMember";
+}
+
+class researchfieldMember extends skosMember {
+  protected $collection_class = "researchfieldCollection";
+
+  protected function isIndexed() {
+    // only actual UMI fields with numeric codes are indexed
+    if (preg_match("/#[0-9]{4}/", $this->id)) return true;
+    
+    return false; 
+  }
+
 }
