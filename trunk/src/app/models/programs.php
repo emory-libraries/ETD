3adb3e7180d56a33b92d2a9400fe6eac69f9918e
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2008-07-24 21:17:00 +0000

    restrict indexed fields to bottom-level elements or known subfields  #236

--- trunk/src/app/models/programs.php
+++ trunk/src/app/models/programs.php
@@ -3,6 +3,7 @@
 require_once("skosCollection.php");
 
 class programs extends collectionHierarchy  {
+  protected $collection_class = "programCollection";
   
   public function __construct($id = "#programs") {
     
@@ -14,4 +15,33 @@ class programs extends collectionHierarchy  {
 
     $this->index_field = "program_facet";
   }
+}
+
+/* Using custom program collection & member classes in order to
+   customize indexed function used to retrieve all indexed fields for
+   generating Solr query.
+*/
+
+
+class programCollection extends skosCollection {
+  protected $member_class = "programMember";
+}
+
+class programMember extends skosMember {
+  protected $collection_class = "programCollection";
+
+  protected function isIndexed() {
+    // bottom level element - field with no subfields or subfield
+    if (count($this->collection->members) == 0) return true;
+
+    /* There is no good way to detect fields with subfields, since
+       member has no access upwards in the hierarchy; looking for ids
+       of known fields with subfields.
+    */
+    if (in_array($this->id, array("#religion", "#biosci", "#psychology"))) return true;
+
+    // otherwise, this item is not indexed
+    return false; 
+  }
+
 }
