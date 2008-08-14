d0db9cc305c27c63e51287b88aaf2175522009c7
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2008-08-14 18:27:35 +0000

    comments for default facet settings

--- src/lib/Etd/Service/Solr.php
+++ src/lib/Etd/Service/Solr.php
@@ -19,7 +19,7 @@ class Etd_Service_Solr extends Emory_Service_Solr {
   // convenience function to limit query to published records only 
   public function queryPublished($queryString, $start = null, $max = null, $sort = null) {
     $this->addFilter("status:published");
-    $this->facet_limit = 5;
+    $this->facet_limit = 5;	// only display top 5 facets
     // FIXME: minimum count ?    
     return $this->query($queryString, $start, $max, $sort);
   }
