d5726b676edadeec9e2212df6b887b3094fa79f2
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2008-08-18 17:03:29 +0000

    adds program coordinator & faculty views, facets, logging, co-chair support, inactive status, EtdSet object, more unit tests

---
+++ tags/v0.9.4/src/app/controllers/UserController.php
@@ -0,0 +1,158 @@
+<?php
+
+require_once("models/user.php");
+
+class UserController extends Etd_Controller_Action {
+  protected $requires_fedora = true;
+  
+  public function viewAction() {
+    $user = $this->_helper->getFromFedora("pid", "user");
+    if (!$this->_helper->access->allowedOnUser("view", $user)) return false;
+
+    $this->view->title = "View User Information";
+    $this->view->user = $user;
+  }
+
+  // create a new user record
+  public function newAction() {
+    if (!$this->_helper->access->allowedOnUser("create")) return false;
+    $this->_forward("edit");
+  }
+
+  public function findAction() {
+    // FIXME: still needed? may not make sense anymore..
+    $user = user::find_by_username($this->_getParam("id"));
+    $this->view->user = $user;
+    //    print "user pid is " . $user->pid . "\n";
+    $this->_helper->viewRenderer->setScriptAction("view");
+  }
+
+
+  // edit or create (if pid is not specified, creates a new recodr);
+  public function editAction() {
+    // if pid is null, create an empty user object
+    $pid = $this->_getParam("pid", null);
+
+    // etd record the user object should belong to (required)
+    if (!$this->_hasParam("etd")) throw new Exception("Required parameter 'etd' is not set");
+    $this->view->etd_pid = $this->_getParam("etd");	
+    
+    if (is_null($pid))	{ // if pid is null, action is actually create (user is not author on an object yet)
+      $user = new user();	// create new empty user object
+      if (!$this->_helper->access->allowedOnUser("create")) return false;
+    } else { 		// if pid is defined, action is editing an existing object
+      $user = $this->_helper->getFromFedora("pid", "user");	  
+      if (!$this->_helper->access->allowedOnUser("edit", $user)) return false;
+    }
+    $this->view->user = $user;
+
+    $this->view->title = "Edit User Information";
+
+    // xforms setting - so layout can include needed code in the header
+    $this->view->xforms = true;
+    $this->view->xforms_bind_script = "user/_mads_bind.phtml";
+    $this->view->namespaces = array("mads" => "http://www.loc.gov/mads/");
+    // link to xml rather than embedding directly in the page
+
+    $this->view->xforms_model_uri = $this->view->url(array("controller" => "user",
+							   "action" => "mads", "pid" => $pid), '', true, false);
+  }
+
+
+  public function saveAction() {
+    $pid = $this->_getParam("pid", null);
+    // should only be set for new records 
+    $etd_pid = $this->_getParam("etd", null);
+    
+    $user = new user($pid);
+    if (is_null($pid))	{ // if pid is null, action is actually create (user is not author on an object yet)
+      if (!$this->_helper->access->allowedOnUser("create")) return false;
+    } else { 		// if pid is defined, action is editing an existing object
+      if (!$this->_helper->access->allowedOnUser("edit", $user)) return false;
+    }
+
+    global $HTTP_RAW_POST_DATA;
+    $xml = $HTTP_RAW_POST_DATA;
+    if ($xml == "") {
+      // if no xml is submitted, don't modify 
+      // forward to a different view?
+      $this->view->noxml = true;
+    } else {
+
+      if (is_null($pid)) {
+	// new record - set object label & dc:title, object owner
+	$dom = new DOMDocument();
+	$dom->loadXML($xml);
+	$mads = new mads($dom);
+	$user->label = $mads->name->first . " " . $mads->name->last;
+	$user->owner = $mads->netid;
+	$user->rels_ext->addRelationToResource("rel:authorInfoFor", $etd_pid);
+      }
+      
+      $user->mads->updateXML($xml);
+
+      // if no date for current, set to today
+      if (!$user->mads->current->date) $user->mads->current->date = date("Y-m-d");	
+      // normalize date format
+      $user->normalizeDates();
+      
+      $resource = "contact information";
+      if ($user->mads->hasChanged()) {
+	try {
+	  $save_result = $user->save("edited user information");
+	} catch (PersisServiceUnavailable $e) {
+	  // this can only happen when saving a new record (ingest)
+	  $this->_helper->flashMessenger->addMessage("Error: could not create new record because Persistent Identifier Service is not available");
+	  // redirect to an error page
+	  $this->_helper->redirector->gotoRouteAndExit(array("controller" => "error", "action" => "unavailable"));
+	}
+	if ($save_result)
+	  $this->_helper->flashMessenger->addMessage("Saved changes to $resource");
+	else
+	  $this->_helper->flashMessenger->addMessage("Error: could not save changes to $resource");
+      } else {
+	$this->_helper->flashMessenger->addMessage("No changes made to $resource");
+      }
+
+    }
+
+    if ($etd_pid) {
+      $etd = new etd($etd_pid);
+      $etd->rels_ext->addRelationToResource("rel:hasAuthorInfo", $user->pid);
+      $save_result = $etd->save("associated user object with etd");
+      // only display a message if there is a problem
+      if (!$save_result)  // record changed but save failed for some reason
+	$this->_helper->flashMessenger->addMessage("Error: problem associating contact information with your ETD record");
+    }
+
+    //redirect to user view
+    $this->_helper->redirector->gotoRoute(array("controller" => "user",
+    						"action" => "view", "pid" => $user->pid), "", true);
+
+    
+    $this->view->pid = $user->pid;
+    $this->view->xml = $xml;
+    $this->view->title = "save user information";
+   }
+
+  // show mads xml - referenced as model for xform
+   public function madsAction() {
+    // if pid is null, display template xml with netid set to id for current user
+     $pid = $this->_getParam("pid", null);
+
+     if (is_null($pid))	{ // if pid is null, action is actually create (user is not author on an object yet)
+       $user = new user($pid);	// create new object from template
+       if (!$this->_helper->access->allowedOnUser("create")) return false; 
+       
+       // empty template - set a few values
+       $user->mads->initializeFromEsd($this->view->current_user);
+     } else { 		// if pid is defined, action is viewing an existing object
+       $user = $this->_helper->getFromFedora("pid", "user");	  
+       if (!$this->_helper->access->allowedOnUser("view", $user)) return false;
+     }
+     $this->view->user = $user;
+
+     $this->_helper->displayXml($user->mads->saveXML());
+   }
+  
+}
---
+++ tags/v0.9.4/tests/controllers/TestSubmissionController.php
@@ -0,0 +1,152 @@
+<?
+
+require_once("../bootstrap.php");
+require_once('ControllerTestCase.php');
+require_once('controllers/SubmissionController.php');
+      
+class SubmissionControllerTest extends ControllerTestCase {
+
+  // array of test foxml files & their pids 
+  private $etdxml;
+  private $test_user;
+  
+  function setUp() {
+    $this->test_user = new esdPerson();
+    $this->test_user->role = "student";
+    $this->test_user->netid = "author";
+    $this->test_user->name = "Author";
+    $this->test_user->lastname = "Jones";
+    
+    $_GET 	= array();
+    $_POST	= array();
+    
+    $this->response = $this->makeResponse();
+    $this->request  = $this->makeRequest();
+
+    $this->etdxml = array("etd1" => "test:etd1",
+			  "etd2" => "test:etd2",
+			  "user" => "test:user1",
+			  //"etd3" => "test:etd3",	// not working for some reason
+			  );
+    
+
+    // load a test objects to repository
+    // NOTE: for risearch queries to work, syncupdates must be turned on for test fedora instance
+    foreach (array_keys($this->etdxml) as $etdfile) {
+      $pid = fedora::ingest(file_get_contents('../fixtures/' . $etdfile . '.xml'), "loading test etd");
+    }
+
+  }
+  
+  function tearDown() {
+    foreach ($this->etdxml as $file => $pid)
+      fedora::purge($pid, "removing test etd");
+    
+    Zend_Registry::set('current_user', null);
+  }
+
+
+  function testStartAction() {
+    $this->test_user->role = "staff";	// set to non-student
+    Zend_Registry::set('current_user', $this->test_user);
+    $SubmissionController = new SubmissionControllerForTest($this->request,$this->response);
+    $this->assertFalse($SubmissionController->startAction());
+    // should not be allowed (not a student)
+
+    // student who already has a submission 
+    $this->test_user->role = "student with submission";
+    Zend_Registry::set('current_user', $this->test_user);
+    // should not be allowed to create a new submission
+    $this->assertFalse($SubmissionController->startAction());
+
+    // student
+    $SubmissionController = new SubmissionControllerForTest($this->request,$this->response);
+    $this->test_user->role = "student";
+    Zend_Registry::set('current_user', $this->test_user);
+    $SubmissionController->startAction();
+    $this->assertFalse($SubmissionController->redirectRan);
+  }
+
+  /* FIXME: not sure how to test processPdf action - LOTS of stuff going on, hard to simulate... */
+
+
+  function testReviewAction() {
+    $this->test_user->role = "student";	// set to non-student
+    Zend_Registry::set('current_user', $this->test_user);
+    $SubmissionController = new SubmissionControllerForTest($this->request,$this->response);
+
+    $this->setUpGet(array('pid' => 'test:etd2'));	   // reviewed etd
+    // etd is wrong status, should not be allowed
+    $this->assertFalse($SubmissionController->reviewAction());
+    $this->assertFalse(isset($SubmissionController->view->etd));
+
+    // set status to draft so it can be reviewed
+    $etd = new etd("test:etd2");
+    $etd->setStatus("draft");
+    $etd->save("setting status to draft to test review");
+    
+    $SubmissionController = new SubmissionControllerForTest($this->request,$this->response);
+    $SubmissionController->reviewAction();
+    $this->assertFalse($SubmissionController->redirectRan);
+    $this->assertTrue(isset($SubmissionController->view->etd), "etd variable set for review");
+    $messages = $SubmissionController->getHelper('FlashMessenger')->getMessages();
+  }
+
+  function testSubmitAction() {
+    $this->test_user->role = "student";	// set to non-student
+    Zend_Registry::set('current_user', $this->test_user);
+    $SubmissionController = new SubmissionControllerForTest($this->request,$this->response);
+
+    $this->setUpGet(array('pid' => 'test:etd2'));	   // reviewed etd
+    // etd is wrong status, should not be allowed
+    $this->assertFalse($SubmissionController->submitAction());
+    $etd = new etd("test:etd2");
+    $this->assertEqual("reviewed", $etd->status(), "status unchanged");	
+
+    // set status to draft so it can be submitted
+    $etd = new etd("test:etd2");
+    $etd->setStatus("draft");
+    $etd->save("changing status to test submit");
+
+    // notices for non-existent users in metadata
+    $this->expectError("Committee member/chair (nobody) not found in ESD");
+    $this->expectError("Committee member/chair (nobodytoo) not found in ESD");
+    $SubmissionController->submitAction();
+    $etd = new etd("test:etd2");
+    $this->assertEqual("submitted", $etd->status());
+    $this->assertTrue($SubmissionController->redirectRan);	// currently redirects to my etds page...
+    $messages = $SubmissionController->getHelper('FlashMessenger')->getMessages();
+    $this->assertPattern("/status changed/", $messages[0]);
+    $this->assertPattern("/Submission notification email sent to mmouse@emory.edu, mmouse@disney.com/",
+	 $messages[1]);	        // 	   current & permanent email addresses in user information
+    $this->assertEqual("Submitted for Approval by " . $this->test_user->fullname, $etd->premis->event[1]->detail);
+    $this->assertEqual("author", $etd->premis->event[1]->agent->value);
+
+    // NOTE: currently no way to test generation of the email, but it may be added in the future
+  }
+
+
+}
+
+
+class SubmissionControllerForTest extends SubmissionController {
+  
+  public $renderRan = false;
+  public $redirectRan = false;
+  
+  public function initView() {
+    $this->view = new Zend_View();
+    Zend_Controller_Action_HelperBroker::addPrefix('TestEtd_Controller_Action_Helper');
+  }
+  
+  public function render() {
+    $this->renderRan = true;
+  }
+  
+  public function _redirect() {
+    $this->redirectRan = true;
+  }
+} 	
+
+runtest(new SubmissionControllerTest());
+?>
