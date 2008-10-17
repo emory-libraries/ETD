5614f1b5d297fbc73d4be7fcd0d3cf771b46f721
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2008-10-17 18:53:08 +0000

    modifying FCKeditor to use classes for all formatting to improve handling MS junk tags #355

--- src/public/js/FCKconfig.js
+++ src/public/js/FCKconfig.js
@@ -1,22 +1,33 @@
-// FIXME: universal key doesn't exist anymore?
+// NOTE: using etd.css (loaded via view helper so css url can be generated dynamically)
+FCKConfig.BodyClass = 'fullrecord' ;
+FCKConfig.IndentClasses = [ 'Indent1', 'Indent2', 'Indent3' ] ;
+FCKConfig.JustifyClasses = [ 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyFull' ] ;
 
+
+// load universal key tool for alternate language input
 FCKConfig.Plugins.Add('universalkey');
 
 FCKConfig.ToolbarSets["etd-title"] = [
       ['Preview','-', 'Bold','Italic','Underline','-','Subscript','Superscript','-','Link','Unlink',
-       '-', 'SpecialChar', 'UniversalKey', '-', 'SpellCheck', 'About']
+       '-', 'SpecialChar', 'UniversalKey', 
+       '-', 'Paste', 'PasteText', 'PasteWord',
+       '-', 'SpellCheck', 'About']
 ];
 
 FCKConfig.ToolbarSets["etd"] = [
 	['Preview','-', 'Bold','Italic','Underline','-','Subscript','Superscript','-','Link','Unlink',
-	   '-', 'SpecialChar', 'UniversalKey', '-', 'SpellCheck', 'About'],
+	 '-', 'SpecialChar', 'UniversalKey', 
+	 '-', 'Paste', 'PasteText', 'PasteWord',
+	 '-', 'SpellCheck', 'About'],
 	  '/',
 	  ['OrderedList','UnorderedList','-','Outdent','Indent','Blockquote']
   ];
 /* same as etd + view source */
 FCKConfig.ToolbarSets["etd-admin"] = [
 	['Preview','-', 'Bold','Italic','Underline','-','Subscript','Superscript','-','Link','Unlink',
-	   '-', 'SpecialChar', 'UniversalKey', '-', 'SpellCheck', 'About', '-', 'Source'],
+	 '-', 'SpecialChar', 'UniversalKey',
+	 '-', 'Paste', 'PasteText', 'PasteWord',
+	 '-', 'SpellCheck', 'About', '-', 'Source'],
 	  '/',
 	  ['OrderedList','UnorderedList','-','Outdent','Indent','Blockquote']
   ];
