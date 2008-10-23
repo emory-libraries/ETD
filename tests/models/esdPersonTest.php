da44ef2d88e96c998999dc3d17a6ec42ff57f5e3
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2008-10-23 19:02:32 +0000

    removed problem test for value that changes in ESD

--- tests/models/esdPersonTest.php
+++ tests/models/esdPersonTest.php
@@ -29,7 +29,8 @@ class TestEsdPerson extends UnitTestCase {
     $this->assertEqual($user->department, "University Libraries");
     $this->assertEqual($user->academic_career, "GSAS");
     $this->assertEqual($user->academic_program, null);
-    $this->assertEqual($user->degree, "PHD");	// FIXME: why does this change?
+    // NOTE: this test is useless; value returned by ESD oscillates between MA and PHD for no apparent reason
+    //    $this->assertEqual($user->degree, "PHD");	// FIXME: why does this change?
     $this->assertEqual($user->term_complete, "5061");
     $this->assertEqual($user->email, "rsutton@emory.edu");
     $this->assertEqual($user->academic_plan_id, "ENGLISHMA");
