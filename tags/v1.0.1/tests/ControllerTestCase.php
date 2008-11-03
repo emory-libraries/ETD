4ccf1dedb5c6809d797dc0cadbc5153af9757680
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2008-11-03 21:47:42 +0000

    error message for missing files that should be available - #423, #427

---
+++ tags/v1.0.1/tests/ControllerTestCase.php
@@ -0,0 +1,88 @@
+<?
+require_once("UnitTest.php");  
+ 
+class ControllerTestCase extends UnitTest {
+  protected $request;
+  protected $response;
+
+  
+  protected function makeRequest($url = null) {
+    return new Zend_Controller_Request_Http($url);
+  }
+  
+  protected function makeResponse() {
+    return new TestEtd_Controller_Response_Http();
+  }	
+  
+  protected function setUpPost(array $params = array()) {
+    $_SERVER['REQUEST_METHOD'] = 'POST';
+    foreach($params as $key=>$value){
+      $_POST[$key] = $value;
+    }
+  }
+  
+  protected function setUpGet(array $params = array())  {
+    $_SERVER['REQUEST_METHOD'] = 'GET';
+    foreach($params as $key=>$value){
+      $_GET[$key] = $value;
+    }
+  }
+
+  protected function resetGet(){
+    $_GET = array();
+  }
+}
+
+
+/* bypass the real redirector helper because routes aren't defined */
+class Test_Controller_Action_Helper_Redirector extends Zend_Controller_Action_Helper_Abstract {
+  public function gotoRoute(array $urlOptions = array(), $name = null, $reset = false) {
+    $this->_actionController->_redirect();
+  }
+  public function gotoRouteAndExit(array $urlOptions = array(), $name = null, $reset = false) {
+    $this->gotoRoute($urlOptions, $name, $reset);
+    // no real way to simulate exiting...
+  }
+
+  public function gotoUrl($url, $opts) {
+    $this->_actionController->_redirect();
+  }
+}
+
+/* bypass real flashmessenger helper to simplify; clear out messages once they are retrieved */
+class Test_Controller_Action_Helper_FlashMessenger extends Zend_Controller_Action_Helper_Abstract {
+  private $messages;
+  public function __construct() { $this->messages = array(); }
+  public function addMessage($text) {  $this->messages[] = $text;  }
+  public function getMessages() {
+    $messages = $this->messages;
+    $this->messages = array();		// as a convenience, clear out messages when retrieving them
+    return $messages;
+  }
+}
+
+class Test_Controller_Action_Helper_Layout extends Zend_Controller_Action_Helper_Abstract {
+  public $enabled = true;
+  public $name;
+  public function disableLayout() { $this->enabled = false; }
+  public function setLayout($name) { $this->name = $name; }
+}
+
+class Test_Controller_Action_Helper_GetFromFedora extends Etd_Controller_Action_Helper_GetFromFedora {
+  protected $object = null;
+  public function direct($pid, $type) {
+    if ($this->object == null)  return $this->find_or_error($pid, $type);
+    else return $this->object;
+  }
+  public function setReturnObject($obj) { $this->object = $obj; }
+  public function clearReturnObject() { $this->object = null; }
+}
+
+
+
+// override http response class to allow setting headers (otherwise not allowed because of simpletest headers)
+class TestEtd_Controller_Response_Http extends Zend_Controller_Response_Http {
+  public function canSendHeaders() { return true; }
+}
+
+?>
---
+++ tags/v1.0.1/tests/lib/TestGetFromFedora.php
@@ -0,0 +1,110 @@
+<?php
+require_once("../bootstrap.php");
+
+class TestGetFromFedora extends ControllerTestCase {
+  
+  private $helper;
+  private $controller;
+
+  private $_fedora;
+  private $mock_fedora;
+
+  function setUp() {
+    $_GET = array();
+    $_POST = array();
+    
+    $this->response = $this->makeResponse();
+    $this->request  = $this->makeRequest();
+
+    $this->controller = new ControllerForTest($this->request,$this->response);
+    $this->helper = $this->controller->getHelper("GetFromFedora");
+
+    $this->_fedora = Zend_Registry::get('fedora');
+    $this->mock_fedora = new MockFedoraConnection();
+    Zend_Registry::set('fedora', $this->mock_fedora);
+  }
+  
+  function tearDown() {
+    $this->resetGet();
+    Zend_Registry::set('fedora', $this->_fedora);
+  }
+
+  function testDirect(){
+    // no value for id param
+    $this->assertNull($this->helper->direct("id", "etd"));
+    $messages = $this->controller->getHelper("flashMessenger")->getMessages();
+    $this->assertEqual("Error: No record specified for etd", $messages[0]);
+
+    // simulate retriving an etd object from Fedora
+    $this->setUpGet(array("id" => "test:1"));
+    // simulated etd object has no rels_ext; ignore the error
+    $this->expectError("Object does not have configured datastream: rels_ext");
+    $result = $this->helper->direct("id", "etd");
+    $this->assertIsA($result, "etd");
+    
+    
+    // test various exceptions
+    // - not found
+    $this->mock_fedora->setException("NotFound");
+    $this->helper->direct("id", "etd");
+    $messages = $this->controller->getHelper("flashMessenger")->getMessages();
+    $this->assertPattern("/Record not found/", $messages[0]);
+    // - access denied
+    $this->setUpGet(array("id" => "test:1"));
+    $this->mock_fedora->setException("AccessDenied");
+    $this->helper->direct("id", "etd");
+    $response = $this->controller->getResponse();
+    $this->assertEqual("403", $response->getHttpResponseCode());
+    $this->assertPattern('/Permission Denied/', $response->getBody());
+    // - not authorized
+    $this->setUpGet(array("id" => "test:1"));
+    $this->mock_fedora->setException("NotAuthorized");
+    $this->helper->direct("id", "etd");
+    $response = $this->controller->getResponse();
+    $this->assertEqual("403", $response->getHttpResponseCode());
+    $this->assertPattern('/Permission Denied/', $response->getBody());
+    // - generic foxml exception
+    $this->setUpGet(array("id" => "test:1"));
+    $this->mock_fedora->setException("generic");
+    $this->helper->direct("id", "etd");
+    $response = $this->controller->getResponse();
+    $this->assertEqual("403", $response->getHttpResponseCode());
+    $this->assertPattern('/Permission Denied/', $response->getBody());
+    
+    //    $this->assertPattern("/Record not found/", $messages[0]);
+    
+  }
+
+  // other cases to test:
+  // FIXME: how to get a mock object to throw an exception ?
+  // exceptions:
+  //  - FedoraObjectNotFound 
+  //  - FedoraAccessDenied
+  //  - FedoraNotAuthorized
+  //  - FoxmlException
+
+}
+
+
+class ControllerForTest extends Etd_Controller_Action {
+  public $renderRan = false;
+  public $redirectRan = false;
+  
+  public function initView() {
+    $this->view = new Zend_View();
+    Zend_Controller_Action_HelperBroker::addPrefix('Test_Controller_Action_Helper');
+  }
+  
+  public function render() {
+    $this->renderRan = true;
+  }
+  
+  public function _redirect() {
+    $this->redirectRan = true;
+  }
+} 	
+
+
+
+runtest(new TestGetFromFedora());
+
