5daa6403e8b6022c97b11f39c51e2c721ef13859
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2008-11-19 19:30:08 +0000

    allow faculty to submit records; find & display submissions where faculty user is owner/author - #430

---
+++ tags/v1.0.3/src/lib/xml_acl.php
@@ -0,0 +1,49 @@
+<?php
+
+class Xml_Acl extends Zend_Acl {
+
+    public function __construct($filename = "../config/access.xml") {
+      //      $config = simplexml_load_file($filename);
+      $config = simplexml_load_string(file_get_contents($filename));
+
+    //roles
+    foreach ($config->roles->role as $role) {
+      $inherits = array();
+      if ($role{"inherits"}) {
+	array_push($inherits, (string)$role{"inherits"});
+      }
+      
+      $this->addRole(new Zend_Acl_Role((string)$role{"name"}), $inherits);
+    }
+    
+    //resources
+    foreach ($config->resources->resource as $resource) {
+      $this->add(new Zend_Acl_Resource((string)$resource{"name"}));
+      foreach ($resource->resource as $subresource) {
+	$this->add(new Zend_Acl_Resource((string)$subresource{"name"}),
+		  (string)$resource{"name"});
+      }
+    }
+
+    //privileges by role
+    foreach ($config->roles->role as $role) {
+      foreach ($role->allow as $allow) {
+	if ($allow == "") {
+	  if (isset($allow{"resource"}))
+	    $this->allow((string)$role{"name"}, (string)$allow{"resource"});	// anything on this resource
+	  else
+	    $this->allow((string)$role{"name"});	// anything anywhere
+	}
+	else 
+	  $this->allow((string)$role{"name"}, (string)$allow{"resource"},
+		     explode(", ", (string)$allow));
+      }
+      // fixme: probably should handle deny also 
+    }
+  }
+
+  
+}
+
+
+?>
