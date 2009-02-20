aeba1c99bb24c15f22b42297559795e8ac1587a6
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2009-02-20 19:20:56 +0000

    test that custom types are initialized properly (mods, authorinfo)  #463

--- branches/undergrad_honors/tests/models/honorsEtdTest.php
+++ branches/undergrad_honors/tests/models/honorsEtdTest.php
@@ -17,7 +17,9 @@ class TestHonorsEtd extends UnitTestCase {
   
   function testBasicProperties() {
     $this->assertIsA($this->etd, "honors_etd");
-    $this->assertIsA($this->etd, "etd");
+    $this->assertIsA($this->etd, "etd");		// inherits 
+    $this->assertIsA($this->etd->mods, "honors_mods");
+    $this->assertIsA($this->etd->mods, "etd_mods");	// inherits 
   }
 
   function testInitializeByTemplate() {
@@ -28,6 +30,30 @@ class TestHonorsEtd extends UnitTestCase {
     $this->assertPattern('|<rel:isMemberOf.*rdf:resource="info:fedora/' .
 			 $config->honors_collection . '".*/>|',
 			 $etd->rels_ext->saveXML());
+  }
+
+  function testAuthorInfo() {
+
+    // Need an etd and related authorInfo object both loaded to fedora
+    $fedora = Zend_Registry::get("fedora");
+    
+    // load test objects to repository
+    $etdpid = $fedora->ingest(file_get_contents('../fixtures/etd2.xml'),
+			      "loading test etd");
+    $userpid = $fedora->ingest(file_get_contents('../fixtures/user.xml'),
+			       "loading test user");
+    $etd = new etd($etdpid);
+    $user = new user($userpid);
+    // add relation between objects  
+    $etd->rels_ext->addRelationToResource("rel:hasAuthorInfo", $userpid);
+
+
+    // NOW: check that user object is initialized properly
+    $etd = new honors_etd($etdpid);
+    $this->assertIsA($etd->authorInfo, "honors_user");
+
+    $fedora->purge($etdpid, "removing test etd");
+    $fedora->purge($userpid, "removing test user");
   }
   
 }
