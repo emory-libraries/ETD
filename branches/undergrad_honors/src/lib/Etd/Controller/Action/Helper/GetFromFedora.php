d81a8967718a23d3e84db054085b7620521f4a90
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2009-02-24 23:18:45 +0000

    if requested type is etd, use factory init #465

--- branches/undergrad_honors/src/lib/Etd/Controller/Action/Helper/GetFromFedora.php
+++ branches/undergrad_honors/src/lib/Etd/Controller/Action/Helper/GetFromFedora.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once("EtdFactory.php");
+
+
 class Etd_Controller_Action_Helper_GetFromFedora extends Zend_Controller_Action_Helper_Abstract {
 
   public function direct($param, $type) {
@@ -32,7 +35,12 @@ class Etd_Controller_Action_Helper_GetFromFedora extends Zend_Controller_Action_
     }
 
     try {
-      $object = new $type($id);
+      if ($type == "etd") {
+	// if requested type is etd, use factory method to initialize correct type
+	$object = EtdFactory::etdByPid($id);
+      } else {
+	$object = new $type($id);
+      }
     } catch (FedoraObjectNotFound $e) {
       $message = "Record not found";
       $log_message = $message . " - " . $e->getMessage();
