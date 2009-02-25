7be6ef99e5e23bedac0d7ba76a8515bb38e2e55c
lhan4 <lhan4@affb5729-703e-0410-bbc3-960ba87530bd>
2009-02-25 20:30:13 +0000

    message.log

--- branches/undergrad_honors/src/app/models/honors_user.php
+++ branches/undergrad_honors/src/app/models/honors_user.php
@@ -1,6 +1,25 @@
+
 <?php
 
 require_once("user.php");
 
 class honors_user extends user {
+
+public function checkRequired() {
+    $missing = array();
+
+    // permanent non-emory email address
+    if ($this->mads->permanent->email == "") {
+      $missing[] = "permanent (non-emory) email address";
+    }
+
+	 // current email address
+    if ($this->mads->current->email == "") {
+      $missing[] = "current email address";
+    }
+
+
+    return $missing;
+  }
+
 }
