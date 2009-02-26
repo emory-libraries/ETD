d5fbb3978db3d708c30c6f72419893cc6d34ad66
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2009-02-26 21:45:54 +0000

    customized required fields for honors mods #466

--- branches/undergrad_honors/src/app/models/honors_mods.php
+++ branches/undergrad_honors/src/app/models/honors_mods.php
@@ -5,4 +5,29 @@ require_once("etd_mods.php");
 class honors_mods extends etd_mods {
 
 
+  // add etd-specific mods mappings
+  protected function configure() {
+    parent::configure();
+
+
+    // override required fields - shares etd fields with these exceptions:
+    // PQ researchfields are optional
+    unset($this->required_fields["researchfields"]);
+    // Honors ETDs are not sent to PQ
+    unset($this->required_fields["send to ProQuest"]);
+    unset($this->required_fields["copyright"]);
+  }
+    
+  /* NOTE: functions for checking required and complete fields are
+     inherited-- the logic is the same, and actual tests are based on
+     the list of required fields. */
+
+  /**
+   * check if the record should  be sent to ProQuest-- never, for honors etds
+   * @return boolean
+   */
+  function submitToProquest() {
+    return false;
+  }
+
 }
