f3d18b3c772f7ebdec06c66f248c8a81caa62301
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2009-03-02 23:10:09 +0000

    added tests for modifying programs in fedora #474

--- branches/undergrad_honors/tests/xacml/programXacmlTest.php
+++ branches/undergrad_honors/tests/xacml/programXacmlTest.php
@@ -13,6 +13,9 @@ require_once('models/etd.php');
 
 class TestProgramXacml extends UnitTestCase {
   private $pid;
+  private $dsid;
+
+  private $fedora;
 
   /**
    * FedoraConnection with default test user credentials
@@ -22,23 +25,50 @@ class TestProgramXacml extends UnitTestCase {
   function setUp() {
     $config = Zend_Registry::get("config");
     $this->pid = $config->programs_pid;
+    $this->dsid = "SKOS";
+    
     if (!isset($this->fedoraAdmin)) {
       $fedora_cfg = Zend_Registry::get('fedora-config');
       $this->fedoraAdmin = new FedoraConnection($fedora_cfg->user, $fedora_cfg->password,
 			       $fedora_cfg->server, $fedora_cfg->port);
     }
+
+    $this->fedora = Zend_Registry::get("fedora");
   }
 
 
-  function testGuestCanView() {
+  function testGuest() {
     // use guest account to access fedora
     setFedoraAccount("guest");
-    $fedora = Zend_Registry::get("fedora");
 
-    $xml = $fedora->getDatastream($this->pid, "SKOS");
+    // guest can view data
+    $xml = $this->fedora->getDatastream($this->pid, $this->dsid);
     $this->assertNotNull($xml);
 
+    // cannot modify
+    $this->expectException(new FedoraAccessDenied("modify datastream - " . $this->pid
+					      . "/" . $this->dsid));
+    $result = $this->fedora->modifyXMLDatastream($this->pid, $this->dsid, "program hierarchy",
+					   "<rdf:RDF/>", "testing modify");
+    // no result from attempted modify
+    $this->assertNull($result, "xacml does not allow guest to modify programs");
   }
+
+  function testModify() {
+    setFedoraAccount("etdadmin");
+    
+    $xml = $this->fedora->getDatastream($this->pid, $this->dsid);
+    $this->assertNotNull($xml);
+    
+    // *can* modify
+    $result = $this->fedora->modifyXMLDatastream($this->pid, $this->dsid, "program hierarchy",
+					   $xml, "modify as etdadmin");
+    $this->assertNotNull($result, "xacml allows etdadmin to modify programs");
+
+    // FIXME: how to we keep from messing up the test programs instance by changing it?!? 
+    
+  }
+   
 
 }
 
