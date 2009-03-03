0dfcebed05d8f0475b381c4013f0faa9de063d6e
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2009-03-03 21:34:42 +0000

    added new honors etd status variants to acl & honors admin privileges on them #483

--- branches/undergrad_honors/tests/lib/AclTest.php
+++ branches/undergrad_honors/tests/lib/AclTest.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once("../bootstrap.php"); 
-require_once('xml_acl.php');
 
 class TestXmlAcl extends UnitTestCase {
     private $acl;
@@ -24,12 +23,23 @@ class TestXmlAcl extends UnitTestCase {
     $this->assertTrue($this->acl->has("etd"));
     $this->assertTrue($this->acl->has("draft etd"));
     $this->assertTrue($this->acl->has("published etd"));
+    
+    $this->assertTrue($this->acl->has("honors etd"), "resource 'honors etd' is defined");
+    $this->assertTrue($this->acl->has("draft honors etd"),
+		      "resource 'draft honors etd' is defined");
+    $this->assertTrue($this->acl->has("published honors etd"),
+		      "resource 'published honors etd' is defined");
   }
 
   function testInheritance() {
     $this->assertTrue($this->acl->inherits("published etd", "etd"));
     $this->assertTrue($this->acl->inherits("draft etd", "etd"));
     $this->assertFalse($this->acl->inherits("etd", "draft etd"));
+
+    $this->assertTrue($this->acl->inherits("honors etd", "etd"));
+    $this->assertTrue($this->acl->inherits("draft honors etd", "draft etd"));
+    // FIXME: how can both of these be true?  Thought Zend Acl was more limited..
+    $this->assertTrue($this->acl->inherits("draft honors etd", "honors etd"));
   }
 
   function testGuest() {
@@ -45,6 +55,10 @@ class TestXmlAcl extends UnitTestCase {
 
     $this->assertFalse($this->acl->isAllowed("guest", "file", "view modified"));
 
+    $this->assertTrue($this->acl->isAllowed("guest", "published honors etd", "view metadata"));
+    $this->assertTrue($this->acl->isAllowed("guest", "published honors etd", "view statistics"));
+    $this->assertFalse($this->acl->isAllowed("guest", "published honors etd", "view history"));
+    $this->assertFalse($this->acl->isAllowed("guest", "honors etd", "edit metadata"));
   }
 
   function testAuthor() {
@@ -61,6 +75,8 @@ class TestXmlAcl extends UnitTestCase {
     $this->assertFalse($this->acl->isAllowed("author", "etd", "edit metadata"));
     $this->assertFalse($this->acl->isAllowed("author", "etd", "add file"));
 
+    $this->assertTrue($this->acl->isAllowed("author", "draft honors etd", "edit metadata"));
+    $this->assertTrue($this->acl->isAllowed("author", "honors etd", "view metadata"));
   }
 
   function testCommittee() {
@@ -72,6 +88,11 @@ class TestXmlAcl extends UnitTestCase {
     
     $this->assertFalse($this->acl->isAllowed("committee", "etd", "edit metadata"));
     $this->assertFalse($this->acl->isAllowed("committee", "etd", "add file"));
+
+    $this->assertTrue($this->acl->isAllowed("committee", "honors etd", "view metadata"));
+    $this->assertTrue($this->acl->isAllowed("committee", "honors etd", "view history"));
+    $this->assertTrue($this->acl->isAllowed("committee", "honors etd", "view status"));
+    
   }
 
   function testAdmin() {
@@ -97,6 +118,48 @@ class TestXmlAcl extends UnitTestCase {
     $this->assertFalse($this->acl->isAllowed("admin", "etd", "inactivate"));
 
     $this->assertFalse($this->acl->isAllowed("admin", "log", "view"));
+  }
+
+  function testHonorsAdmin() {
+    $this->assertTrue($this->acl->isAllowed("honors admin", "honors etd", "view metadata"),
+		      'honors admin can view metadata on honors etd');
+    $this->assertTrue($this->acl->isAllowed("honors admin", "honors etd", "view status"),
+		      'honors admin can view status on honors etd');
+    $this->assertTrue($this->acl->isAllowed("honors admin", "honors etd", "view history"),
+		      'honors admin can view history on honors etd');
+    $this->assertTrue($this->acl->isAllowed("honors admin", "file", "download"),
+		      'honors admin can download file');
+    $this->assertTrue($this->acl->isAllowed("honors admin", "file", "view modified"),
+		      'honors admin can view modified on file');
+    $this->assertTrue($this->acl->isAllowed("honors admin", "honors etd", "edit history"),
+		      'honors admin can edit history on honors honors etd');
+    $this->assertTrue($this->acl->isAllowed("honors admin", "honors etd", "edit status"),
+		      'honors admin can edit status on honors honors etd');
+
+
+    // cannot only do workflow steps on etds in the correct states
+    $this->assertTrue($this->acl->isAllowed("honors admin", "submitted honors etd", "review"),
+		      'honors admin can review submitted honors etd');
+    $this->assertFalse($this->acl->isAllowed("honors admin", "honors etd", "review"),
+		       "honors admin cannot review honors etd");
+    $this->assertFalse($this->acl->isAllowed("honors admin", "draft honors etd", "review"),
+		       "honors admin cannot review draft honors etd");
+    
+    $this->assertTrue($this->acl->isAllowed("honors admin", "reviewed honors etd", "approve"),
+		      "honors admin can approve reviewed honors etd");
+    $this->assertFalse($this->acl->isAllowed("honors admin", "submitted honors etd", "approve"),
+		       "honors admin cannot approve submitted honors etd");
+
+    $this->assertTrue($this->acl->isAllowed("honors admin", "published honors etd", "inactivate"),
+		      "honors admin can inactivate published honors etd");
+    $this->assertFalse($this->acl->isAllowed("honors admin", "honors etd", "inactivate"),
+		       "honors admin cannot inactivate generic (not published) honors etd");
+
+    $this->assertFalse($this->acl->isAllowed("honors admin", "submitted etd", "review"),
+		       'honors admin cannot review submitted non-honors etd');
+
+    $this->assertFalse($this->acl->isAllowed("honors admin", "log", "view"),
+		       "honors admin can view log");
   }
 
 }
