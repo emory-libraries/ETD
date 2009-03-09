ae3ad91e2bc6f80c6b82df11d6ae9c8a831ede85
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2009-03-09 18:41:41 +0000

    suppress notices about 'indirect modification' via __get on array/objects #473

--- branches/undergrad_honors/src/public/index.php
+++ branches/undergrad_honors/src/public/index.php
@@ -14,6 +14,10 @@ require("Zend/Loader.php");
 Zend_Loader::registerAutoload();
 
 
+// NOTE: php is now outputting a notice when using __set on arrays / objects
+// (actual logic seems to work properly)
+error_reporting(E_ALL ^ E_NOTICE);
+
 require_once("api/FedoraConnection.php");
 
 $config_dir = "../config/";
