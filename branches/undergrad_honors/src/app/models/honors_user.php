db5b164f05bb19df6dee02c2f784aee5289f329d
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2009-03-17 21:21:22 +0000

    initialize etd related to honors user info as honors etd

--- branches/undergrad_honors/src/app/models/honors_user.php
+++ branches/undergrad_honors/src/app/models/honors_user.php
@@ -1,7 +1,7 @@
-
 <?php
 
 require_once("user.php");
+require_once("honors_etd.php");
 
 class honors_user extends user {
 
@@ -11,6 +11,14 @@ class honors_user extends user {
     // override required fields - only current and permanent email, not address
     $this->required_fields = array("email", "permanent email");
   }
+
+  protected function configure() {
+    parent::configure();
+    // use honors_etd instead of default etd class
+    $this->relconfig["etd"]["class_name"] = "honors_etd";
+  }
+
+
 
   /* functions for checking required and complete fields are
      inherited-- the logic is the same, and actual tests are based on
