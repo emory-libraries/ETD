4c1b39eeff800c72567c66433dd33dde6cacd316
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2009-03-19 21:31:01 +0000

    test new netid/esdid logic

--- branches/former_faculty/tests/models/esdPersonTest.php
+++ branches/former_faculty/tests/models/esdPersonTest.php
@@ -27,14 +27,20 @@ class TestEsdPerson extends UnitTestCase {
     $this->assertEqual($user->directory_name, "Rebecca Sutton");
     $this->assertEqual($user->department_code, "2870");
     $this->assertEqual($user->department, "University Libraries");
-    $this->assertEqual($user->academic_career, "GSAS");
+
     $this->assertEqual($user->academic_program, null);
+
+    $this->assertEqual($user->email, "rebecca.s.koeser@emory.edu");
+
     // NOTE: this test is useless; value returned by ESD oscillates between MA and PHD for no apparent reason
     //    $this->assertEqual($user->degree, "PHD");	// FIXME: why does this change?
-    $this->assertEqual($user->term_complete, "5061");
-    $this->assertEqual($user->email, "rebecca.s.koeser@emory.edu");
-    $this->assertEqual($user->academic_plan_id, "ENGLISHMA");
-    $this->assertEqual($user->academic_plan, "English");
+
+    // these fields are now empty in ESD (?) - find better test subject ?
+    // OR (perhaps better) figure out how to mock the data (?)
+    //    $this->assertEqual($user->academic_career, "GSAS");
+    //    $this->assertEqual($user->term_complete, "5061");    
+    //    $this->assertEqual($user->academic_plan_id, "ENGLISHMA");
+    //    $this->assertEqual($user->academic_plan, "English");
     $this->assertNull($user->program_coord);
 
     $this->assertEqual($user->role, "superuser");
@@ -87,6 +93,21 @@ class TestEsdPerson extends UnitTestCase {
     $this->assertFalse($user->isCoordinator("Chemistry"));
   }
 
+  function testFormerFaculty_id() {
+    $user = new esdPerson();
+    $user->current_faculty = "N";
+    $user->id = "007";
+    $this->assertEqual("esdid007", $user->netid);
+  }
+
+  function testfindById() {
+    $user = $this->esd->findByUsername("rsutton");
+    $user2 = $this->esd->findByUsername("esdid" . $user->id);
+    $this->assertEqual($user, $user2);
+    
+  }
+
+  
   // FIXME: add tests for functions that find faculty names
     
 }
