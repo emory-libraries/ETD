277488e3c85b409b7402c746ed295793ccba138d
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2009-06-09 16:16:46 +0000

    bugfix: repaired stats script (broken by zendfw upgrade/emory_db_table)

---
+++ tags/v1.2.4/tests/controllers/TestIndexController.php
@@ -0,0 +1,53 @@
+<?php
+require_once("../bootstrap.php"); 
+/**
+ * unit tests for the Index Controller  (main index page only)
+ */
+
+
+require_once('../ControllerTestCase.php');
+require_once('controllers/IndexController.php');
+
+class IndexControllerTest extends ControllerTestCase {
+  function setUp() {
+    $this->response = $this->makeResponse();
+    $this->request  = $this->makeRequest();
+  }
+  
+  function tearDown() {}
+    	
+  function testIndexAction() {
+    $IndexController = new IndexControllerForTest($this->request,$this->response);
+
+    // NOTE: currently loading feed from another url
+    $this->expectError("Could not parse Feed 'recent'");
+    $IndexController->indexAction();
+    
+    $viewVars = $IndexController->view->getVars();	
+    $this->assertTrue(isset($IndexController->view->title));
+
+    // FIXME: test the feed part of this page by customizing the absoluteUrl helper  ?
+    //    $this->assertIsA($IndexController->view->feed, "Zend_Feed_Rss");
+  }
+}
+        
+class IndexControllerForTest extends IndexController {
+  public $renderRan = false;
+  public $redirectRan = false;
+  
+  public function initView() {
+    $this->view = new Zend_View();
+  }
+  
+  public function render() {
+    $this->renderRan = true;
+  }
+  
+  public function _redirect() {
+    $this->redirectRan = true;
+  }
+} 	
+
+    
+runtest(new IndexControllerTest());
+?>
