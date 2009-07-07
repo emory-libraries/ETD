20a0eb6d08243726ca6cdd83a18ddd314ceacc1e
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2009-07-07 17:25:15 +0000

    creating branch for fedora 3 migration

---
+++ branches/fedora3/tests/suites/models.php
@@ -0,0 +1,45 @@
+<?php
+
+if (defined('RUNNER')) {
+  $is_runner = false;	// running as part of a larger suite
+} else {
+  define('RUNNER', true);
+  $is_runner = true;
+}
+
+require_once("../bootstrap.php");
+
+class ModelGroupTest extends GroupTest {
+  function ModelGroupTest() {
+    $this->GroupTest('ETD Model tests');
+    $this->addTestFile('models/etdTest.php');
+    $this->addTestFile('models/etd_htmlTest.php');
+    $this->addTestFile('models/etdRelsTest.php');
+    $this->addTestFile('models/etdModsTest.php');
+    $this->addTestFile('models/FezEtdTest.php');
+    $this->addTestFile('models/premisTest.php');
+    $this->addTestFile('models/policyTest.php');
+    $this->addTestFile('models/filePolicyTest.php');
+
+    $this->addTestFile('models/etdFileTest.php');
+ 
+    $this->addTestFile('models/userTest.php');
+    $this->addTestFile('models/madsTest.php');
+    $this->addTestFile('models/skosCollectionTest.php');
+
+    $this->addTestFile('models/ProQuestSubmissionTest.php');
+    $this->addTestFile('models/esdPersonTest.php');
+
+    $this->addTestFile('models/honorsEtdTest.php');
+    $this->addTestFile('models/etdFactoryTest.php');
+    $this->addTestFile('models/etdSetTest.php');
+  }
+}
+
+if ($is_runner) {
+  $test = new ModelGroupTest;
+  $reporter = isset($argv) ? new TextReporter() : new HtmlReporter();
+  $test->run($reporter);
+}
+
+?>
