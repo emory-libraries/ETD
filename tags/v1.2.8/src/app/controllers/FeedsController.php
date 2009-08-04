dc002f0062eba2f93014ca7a228e7bfdde2f1523
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2009-08-04 17:36:07 +0000

    bugfix: better handling for weird non-breaking spaces

---
+++ tags/v1.2.8/src/app/controllers/FeedsController.php
@@ -0,0 +1,67 @@
+<?php
+
+require_once("models/etd.php");
+require_once("models/etd_feed.php");
+require_once("models/programs.php");
+
+class FeedsController extends Etd_Controller_Action {
+  protected $requires_fedora = true;
+
+  /**
+   * @var Etd_Feed object
+   */
+  public $feed;
+  
+  public function postDispatch() {
+    if (isset($this->feed)) {
+      $this->_helper->displayXml($this->feed->saveXml());
+    }
+  }
+
+  public function indexAction() {
+    $this->view->title = "RSS Feeds";
+
+  }
+
+  /* recently published ETDs */
+  public function recentAction() {
+    $options = $this->getFilterOptions();
+    $program = $this->_getParam("program");
+
+    $options["max"] = 10;
+    $options["return_type"] = "solrEtd";
+    /* NOTE: using solrEtd object here because it is significantly
+       faster to load (does not pull from Fedora) and has sufficient
+       information to generate feed entry.  */
+
+    $etdset = new EtdSet();
+    $etdset->findRecentlyPublished($options);
+
+    $feed_title = "Emory ETDs: Recently published";
+    if (! is_null($program)) $feed_title .= " : " . ucfirst($program);
+    
+
+    // pass in title for the feed, absolute url, and array of etds to be included      
+    $this->feed = new Etd_Feed($feed_title,
+			       $this->_helper->absoluteUrl("recent", "feeds", null, $this->view->url_params),
+			       // FIXME: what was $opts (last param here)
+			       $etdset->etds);
+  }
+
+
+  public function mostViewedAction() {
+    $options = $this->getFilterOptions();
+    $options["max"] = 10;
+    $etdset = new EtdSet();
+    $etdset->findMostViewed($options);
+
+    $feed_title = "Emory ETDs: Most Viewed";
+    
+    // pass in title for the feed, absolute url, and array of etds to be included      
+    $this->feed = new Etd_Feed($feed_title,
+			       $this->_helper->absoluteUrl("most-viewed", "feeds", null, $this->view->url_params),
+			       // FIXME: as above
+			       $etdset->etds);
+  }
+
+}
