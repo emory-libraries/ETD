02d694e26d9162d52b58cca1987049d77231e700
athom09 <athom09@affb5729-703e-0410-bbc3-960ba87530bd>
2009-08-19 16:45:51 +0000

    Changes for trac ticket #538: -Corrected date range issue -Changed Report name and associated action from "lastYear" to "commencement Report" -Added query summary section for debugging
    Remaining:
    Create form to allow select / un-select records to include

--- branches/adminReports/src/app/controllers/ReportController.php
+++ branches/adminReports/src/app/controllers/ReportController.php
@@ -11,18 +11,28 @@ class ReportController extends Etd_Controller_Action {
 	public function indexAction() {
 	}
 	 
-	public function lastyearAction() {
-		$this->view->title = "Last Year's ETDs";
+	public function commencementreportAction() {
+		$this->view->title = "Commencement Report";
+
+        //dates in query and human formats
+        list($this->view->dateRange, $this->view->lastDate, $this->view->nextDate) = $this->getCommencementDateRange();
 		
 		$optionsArray = array();
-		$optionsArray['query'] = "(degree_name:PhD AND (dateIssued:" . $this->getLastYearDateRange() . ") OR (-dateIssued:[* TO *] AND -status:'inactive'))";
+		$optionsArray['query'] = "(degree_name:PhD AND (dateIssued:" . $this->view->dateRange . ") OR (-dateIssued:[* TO *] AND -status:'inactive'))";
 		$optionsArray['sort'] = "author";
 		$optionsArray['NOT']['status'] = "draft";
 		// show ALL records on a single page 
 		$optionsArray['max'] = 1000;
 		//DEBUG
 		$this->view->searchQuery = $optionsArray['query'];
-		
+
+        //remember to update this if the query changes
+        $this->view->querySummary="Date Range: " . $this->view->lastDate . " - " . $this->view->nextDate
+        . "<br>" . "degree_name = 'PhD'"
+        . "<br>" . "status NOT 'inactive' status NOT 'draft'"
+        . "<br>" . "row limit 1000"
+        ;
+  		
 		 /*
   * generic etd find with many different parameters
    *
@@ -49,12 +59,22 @@ class ReportController extends Etd_Controller_Action {
 	
 //	    $this->view->list_title = "Found " . count($this->view->etdSet) . " ETDs.";
 	    $this->view->list_title = "";
-	    $this->view->list_description = "All ETDs for PHDs from last year.";
+	    $this->view->list_description = "Commencement Report";
 	}
 	
-	public function getLastYearDateRange() {
-		$next_grad_date = date("Ymd", mktime(0, 0, 0, 5, 31, date("Y"))); 
-		$last_year_grad_date = date("Ymd", mktime(0, 0, 0, 6, 1, date("Y")-10)); 
-		return "[" . $last_year_grad_date . " TO " . $next_grad_date ."]";
+	public function getCommencementDateRange() {
+        //created dates so they can be reformated for the query and people
+        $last_date=mktime(0, 0, 0, 6, 1, (date("Y")-1));
+        $next_date=mktime(0, 0, 0, 5, 31, date("Y"));
+
+		$last_year_grad_date = date("Ymd", $last_date);
+        $next_grad_date = date("Ymd", $next_date);
+		
+		
+        return array(
+                        "[" . $last_year_grad_date . " TO " . $next_grad_date ."]", //String formated for query
+                        date("Y-m-d", $last_date), //last year's date formated for humans
+                        date("Y-m-d", $next_date) //next grad date formated for humans
+                    );
 	}
 }
