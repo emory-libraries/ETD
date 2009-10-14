3a541a8213d92b92f467115518c8379fff58e201
athom09 <athom09@affb5729-703e-0410-bbc3-960ba87530bd>
2009-10-14 17:05:39 +0000

    Merged revisions 2131 - 2167 of adminReports branch

--- branches/1.4rc/tests/controllers/TestReportController.php
+++ branches/1.4rc/tests/controllers/TestReportController.php
@@ -8,6 +8,7 @@ class ReportControllerTest extends ControllerTestCase {
   // array of test foxml files & their pids 
   private $etdxml;
   private $test_user;
+  private $solr;
   
   function setUp() {
     $ep = new esdPerson();
@@ -23,62 +24,246 @@ class ReportControllerTest extends ControllerTestCase {
     $this->response = $this->makeResponse();
     $this->request  = $this->makeRequest();
 
-//    $this->etdxml = array("etd1" => "test:etd1",
-//			  "etd2" => "test:etd2",
-//			  "user" => "test:user1",
-//			  //"etd3" => "test:etd3",	// not working for some reason
-//			  );
-//    
-//
-//    // load a test objects to repository
-//    // NOTE: for risearch queries to work, syncupdates must be turned on for test fedora instance
-//    foreach (array_keys($this->etdxml) as $etdfile) {
-//      $pid = fedora::ingest(file_get_contents('../fixtures/' . $etdfile . '.xml'), "loading test etd");
-//    }
-//
-    $solr = &new Mock_Etd_Service_Solr();
-    Zend_Registry::set('solr', $solr);
+    $fedora=Zend_Registry::get("fedora");
+    $this->pid = $fedora->ingest(file_get_contents('../fixtures/etd2.xml'), "loading test etd");
+
+    $this->solr = &new Mock_Etd_Service_Solr();
+    Zend_Registry::set('solr', $this->solr);
+
+    $fname = '../fixtures/etd2.xml';
+    $dom = new DOMDocument();
+    $dom->load($fname);
+    $this->etd = new etd($dom);
+
+
   }
   
   function tearDown() {
-//    foreach ($this->etdxml as $file => $pid)
-//      fedora::purge($pid, "removing test etd");
-//
+
     Zend_Registry::set('solr', null);
     Zend_Registry::set('current_user', null);
+    $fedora=Zend_Registry::get("fedora");
+    $fedora->purge($this->pid, "removing test etd");
+
   }
-  
-  function testLastYearAction() {
+
+  function testcommencementReviewAction() {
     $ReportController = new ReportControllerForTest($this->request,$this->response);
-    $ReportController->lastyearAction();
+    $ReportController->commencementReviewAction();
 
     $this->assertTrue(isset($ReportController->view->title));
+    $etdSet = $ReportController->view->etdSet;
+    $this->assertIsA($etdSet, 'EtdSet', "etdSet is a etdSet object.");
+
+    //test as student,  all other tests are done as admin
+    $this->test_user->role = "student";
+    Zend_Registry::set('current_user', $this->test_user);
+    $this->assertFalse($ReportController->commencementAction() );
+}
+
+  
+  function testcommencementAction() {
+    $ReportController = new ReportControllerForTest($this->request,$this->response);
+
+    //set post data
+    $this->setUpPost(array('exclude' => array("test:etd2")));
+
+    $etd = &new MockEtd();
+    $etd->PID = "test:etd1";
+    //print "PID: $this->pid";
+    $this->solr->response->docs[] = $etd;
+
+    $etd = &new MockEtd();
+    $etd->PID = $this->pid;
+    //print "PID: $this->pid";
+    $this->solr->response->docs[] = $etd;
+
+    $ReportController->commencementAction();
+
+    $this->assertTrue(isset($ReportController->view->title));
+    $this->assertTrue(isset($ReportController->view->startDate));
+    $this->assertTrue(isset($ReportController->view->endDate));
+    $this->assertPattern("/\d{4}\-\d{2}\-\d{2}/", $ReportController->view->startDate, "Format should be Y-m-d");
+    $this->assertPattern("/\d{4}\-\d{2}\-\d{2}/", $ReportController->view->endDate, "Format should be Y-m-d");
     
     $etdSet = $ReportController->view->etdSet;
     $this->assertIsA($etdSet, 'EtdSet', "etdSet is a etdSet object.");
-    if($etdSet!=null && count($etdSet)>0) {
-    	foreach ($etdSet->etds as $etd) {
-    		$this->assertTrue($etd!=null,"ETD should not be null");
-    		$this->assertTrue($etd->author()!=null,"ETD Author should not be null");
-			$this->assertTrue($etd->title()!=null,"ETD Title should not be null");
-    		$this->assertTrue($etd!=null,"ETD should not be null");
-    		$etdAdvisors = $etd->chair(); 
-    		if($etdAdvisors!=null && count($etdAdvisors)>0) {
-    			foreach ($etdAdvisors as $advisor) {
-    				$this->assertTrue($advisor!=NULL,"Advisor: " . $advisor . "should not be null");
-    			}
-    		} 
-    	}
-	}
-  }	
 
-  function testGetLastYearDateRange() {
-    $ReportController = new ReportControllerForTest($this->request,$this->response);
-    $lastYearDateRange = $ReportController->getLastYearDateRange();
+    //Make sure it removed 1 etd
+    $this->assertEqual(1, count($etdSet->etds));
+
+    //Make sure remaining is correct one
+    $this->assertEqual("test:etd1", $etdSet->etds[0]->pid);
+
     
-    $this->assertPattern("/\[\d{8}\s[T][O]\s\d{8}\]/", $lastYearDateRange, "Last Year Date Range should be in format: [yyyymmdd TO yyyymmdd]. " . $lastYearDateRange );
-  }	
+    //test as student,  all other tests are done as admin
+    $this->test_user->role = "student";
+    Zend_Registry::set('current_user', $this->test_user);
+    $this->assertFalse($ReportController->commencementAction() );
+    
+}
 
+function testgradDataAction() {
+    $field = "dateIssued";
+    $this->solr->response->facets = new Emory_Service_Solr_Response_Facets(array("dateIssued" =>
+										 array("20070604" => "1",
+										      "20070919" => "2",
+                                              "20071231" => "3",
+                                              "20080531" => "4",
+                                              "20080831" => "5",
+                                              "20081231" =>  "6",
+                                              "20090531" => "7",
+                                              "20090831" => "8" )));
+
+    $ReportController = new ReportControllerForTest($this->request,$this->response);
+    $ReportController->gradDataAction();
+
+    $this->assertTrue(isset($ReportController->view->options));
+    $this->assertTrue(isset($ReportController->view->curStart));
+    $this->assertTrue(isset($ReportController->view->curEnd));
+
+    //test as student,  all other tests are done as admin
+    $this->test_user->role = "student";
+    Zend_Registry::set('current_user', $this->test_user);
+    $this->assertFalse($ReportController->gradDataAction() );
+
+}
+
+function testgradDataCsvAction() {
+
+    $ReportController = new ReportControllerForTest($this->request,$this->response);
+    //set post data
+    $this->setUpPost(array('academicYear' => "20081231:20090831"));
+   $etd = &new MockEtd();
+    $etd->PID = $this->pid;
+    //print "PID: $this->pid";
+    $this->solr->response->docs[] = $etd;
+
+    $ReportController->gradDataCsvAction();
+
+    $this->assertTrue(isset($ReportController->view->data));
+    
+    //Test to make sure data is an array of arrays
+    $this->assertisA($ReportController->view->data, "array");
+
+    foreach($ReportController->view->data as $line){
+        $this->assertisA($line, "array");
+    }
+
+    $this->assertEqual($ReportController->view->data[1][1], "Duck, Daffy");
+    $this->assertEqual($ReportController->view->data[1][22], "Dissertation");
+
+    $response = $ReportController->getResponse();
+    $headers = $response->getHeaders();
+    $this->assertEqual("Content-Type", $headers[0]["name"]);
+    $this->assertEqual("text/csv", $headers[0]["value"]);
+    $this->assertEqual("Content-Disposition", $headers[1]["name"]);
+    $this->assertPattern("/attachment; filename=\"GradReport-\d{8}-\d{8}\.csv\"/", $headers[1]["value"]);
+       
+    //print "<pre>";
+    //print_r($ReportController->view->data);
+    //print_r($headers);
+    //print "</pre>";
+
+
+    //test as student,  all other tests are done as admin
+    $this->test_user->role = "student";
+    Zend_Registry::set('current_user', $this->test_user);
+    $this->assertFalse($ReportController->gradDataCsvAction() );
+}
+
+/*function testembargoCsvAction() {
+
+    $ReportController = new ReportControllerForTest($this->request,$this->response);
+
+    $etd = &new MockEtd();
+    $etd->PID = $this->pid;
+    //print "PID: $this->pid";
+    $this->solr->response->docs[] = $etd;
+
+    $ReportController->embargoCsvAction();
+
+    $this->assertTrue(isset($ReportController->view->data));
+
+    //Test to make sure data is an array of arrays
+    $this->assertisA($ReportController->view->data, "array");
+
+    foreach($ReportController->view->data as $line){
+        $this->assertisA($line, "array");
+    }
+
+    $response = $ReportController->getResponse();
+    $headers = $response->getHeaders();
+    $this->assertEqual("Content-Type", $headers[0]["name"]);
+    $this->assertEqual("text/csv", $headers[0]["value"]);
+    $this->assertEqual("Content-Disposition", $headers[1]["name"]);
+    $this->assertEqual("attachment; filename=\"EmbargoReport.csv\"", $headers[1]["value"]);
+
+    //print "<pre>";
+    //print_r($ReportController->view->data);
+    //print_r($headers);
+    //print "</pre>";
+
+
+    //test as student,  all other tests are done as admin
+    $this->test_user->role = "student";
+    Zend_Registry::set('current_user', $this->test_user);
+    $this->assertFalse($ReportController->embargoCsvAction() );
+}*/
+
+
+  function testGetCommencementDateRange() {
+    $ReportController = new ReportControllerForTest($this->request,$this->response);
+    list($startDate, $endDate) = $ReportController->getCommencementDateRange();
+
+    $this->assertEqual("06-01",  (date("m-d", $startDate)));
+    $this->assertEqual("05-31",  (date("m-d", $endDate)));
+    $this->assertEqual(1,  date("Y", $endDate) - date("Y", $startDate));
+    
+        
+  }
+
+  function testAddCSVFields(){
+      $ReportController = new ReportControllerForTest($this->request,$this->response);
+
+      $line=$ReportController->addCSVFields($this->etd, "chair", array("id", "full"), 3);
+
+      $this->assertIsA($line, "array");
+      $this->assertEqual(count($line), 6);
+  }
+  
+  function testGetSemesterDecorator() {
+    $ReportController = new ReportControllerForTest($this->request,$this->response);
+    
+    $result = $ReportController->getSemesterDecorator("20090101");
+    $this->assertEqual($result, "");
+
+    $result = $ReportController->getSemesterDecorator("20090301");
+    $this->assertEqual($result, "");
+
+    $result = $ReportController->getSemesterDecorator("20090501");
+    $this->assertEqual($result, "");
+
+    $result = $ReportController->getSemesterDecorator("20090601");
+    $this->assertEqual($result, "*");
+
+    $result = $ReportController->getSemesterDecorator("20090701");
+    $this->assertEqual($result, "*");
+
+    $result = $ReportController->getSemesterDecorator("20090801");
+    $this->assertEqual($result, "*");
+
+    $result = $ReportController->getSemesterDecorator("20090901");
+    $this->assertEqual($result, "**");
+
+        $result = $ReportController->getSemesterDecorator("20091001");
+    $this->assertEqual($result, "**");
+
+        $result = $ReportController->getSemesterDecorator("20091201");
+    $this->assertEqual($result, "**");
+    
+  }
+  
 }
 
 
