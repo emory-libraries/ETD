979b34e205a42e1a111da2a1074fc76fb17f067d
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2009-10-19 19:14:12 +0000

    release candidate branch merged into trunk & tagged for deployment, dev branch no longer needed

--- branches/1.4rc/src/scripts/console.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env php -q
-<?php
-chdir("suites");
-// set up include path, fedora/db connections, load common models, etc.
-require_once("bootstrap.php");
-  
-// load interactive php shell
-require "php-shell-cmd.php";
-?>
