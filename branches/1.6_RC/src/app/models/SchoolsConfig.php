59b6753905a8b858325d12785c8ddd20fcb4b137
athom09 <athom09@affb5729-703e-0410-bbc3-960ba87530bd>
2009-12-18 20:36:06 +0000

    Recreating branch

---
+++ branches/1.6_RC/src/app/models/SchoolsConfig.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * @category Etd
+ * @package Etd_Models
+ */
+
+require_once("esdPerson.php");
+
+/**
+ * multi-school config object
+ * can be initialized as a standard Zend_Config_Xml object, but
+ * adds several functions for finding & accessing specific school data
+ */
+class SchoolsConfig extends Zend_Config_Xml {
+
+  /**
+   * return full label by school id/shorthand
+   * @param string $id id for the school (equivalent to section tag in xml config)
+   * @return string|null label as string on success, null when no match for id
+   */
+  public function getLabel($id) {
+    if (isset($this->$id)) return $this->$id->label;
+    return null;
+  }
+
+  /**
+   * get school id by the id of the corresponding Fedora collection object
+   * @param string $collection fedora pid for configured collection
+   * @return string|null
+   */
+  public function getIdByFedoraCollection($collection) {
+    foreach ($this as $school) {
+      if ($collection == $school->fedora_collection) {
+	return $this->idBySchool($school);
+      }
+    }
+    return null;
+  }
+
+  /**
+   * determine if a user is a school-specific admin, according to configured filters
+   * if yes, returns school configured ACL id; otherwise returns null
+   * @param esdPerson $user
+   * @return string|false
+   */
+  public function isAdmin(esdPerson $user) {
+    foreach ($this as $school) {
+      // if department name is configured & matches user department
+      if ((isset($school->admin->department) && $school->admin->department != '' && 
+	   isset($user->department) && $user->department != '' &&
+	   $user->department == $school->admin->department) ||
+	  // OR if netid matches (handles single netid or multiple)
+	  (isset($school->admin->netid) && $school->admin->netid != '' 
+	   && $user->netid == $school->admin->netid
+	   || (is_object($school->admin->netid) &&
+	       in_array($user->netid, $school->admin->netid->toArray())))) {
+	return $school->acl_id;
+      }
+    }
+  }
+
+
+  /**
+   * return school section id by corresponding database id
+   * @param string $db_id
+   * @return string|null
+   */
+  public function getIdByDatabaseId($db_id) {
+    foreach ($this as $school) {
+      if ($school->db_id == $db_id) return $this->idBySchool($school);
+    }
+    return null;
+  }
+
+  
+  /**
+   * return school config by acl id
+   * @param string $acl_id school id used in ACL, e.g. honors for honors admin
+   * @return Zend_Config|null school config on success, null when no match for acl id
+   */
+  public function getSchoolByAclId($id) {
+    foreach ($this as $school) {
+      if ($school->acl_id == $id) return $school;
+    }
+    return null;
+  }
+
+  /**
+   * return school id (section label) by the school section config object
+   * - finds the section id of this school by looking for its key in the full data array
+   * @return string
+   */
+  protected function idBySchool(Zend_Config $school) {
+    return array_search($school, $this->_data);
+  }
+   
+}
