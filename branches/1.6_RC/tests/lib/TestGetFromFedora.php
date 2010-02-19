97715e1603f8a1c663579ccb69af0e5150ff18f2
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2010-02-19 18:55:45 +0000

    restore ability to get etd_file objects with GetFromFedora helper; better unit testing

--- branches/1.6_RC/tests/lib/TestGetFromFedora.php
+++ branches/1.6_RC/tests/lib/TestGetFromFedora.php
@@ -9,9 +9,17 @@ class TestGetFromFedora extends ControllerTestCase {
   private $_fedora;
   private $mock_fedora;
 
+  private $etdpid;
+  private $filepid;
+  private $userpid;
+
   function __construct() {
     $this->fedora = Zend_Registry::get("fedora");
     $fedora_cfg = Zend_Registry::get('fedora-config');
+
+    // get test pids for fedora objects
+    list($this->etdpid, $this->filepid,
+	   $this->userpid) = $this->fedora->getNextPid($fedora_cfg->pidspace, 3);
   }
 
 
@@ -28,38 +36,68 @@ class TestGetFromFedora extends ControllerTestCase {
     $this->_fedora = Zend_Registry::get('fedora');
     $this->mock_fedora = &new MockFedoraConnection();
     $this->mock_fedora->risearch = $this->_fedora->risearch; 
-    Zend_Registry::set('fedora', $this->mock_fedora);
+
+    // ingest fixtures into fedora
+    $dom = new DOMDocument();
+    $dom->load('../fixtures/etd1.xml');
+    $etd = new etd($dom);
+    $etd->pid = $this->etdpid;
+    $this->_fedora->ingest($etd->saveXML(), "test etd for retrieving with getfromfedora helper");
+
+    $dom = new DOMDocument();
+    $dom->load('../fixtures/etdfile.xml');
+    $etdfile = new etd_file($dom);
+    $etdfile->pid = $this->filepid;
+    $this->_fedora->ingest($etdfile->saveXML(), "test etd_file for retrieving with getfromfedora helper");
+
+    $dom = new DOMDocument();
+    $dom->load('../fixtures/user.xml');
+    $authinfo = new user($dom);
+    $authinfo->pid = $this->userpid;
+    $this->_fedora->ingest($authinfo->saveXML(), "test authorInfo for retrieving with getfromfedora helper");
   }
-  
+
   function tearDown() {
     $this->resetGet();
+    $this->_fedora->purge($this->etdpid, "removing test etd");
+    $this->_fedora->purge($this->filepid, "removing test etd_file");
+    $this->_fedora->purge($this->userpid, "removing test etd authorInfo");
+    // restore real fedora in registry (in case mock fedora was used)
     Zend_Registry::set('fedora', $this->_fedora);
+  }
+
+  // replace fedoraconnection in registry with real mock fedora - not used by all tests, so not in setup
+  function setMockFedora() {
+    Zend_Registry::set('fedora', $this->mock_fedora);
   }
 
 
   // testing with mock objects
-  function testDirect_mock(){
+  function testDirect_norecord(){
+    $this->setMockFedora();
     // no value for id param
     $this->assertNull($this->helper->direct("id", "etd"));
     $messages = $this->controller->getHelper("flashMessenger")->getMessages();
     $this->assertEqual("Error: No record specified for etd", $messages[0]);
+  }
 
+  /* test various exceptions */
+  
+  function test_exceptions_notfound(){
+    $this->setMockFedora();
     // simulate retriving an etd object from Fedora
-    $this->setUpGet(array("id" => "test:1"));
-    // simulated etd object does not have correct cmodel; ignore the error
-    $this->expectException();	// foxmlbadcontentmodel
-    // no results returned from risearch for simulated etd
-    //    $this->expectError("No response returned from risearch; cannot determine if test:1 is an honors etd");
-    $result = $this->helper->direct("id", "etd");
-    $this->assertIsA($result, "etd");
-    
-    
-    // test various exceptions
-    // - not found
+    $this->setUpGet(array("id" => $this->etdpid));
+
+    // not found
     $this->mock_fedora->setException("NotFound");
-    $this->helper->direct("id", "etd");
+    $result = $this->helper->direct("id", "etd");
+    $this->assertNull($result);
     $messages = $this->controller->getHelper("flashMessenger")->getMessages();
     $this->assertPattern("/Record not found/", $messages[0]);
+  }
+
+  function test_exceptions_accessdenied() {
+    $this->setMockFedora();
     // - access denied
     $this->setUpGet(array("id" => "test:1"));
     $this->mock_fedora->setException("AccessDenied");
@@ -67,6 +105,10 @@ class TestGetFromFedora extends ControllerTestCase {
     $response = $this->controller->getResponse();
     $this->assertEqual("403", $response->getHttpResponseCode());
     $this->assertPattern('/Permission Denied/', $response->getBody());
+  }
+
+  function test_exceptions_notauthorized() {
+    $this->setMockFedora();
     // - not authorized
     $this->setUpGet(array("id" => "test:1"));
     $this->mock_fedora->setException("NotAuthorized");
@@ -74,6 +116,10 @@ class TestGetFromFedora extends ControllerTestCase {
     $response = $this->controller->getResponse();
     $this->assertEqual("403", $response->getHttpResponseCode());
     $this->assertPattern('/Permission Denied/', $response->getBody());
+  }
+  
+  function test_exception() {
+    $this->setMockFedora();
     // - generic foxml exception
     $this->setUpGet(array("id" => "test:1"));
     $this->mock_fedora->setException("generic");
@@ -81,6 +127,30 @@ class TestGetFromFedora extends ControllerTestCase {
     $response = $this->controller->getResponse();
     $this->assertEqual("403", $response->getHttpResponseCode());
     $this->assertPattern('/Permission Denied/', $response->getBody());
+  }
+
+  function testDirect_etd() {
+    // retrieve an etd object from Fedora
+    $this->setUpGet(array("id" => $this->etdpid));
+    $result = $this->helper->direct("id", "etd");
+    $this->assertIsA($result, "etd");
+    $this->assertEqual($this->etdpid,  $result->pid);
+  }
+
+  function testDirect_etdfile() {
+    // retrieve an etd_file object from Fedora
+    $this->setUpGet(array("id" => $this->filepid));
+    $result = $this->helper->direct("id", "etd_file");
+    $this->assertIsA($result, "etd_file");
+    $this->assertEqual($this->filepid,  $result->pid);
+  }
+  
+  function testDirect_user() {
+    // retrieve a user object from Fedora
+    $this->setUpGet(array("id" => $this->userpid));
+    $result = $this->helper->direct("id", "user");
+    $this->assertIsA($result, "user");
+    $this->assertEqual($this->userpid,  $result->pid);
   }
 
 }
