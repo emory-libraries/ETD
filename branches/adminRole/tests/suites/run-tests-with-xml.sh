7f13ecae11cbc18df224c83db5d5597f55d224af
athom09 <athom09@affb5729-703e-0410-bbc3-960ba87530bd>
2010-03-12 20:32:03 +0000

    Deleting branch after code was deployed to production in tag v1.6

--- branches/adminRole/tests/suites/run-tests-with-xml.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-mkdir -p test-results
-rm -f test-results/*
-php xml_suite.php > test-results/suite.xml
-../simpletest/local/clean_simpletest_xml.php test-results/suite.xml 
-xsltproc ../simpletest/local/simpletest_to_junit.xsl test-results/suite.xml > test-results/TEST-suite.xml
