9195747ae0271fa57d997989e866f06b7a9944fc
rsutton <rsutton@affb5729-703e-0410-bbc3-960ba87530bd>
2010-04-21 22:05:24 +0000

    clean up report controller

--- trunk/src/app/controllers/ReportController.php
+++ trunk/src/app/controllers/ReportController.php
@@ -10,10 +10,7 @@ require_once("models/etd.php");
 
 class ReportController extends Etd_Controller_Action {
 	protected $requires_fedora = false;
-	protected $params;
-	/* FIXME: what are these for? used anywhere? */
-	private $etd_pid;
-	private $message;
+	protected $params;	
 
 	/**
 	 * copy of fedoraConnection with current user's auth credentials
@@ -67,51 +64,52 @@ class ReportController extends Etd_Controller_Action {
 	 */
 	public function indexAction() {
         if(!$this->_helper->access->allowed("report", "view")) {return false;}
+        $this->view->title = "Reports";
 	}
 	 
 	/**
-     *This action creates a form to allows a user to select ETDs to be excluded 
-     * from the commencement report
-     */
-    public function commencementReviewAction() {
-        if(!$this->_helper->access->allowed("report", "view")) {return false;}
-
-		$this->view->title = "Commencement Report Review";
-
-        //Create dates in query and human formats
-        list($startDate, $endDate) = $this->getCommencementDateRange();
-        $dateRange= "[" . date("Ymd", $startDate) . " TO " . date("Ymd", $endDate) ."]";
-
-
-		$optionsArray = array();
-		$optionsArray['query'] = "(degree_name:PhD AND (dateIssued:" . $dateRange . ") OR (-dateIssued:[* TO *] AND -status:'inactive'))";
-		$optionsArray['sort'] = "author";
-		$optionsArray['NOT']['status'] = "draft";
-		// show ALL records on a single page 
-		$optionsArray['max'] = 1000;
-		$optionsArray['return_type'] = "solrEtd";
-		        
-	    $etdSet = new EtdSet();
-	    $etdSet->find($optionsArray);
-	    $this->view->etdSet = $etdSet;
+	 * commencement review - allow user to select ETDs to be excluded 
+	 * from the commencement report
+	 */
+	public function commencementReviewAction() {
+	  if(!$this->_helper->access->allowed("report", "view")) {return false;}
+	  
+	  $this->view->title = "Reports : Commencement Report Review";
+	  
+	  //Create dates in query and human formats
+	  list($startDate, $endDate) = $this->getCommencementDateRange();
+	  $dateRange= "[" . date("Ymd", $startDate) . " TO " . date("Ymd", $endDate) ."]";
+	  
+	  
+	  $optionsArray = array();
+	  $optionsArray['query'] = "(degree_name:PhD AND (dateIssued:" . $dateRange . ") OR (-dateIssued:[* TO *] AND -status:'inactive'))";
+	  $optionsArray['sort'] = "author";
+	  $optionsArray['NOT']['status'] = "draft";
+	  // show ALL records on a single page 
+	  $optionsArray['max'] = 1000;
+	  $optionsArray['return_type'] = "solrEtd";
+	  
+	  $etdSet = new EtdSet();
+	  $etdSet->find($optionsArray);
+	  $this->view->etdSet = $etdSet;
 	}
 
 
     /**
-     * This action produces the commencement report and filters out the excluded
-     *  pids from the previous form
+     *  Produce the commencement report, filtering out any user-selected records
+     *  set to be excluded on the commencement-review page
      */
     public function commencementAction() {
         if(!$this->_helper->access->allowed("report", "view")) {return false;}
+      
+        $this->view->title = "Reports : Commencement Report";
 
-		$this->view->title = "Commencement Report";
+        //Get the list of PIDs to exclude
+        $inputField="exclude";
 
-       //Get the list of PIDs to exclude
-       $inputField="exclude";
-
-       if($this->_hasParam($inputField)){
+        if($this->_hasParam($inputField)){
             $exclude = $this->_getParam($inputField);
-       }
+        }
 
         //Create dates in query and human formats
         list($startDate, $endDate) = $this->getCommencementDateRange();
@@ -128,7 +126,7 @@ class ReportController extends Etd_Controller_Action {
 		$optionsArray['NOT']['status'] = "draft";
 		// show ALL records on a single page 
 		$optionsArray['max'] = 1000;
-		/* FIXME: should this really be solrEtd ? */
+		/* FIXME: should this really be solrEtd ? loses title formatting, but is much faster... */
 		$optionsArray['return_type'] = "solrEtd";
 
 		        
@@ -136,27 +134,28 @@ class ReportController extends Etd_Controller_Action {
 	    $etdSet->find($optionsArray);
         
         //remove ETDs by pid or calculate & save grad semester indicator
-        foreach($etdSet->etds as $index => $etd){
-	  if(is_array($exclude) && in_array($etd->pid(), $exclude)){
+        foreach ($etdSet->etds as $index => $etd) {
+            if(is_array($exclude) && in_array($etd->pid(), $exclude)){
                unset($etdSet->etds[$index]);
             } else {
-	    	$etdSet->etds[$index]->semester = $this->getSemesterDecorator($etd->pubdate());
+                $etdSet->etds[$index]->semester = $this->getSemesterDecorator($etd->pubdate());
             }
         }
 
-	$this->view->etdSet = $etdSet;
+        $this->view->etdSet = $etdSet;
 
-	// get dojo cdn from config
-	$config = Zend_Registry::get('config');
-	$this->view->dojo_config = $config->dojo;
+        // get dojo cdn from config
+        $config = Zend_Registry::get('config');
+        $this->view->dojo_config = $config->dojo;
     }
 
     /**
-     * Action to render gradData date selection form
+     *  Display an academic year date selection form for grad-data report
      *  Start of year is 12/31 of last year, end is 8/31 of current year
      */
     public function gradDataAction(){
-         if(!$this->_helper->access->allowed("report", "view")) {return false;}
+        if(!$this->_helper->access->allowed("report", "view")) {return false;}
+        $this->view->title = "Reports : Graduate Schol Academic Year";
 
         // academic start and end months
         $acStart="Dec 31";
@@ -168,11 +167,10 @@ class ReportController extends Etd_Controller_Action {
         //We are looking for completed yeears only!
         //If the report is run durring an academic year we get the most renct complted year: December from 2 years ago and  August from 1 yer ago
         //If it is betwen the end of one and the start of the next: we get Decenber for previous year and August for curent your
-        if($curDate < strtotime("$acEnd +1 day", $curDate)){
+        if ($curDate < strtotime("$acEnd +1 day", $curDate)){
             $startDate = strtotime("$acStart -2 years" , $curDate);
             $endDate = strtotime("$acEnd -1 year" , $curDate);
-        }
-        else{
+        } else{
             $startDate = strtotime("$acStart -1 year" , $curDate);
             $endDate = strtotime($acEnd, $curDate);
         }
@@ -192,8 +190,6 @@ class ReportController extends Etd_Controller_Action {
         $this->view->options=$options;
         //################
 
-
-
         /*
         //get dates where submissions exist
         $field = "dateIssued";
@@ -207,7 +203,7 @@ class ReportController extends Etd_Controller_Action {
     }
 
     /*
-     * Action to create CSV file from submitted date range
+     * generate a CSV file of graduate school data for the requested date range
      */
     public function gradDataCsvAction(){
         if(!$this->_helper->access->allowed("report", "view")) {return false;}
@@ -277,26 +273,29 @@ class ReportController extends Etd_Controller_Action {
 
     /**
      * Creates a timestamp for 06-01 of last year and a timestamp of 05-31 of this year
-     * Returns both in an Array
+     * Returns an array of start, end
      * @return array
      */
-	public function getCommencementDateRange() {
-        //created dates so they can be reformated for the query and people
-        $startDate=mktime(0, 0, 0, 6, 1, (date("Y")-1));
-        $endDate=mktime(0, 0, 0, 5, 31, date("Y"));
-
-		return array($startDate, $endDate);
-	}
+    public function getCommencementDateRange() {
+      //created dates so they can be reformated for the query and people
+      $startDate = mktime(0, 0, 0, 6, 1, (date("Y")-1));
+      $endDate = mktime(0, 0, 0, 5, 31, date("Y"));
+      
+      return array($startDate, $endDate);
+    }
 
 
     /*
      * This returns new fields to be added to the current CSV line, from the dataset $etdSet
-     * It retreives the fields spracified in $fields from the section of the
-     * response specified by $group.  $max limits the number of sets to be added
+     * It retreives the fields specified in $fields from the section of the
+     * response specified by $group.  $max indicates the number of sets to be added
      * Example:  addCSVFields($etd, "chair", array("id", "full"), 2):
      * Would add $etd->mods->chair->id and $etd->mods->chair->full fields to the
      * CSV line from the first two entries in the $etd->mods->chair array.
-     *  @param etdSet $etd - The etd result set
+     * Will add empty fields according to the specified maximum, so CSV columns
+     * will line up no matter how many entries are present.
+     *
+     * @param etdSet $etd - The etd result set
      * @paam string $group - The group of the field that is being added
      * @param array $fields - List of fields from the group
      * @param int $max - Max number of fields to add
@@ -304,15 +303,14 @@ class ReportController extends Etd_Controller_Action {
      */
     public function addCSVFields($etd, $group, $fields, $max){
         for($i = 0; $i < $max; $i++){
-            if( isset($etd->mods->{$group}[$i]) ){
-	      foreach($fields as $field) {
-		$value = $etd->mods->{$group}[$i]->$field;
-		// ignore ids with underscores -- some hand-entered non-Emory advisor ids have this
-		if ($field == "id" && preg_match("/_/", $value)) $value = "";
-		$line[] = $value;
-	      }
-            }
-            else{
+            if (isset($etd->mods->{$group}[$i]) ){
+                foreach($fields as $field) {
+                $value = $etd->mods->{$group}[$i]->$field;
+                // ignore ids with underscores -- some hand-entered non-Emory advisor ids have this
+                if ($field == "id" && preg_match("/_/", $value)) $value = "";
+                    $line[] = $value;
+                }
+            } else {
                 foreach($fields as $field){
                     $line[] = "";
                 }
@@ -322,9 +320,9 @@ class ReportController extends Etd_Controller_Action {
     }
 
 
-        /**
-         * Action to create CSV file with Embargo data
-         */
+    /**
+     * generate a CSV file with information about embargoed records
+     */
     public function embargoCsvAction(){
       if(!$this->_helper->access->allowed("report", "view")) {return false;}
 
@@ -389,10 +387,9 @@ class ReportController extends Etd_Controller_Action {
        $this->getResponse()->setHeader('Content-Disposition', 'attachment; filename="' . $filename . '"');
     }
 
-/**
- *
- * Action to create CSV file with email data
- */
+    /**
+     * generate a CSV file with student name, emory email, and permanent email
+     */
    public function exportemailsAction() {
      if(!$this->_helper->access->allowed("report", "view")) {return false;}
      $etdSet = new EtdSet();
@@ -423,10 +420,12 @@ class ReportController extends Etd_Controller_Action {
      
    }
 
-
+   /**
+    * summary statistics based on facets in the solr index
+    */
    public function summaryStatAction() {
      if(!$this->_helper->access->allowed("report", "view")) {return false;}
-     $this->view->title = "Summary Statistics";
+     $this->view->title = "Reports : Summary Statistics";
 
      $solr = Zend_Registry::get('solr');
      $solr->clearFacets();
@@ -465,26 +464,23 @@ class ReportController extends Etd_Controller_Action {
 
 
     /**
- * Function to retun a decorator to be used with the author name
- *
- * @param Date $grad_date - grad date of curent ETD
- * @return String
- */
-function getSemesterDecorator($grad_date) {
-		$date_grad_date = strtotime($grad_date);
-		$decorator = "";
-		if(intval(date("m", $date_grad_date))>=1 && intval(date("m", $date_grad_date))<=5) { // SPRING!
-			$decorator = "";
-		} else if(intval(date("m", $date_grad_date))>5 && intval(date("m", $date_grad_date))<=8) { // SUMMER!
-			$decorator = "*";
-		} else if(intval(date("m", $date_grad_date))>8 && intval(date("m", $date_grad_date))<=12) { // FALL!
-			$decorator = "**";
-		}
-		return $decorator;
-}
-
- 
-
-
+     * Function to retun a decorator to be used with the author name
+     *
+     * @param Date $grad_date - grad date of curent ETD
+     * @return String
+     * @todo convert this to a view helper
+     */
+    function getSemesterDecorator($grad_date) {
+            $date_grad_date = strtotime($grad_date);
+            $decorator = "";
+            if(intval(date("m", $date_grad_date))>=1 && intval(date("m", $date_grad_date))<=5) { // SPRING!
+                $decorator = "";
+            } else if(intval(date("m", $date_grad_date))>5 && intval(date("m", $date_grad_date))<=8) { // SUMMER!
+                $decorator = "*";
+            } else if(intval(date("m", $date_grad_date))>8 && intval(date("m", $date_grad_date))<=12) { // FALL!
+                $decorator = "**";
+            }
+            return $decorator;
+    }
  
 }
