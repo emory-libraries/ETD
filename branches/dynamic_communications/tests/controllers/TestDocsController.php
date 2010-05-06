b5307e8f4e1a71da850e68457e6bf5b04fc864ef
sconna2 <sconna2@affb5729-703e-0410-bbc3-960ba87530bd>
2010-05-06 15:45:10 +0000

    Added tests for new content rss feed code in DocsController.

--- branches/dynamic_communications/tests/controllers/TestDocsController.php
+++ branches/dynamic_communications/tests/controllers/TestDocsController.php
@@ -11,7 +11,23 @@ require_once('../ControllerTestCase.php');
 require_once('controllers/DocsController.php');
       
 class DocsControllerTest extends ControllerTestCase {
+  
+  // Test Data
   private $docs_feed_url = "https://digital.library.emory.edu/taxonomy/term/26/all/feed";
+  private $topics = array( 
+    array("subject" => 'about', "subject_title" => 'About', 
+          "title" => 'About Us'),
+    array("subject" => 'faq', "subject_title" => 'Frequently', 
+          "title" => 'Frequently Asked Questions'),
+    array("subject" => 'instructions', "subject_title" => 'Instructions', 
+          "title" => 'Instructions for Electronic Submission of your Thesis/Dissertation'),
+    array("subject" => 'ip', "subject_title" => 'Intellectual', 
+          "title" => 'Notes on Intellectual Property'),
+    array("subject" => 'policies', "subject_title" => 'Policies', 
+          "title" => 'Policies and Procedures'),
+    array("subject" => 'boundcopies', "subject_title" => 'Bound', 
+          "title" => 'Obtaining Bound Copies of Your Theses and Dissertations'),                                                                          
+  );  
   
   function setUp() {
     $this->response = $this->makeResponse();
@@ -28,32 +44,52 @@ class DocsControllerTest extends ControllerTestCase {
     $this->assertFalse($DocsController->view->printable);
   }
   
-  function test_getTopicSubjectAbout() {
+  function test_getTitleSubject() {
+    $index = new DocsControllerForTest($this->request,$this->response);    
+    foreach ($this->topics as $topic) {
+      $subject_title = $index->getTitleSubject($topic["subject"]);
+      $this->assertEqual($subject_title, $topic["subject_title"]);
+    }      
+  }
+  
+  function test_isSubjectTextInTitle() {
+    $index = new DocsControllerForTest($this->request,$this->response);    
+    foreach ($this->topics as $topic) {
+      $found = $index->isSubjectTextInTitle($topic["subject_title"], $topic["title"]);
+      $this->assertTrue($found);
+    }      
+  }
+      
+  function test_getTopicSubject() {
     $index = new DocsControllerForTest($this->request,$this->response);
-    $subject = "about";
-    $subject_title = $index->getTitleSubject($subject);
+    
+    foreach ($this->topics as $topic) {
 
-    $rss_function = "Data pulled from function."; 
-    $rss_feed_section = "Data pulled from rss feed."; 
+      $rss_function = "Data pulled from function."; 
+      $rss_feed_section = "Data pulled from rss feed."; 
 
-    // Get the title from the function
-    try {
-      $rss_function = $index->getTopicSubject($subject, $this->rss_data, $this->docs_feed_url);
-    } catch (Exception $e) {
-      $ex = $e;   // store for testing outside the try/catch
-    }
-
-    foreach ($this->rss_data as $part) {
-      // Check if the title string in the feed contains the topic
-      if (!(strpos($part->title(),$subject_title)===false)) {
-        $rss_feed_section = $docSubject = "<h3>" . $part->title() . "</h3>" . $part->description();
+      // Get the section from the rss feed via getTopicSubject function
+      try {
+        $rss_function = $index->getTopicSubject($topic["subject"], $this->rss_data, $this->docs_feed_url);
+      } catch (Exception $e) {
+        $ex = $e; 
       }
-    }       
-    $this->assertEqual(strlen($rss_feed_section), strlen($rss_function));
-    $this->assertEqual($rss_feed_section, $rss_function);
-    unset($ex); 
-  }  
 
+      // Get the section from the rss feed locally
+      foreach ($this->rss_data as $part) {
+        // Check if the title string in the feed contains the topic
+        if (!(strpos($part->title(),$topic["subject_title"])===false)) {
+          // If we do have a match on the title, then extract this section.
+          $rss_feed_section = $docSubject = "<h3>" . $part->title() . "</h3>" . $part->description();
+        }
+      }  
+      
+      // Compare the results from the function to those obtained locally.     
+      $this->assertEqual(strlen($rss_feed_section), strlen($rss_function));
+      $this->assertEqual($rss_feed_section, $rss_function);
+      unset($ex); 
+    }
+  }  
 }
 
 class DocsControllerForTest extends DocsController {
