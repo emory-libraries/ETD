60fc748d3e213426e13f55ea0fdf46cc024ac291
athom09 <athom09@affb5729-703e-0410-bbc3-960ba87530bd>
2010-05-13 19:53:05 +0000

    tractrac #689 - removed datastreams from the search path - changed /datastreams/etd_mods.xml to datastreams/etd_mods.xml - added function getFedoraTemplate to override base class function

--- branches/userRefactor/tests/xslt/etdmsTest.php
+++ branches/userRefactor/tests/xslt/etdmsTest.php
@@ -51,7 +51,7 @@ class TestEtdmsXslt extends UnitTestCase {
 
     function test_StillEmbargoed() {
       $xml = new DOMDocument();
-      $xml->loadXML(file_get_contents("models/datastremas/mods.xml", FILE_USE_INCLUDE_PATH));
+      $xml->loadXML(file_get_contents("models/datastremas/etd_mods.xml", FILE_USE_INCLUDE_PATH));
       $mods = new etd_mods($xml);
 
       $mods->embargo = "1 year";
@@ -69,7 +69,7 @@ class TestEtdmsXslt extends UnitTestCase {
 
     function test_oldEmbargoDateFormat() {
       $xml = new DOMDocument();
-      $xml->loadXML(file_get_contents("models/datastremas/mods.xml", FILE_USE_INCLUDE_PATH));
+      $xml->loadXML(file_get_contents("models/datastremas/etd_mods.xml", FILE_USE_INCLUDE_PATH));
       $mods = new etd_mods($xml);
 
       $mods->embargo = "1 year";
