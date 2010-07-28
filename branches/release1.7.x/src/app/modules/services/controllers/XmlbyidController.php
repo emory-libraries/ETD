2e8d3d64a55eab8c8010476834e8bdd8a48915fe
athom09 <athom09@affb5729-703e-0410-bbc3-960ba87530bd>
2010-07-28 00:53:46 +0000

    trac #725 - added authorized function to validate incoming request.  also assed alternate_ports to config

--- branches/release1.7.x/src/app/modules/services/controllers/XmlbyidController.php
+++ branches/release1.7.x/src/app/modules/services/controllers/XmlbyidController.php
@@ -24,24 +24,7 @@ class Services_XmlbyidController extends Zend_Controller_Action {
 
     // load Fedora configuration
     $fedora_cfg = Zend_Registry::get('fedora-config');
-    $allowed_ips = array(gethostbyname($fedora_cfg->server));
-    
-    // if included in configuration, allow these alternate hostnames
-    if (isset($fedora_cfg->alternate_hosts)) {
-      $alternate_hostnames = $fedora_cfg->alternate_hosts->server->toArray();
-      foreach ($fedora_cfg->alternate_hosts->server as $ah)
-	$allowed_ips[] = gethostbyname($ah);
-    } else {
-      $alternate_hostnames = array();
-    }
-
-    // ONLY allow requests from Fedora, since this service may have access to restricted content
-    if (! in_array($this->_request->getServer("REMOTE_ADDR"), $allowed_ips)) {
-      $this->_response->setHttpResponseCode(403);	// Forbidden
-      // FIXME: should any text/message be displayed here?
-      return;
-    }
-    
+        
     $url =  $this->_getParam("url");  // fedora datastream url
     $url = urldecode($url);
     $id = $this->_getParam("id");   
@@ -51,29 +34,37 @@ class Services_XmlbyidController extends Zend_Controller_Action {
       if (preg_match("|(https?)://([a-z0-9.]+):([0-9]+)/[^/]+/get/([-a-zA-z:0-9]+)/([a-zA-Z0-9]+)/?(.*)?|", $url, $matches)) {
 	list($full_match, $protocol, $hostname, $port, $pid, $datastream, $datetime) = $matches;
 
+
 	// check url against the configured fedora instance before sending credentials
-	if ((($protocol == $fedora_cfg->protocol && $port == $fedora_cfg->port) ||
-	     (isset($fedora_cfg->nonssl_port) && $protocol == "http" && $port = $fedora_cfg->nonssl_port))
-	    && (gethostbyname($hostname) == gethostbyname($fedora_cfg->server)
-		|| in_array($hostname, $alternate_hostnames))) {
+	if (! $this->authorized($hostname, $port)) {
+
 
 	  // should match either main protocol & port or http & nonssl port (if configured)
 	  // hostnames should resolve to the same ip
 	  
-	  // everything is good - proceed
-	  
-	} else {	// url doesn't match fedora config
-	  throw new Exception("Not configured to access '$url'");
-	}
-      } else {	// regexp failed
-	throw new Exception("Could not parse datastream url '$url'");
-      }
+      $this->_response->setHttpResponseCode(403);
+      $this->_response->setBody("Not configured to access '$url'");
+
+      //throw new Exception("Not configured to access '$url'");
+      return;
+} 
+      
 
       // create a new FedoraConnection using fedora config, but with maintenance account credentials
       $fedora_opts = $fedora_cfg->toArray();
+//      print "<pre>";
+//      print "CFG BEFORE:";
+//      print_r($fedora_opts);
+//      print "<pre>";
       $fedora_opts["username"] = $fedora_cfg->maintenance_account->username;
       $fedora_opts["password"] = $fedora_cfg->maintenance_account->password;
+      //$fedora_opts["server"] = $hostname;
+      //$fedora_opts["port"] = "8280";
       $fedora = new FedoraConnection($fedora_opts);
+//      print "<pre>";
+//      print "CFG AFTER:";
+//      print_r($fedora_opts);
+//      print "<pre>";
       
       $xml = $fedora->getDatastream($pid, $datastream);
       
@@ -81,7 +72,7 @@ class Services_XmlbyidController extends Zend_Controller_Action {
       
       $dom = new DOMDocument();
       if (! $dom->loadXML($xml))
-	throw new Exception("Could not load content as xml");
+	  throw new Exception("Could not load content as xml");
       
       // NOTE: not using DOMDocument function getElementById because if looks for xml:id only
       $xpath = new DOMXpath($dom);
@@ -92,7 +83,9 @@ class Services_XmlbyidController extends Zend_Controller_Action {
 	$this->_helper->displayXml($dom->saveXml($nodelist->item(0)));
       else 
 	throw new Exception("id '$id' not found");
-      
+    } else {	// regexp failed
+	throw new Exception("Could not parse datastream url '$url'");
+      }
     } catch (Exception $e) {
       $this->_response->setHttpResponseCode(400);	// bad request
       $this->_helper->viewRenderer->setNoRender(true);
@@ -104,8 +97,59 @@ class Services_XmlbyidController extends Zend_Controller_Action {
 	  $this->_response->setBody("<p>Error: " . $e->getMessage() . "</p>");
       }
     }
+
   }
   
   public function aboutAction() {}
 
+
+  public function authorized($hostname, $port){
+      $cfg = Zend_Registry::get('fedora-config');
+      
+      $portOK=false;
+      $hostOK = false;
+
+      //hosts and ports that etd will accept a connection from.
+      //Inital values are the from the standard  config fields
+      $allowed_ips = array(gethostbyname($cfg->server));
+      $allowed_ports = array($cfg->port, $cfg->nonssl_port);
+
+      // if included in configuration, allow these alternate hostnames
+    if (isset($cfg->alternate_hosts)) {
+      if(is_object($cfg->alternate_hosts->server)){
+        foreach ($cfg->alternate_hosts->server->toArray() as $ah){
+         $allowed_ips[] = gethostbyname($ah);
+        }
+
+      }
+      else{
+          $allowed_ips[] = gethostbyname($cfg->alternate_hosts->server);
+      }
+      
+    }
+
+    // if included in configuration, allow these alternate ports
+    if (isset($cfg->alternate_ports)){
+      if(is_object($cfg->alternate_ports->port)){
+        foreach ($cfg->alternate_ports->port->toArray() as $ap)
+            $allowed_ports[] = $ap;
+      }
+      else
+      $allowed_ports[] = $cfg->alternate_ports->port;
+    }
+
+
+      //test for allowed port
+      if(in_array($port, $allowed_ports)){
+              $portOK = true;
+      }
+
+      //test for allowed hosts
+      if(in_array(gethostbyname($hostname), $allowed_ips) || in_array($hostname, $allowed_ips)){
+        $hostOK = true;
+      }
+
+      return ($hostOK && $portOK);
+      
+      }
 }
