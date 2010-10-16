43235d23e2fb2ff8ecbf7787a24c0d9f3f089341
sconna2 <sconna2@affb5729-703e-0410-bbc3-960ba87530bd>
2010-10-16 14:30:49 +0000

    update new pa branch with partnering agencies branch code.

---
+++ branches/pa/src/app/models/vocabularies.php
@@ -0,0 +1,58 @@
+<?
+/**
+ * @category Etd
+ * @package Etd_Models
+ * @subpackage Skos_Hierarchy
+ */
+
+require_once("skosCollection.php");
+
+/**
+ * TODO: consider consolidating this functionality 
+ * with foxmlPrograms.
+ */
+
+/**
+ * Foxml object with vocabulary SKOS datastream.
+ * Extending from foxml class in order to inherit existing
+ * functionality for interacting with Fedora.
+ * 
+ */
+class foxmlVocabularies extends foxmlSkosCollection {
+  public function __construct($id = "#vocabularies") {
+    // initialize with a pid specified in the config - complain if it is not available
+    if (! Zend_Registry::isRegistered("config")) {
+      throw new FoxmlException("Configuration not registered, cannot retrieve pid");
+    }
+    $config = Zend_Registry::get("config");
+    if (! isset($config->vocabularies_pid) || $config->vocabularies_pid == "") {
+      throw new FoxmlException("Configuration does not contain vocabularies pid, cannot initialize");
+    }
+    parent::__construct($config->vocabularies_pid);
+
+    // initializing SKOS datastream here in order to pass a collection id
+    $ds = "skos";
+    $dom = new DOMDocument();
+    $xml = $this->fedora->getDatastream($this->pid, $this->xmlconfig[$ds]['dsID']);
+    if ($xml) {
+      $dom->loadXML($xml);
+      $this->map[$ds] = new $this->xmlconfig[$ds]['class_name']($dom, $id);
+    }
+  }
+  protected function configure() {
+    parent::configure();
+    $this->xmlconfig["skos"]["class_name"] = "vocabularies";
+  }  
+
+}
+
+/**
+ * custom version of collectionHierarchy for partnering agencies
+ */
+class vocabularies extends collectionHierarchy  {
+
+  public function __construct($dom, $id = "#vocabularies") {
+    parent::__construct($dom, $id);
+  }
+
+}
