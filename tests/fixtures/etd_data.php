3774965628125f373365608ed1a8563d93c19aed
Sari Connard <sconna2@users.noreply.github.com>
2010-11-12 13:33:55 +0000

    For testing, the table etd_admins should be empty at startup, but in case it is not, truncate the table before loading the test data.

--- tests/fixtures/etd_data.php
+++ tests/fixtures/etd_data.php
@@ -22,6 +22,7 @@ class etd_test_data {
 
   public function loadAll() {
     // combine field names as array keys, data for values to generate format needed for db insert
+    $this->db->query("truncate table {$this->table}");    
     foreach ($this->data as $row) {
       $this->db->insert($this->table, array_combine($this->fields, $row));
     }
@@ -29,7 +30,7 @@ class etd_test_data {
   }
 
   public function cleanUp() {
-          $this->db->query("truncate table {$this->table}");
+    $this->db->query("truncate table {$this->table}");
   }
 }
 
