7695588160a2c7629cc63819cbd184abe43b2e0a
athom09 <athom09@affb5729-703e-0410-bbc3-960ba87530bd>
2010-11-16 17:26:07 +0000

    Creating bugfix release 1.8.4  to correct permission check for edit schools

---
+++ tags/v1.8.4/tests/fixtures/etd_data.php
@@ -0,0 +1,36 @@
+<?php
+
+class etd_test_data {
+  private $db;
+  public $table;
+  public $fields;
+  public $data;
+
+  public function __construct() {
+    $this->db = Zend_Registry::get("etd-db");
+    $this->table = "etd_admins";
+
+    $this->fields = array('netid', 'schoolid', 'programid');
+    
+    $this->data = array(
+    array('EPUSR', 'PUBH', 'RSPH-AEPI'),
+    array('GHUSR', 'PUBH', 'RSPH-IH'),
+    array('ROLL1', 'PUBH', 'RSPH-PS'),
+    array('ROLL1', 'PUBH', 'RSPH-MS'),
+);
+  }
+
+  public function loadAll() {
+    // combine field names as array keys, data for values to generate format needed for db insert
+    foreach ($this->data as $row) {
+      $this->db->insert($this->table, array_combine($this->fields, $row));
+    }
+
+  }
+
+  public function cleanUp() {
+          $this->db->query("truncate table {$this->table}");
+  }
+}
+
+?>
