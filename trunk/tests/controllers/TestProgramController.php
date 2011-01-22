0a62c79c917b118d53bb8c5800018040b0d39f50
sconna2 <sconna2@affb5729-703e-0410-bbc3-960ba87530bd>
2011-01-22 17:13:28 +0000

    (trac 764,772-6) auto create programs or vocabulary collection if it does not exist.  Add new vocabularies collection model.

--- trunk/tests/controllers/TestProgramController.php
+++ trunk/tests/controllers/TestProgramController.php
@@ -52,7 +52,7 @@ class ProgramControllerTest extends ControllerTestCase {
     $this->_realconfig = Zend_Registry::get('config');
 
     // stub config with test pid for programs_pid
-    $testconfig = new Zend_Config(array("programs_pid" => $this->pid));
+    $testconfig = new Zend_Config(array("programs_collection" => array("pid" => $this->pid)));
     
     // temporarily override config in with test configuration
     Zend_Registry::set('config', $testconfig);
@@ -95,8 +95,8 @@ class ProgramControllerTest extends ControllerTestCase {
 
     // edit one subsection
     $this->setUpGet(array('section' => 'religion', 'religion' => "Religion LABEL",
-			   "religion_members" => "american ethics hebrew asian music",
-			   "american" => "American Religion"));
+         "religion_members" => "american ethics hebrew asian music",
+         "american" => "American Religion"));
     $programController->saveAction();
     $messages = $programController->getHelper('FlashMessenger')->getMessages();
     $this->assertPattern("/saved changes.*religion/i", $messages[0]);
@@ -106,22 +106,22 @@ class ProgramControllerTest extends ControllerTestCase {
     // labels updated
     $this->assertEqual("Religion LABEL", $programObj->skos->label, "label updated");
     $this->assertEqual("American Religion", $programObj->skos->collection->american->label,
-		       "sub-label updated");
+           "sub-label updated");
     $this->assertTrue($programObj->skos->collection->hasMember("ethics"),
-		      "religion has member ethics after update");
+          "religion has member ethics after update");
     $this->assertTrue($programObj->skos->collection->hasMember("american"),
-		      "religion has member american after update");
+          "religion has member american after update");
     $this->assertTrue($programObj->skos->collection->hasMember("hebrew"),
-		      "religion has member hebrew after update");
+          "religion has member hebrew after update");
     $this->assertTrue($programObj->skos->collection->hasMember("asian"),
-		      "religion has member asian after update");
+          "religion has member asian after update");
     $this->assertTrue($programObj->skos->collection->hasMember("music"),
-		      "religion has member music after update");
+          "religion has member music after update");
     // former collection members should have been removed
     $this->assertFalse($programObj->skos->collection->hasMember("comparative"),
-		       "religion no longer has member comparative");
+           "religion no longer has member comparative");
     $this->assertFalse($programObj->skos->collection->hasMember("historical"),
-		       "religion no longer has member historical");
+           "religion no longer has member historical");
 
   }
 
@@ -162,7 +162,7 @@ class ProgramControllerForTest extends ProgramController {
   public function _redirect() {
     $this->redirectRan = true;
   }
-} 	
+}   
 
 runtest(new ProgramControllerTest());
 
