0bb7a99d5aec3f0e0668a02cb17e35d7740d8002
Sari Connard <sconna2@users.noreply.github.com>
2011-02-07 20:41:17 +0000

    (trac #862) remove identifier and subjet from etdfile DC configuration.

--- src/app/models/datastreams/etdfile_dc.php
+++ src/app/models/datastreams/etdfile_dc.php
@@ -28,11 +28,6 @@ class etdfile_dc extends etd_dc {
   protected function configure() {
     parent::configure();
 
-    // adjustments a few stock dc fields
-    // TODO: update etd code to use base class mappings of identifiers and subjects
-    $this->xmlconfig['identifier']['is_series'] = true;
-    $this->xmlconfig['subject']['is_series'] = true;   
-
     // a few special cases 
     $this->xmlconfig["ark"] = array("xpath" => "dc:identifier[contains(., 'ark')]");
     $this->xmlconfig["fedora_pid"] = array("xpath" => "dc:identifier[contains(., 'info:fedora')]");
