e149cf4e1f587a839b691d0cf15af0d963c64a98
sconna2 <sconna2@affb5729-703e-0410-bbc3-960ba87530bd>
2011-02-07 14:20:11 +0000

    (trac #862) added the custom unversioned DC extension for etd objects.

---
+++ branches/release_1.9.x/src/app/models/datastreams/etd_dc_unversioned.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once("fedora/models/dublin_core.php");
+
+/**
+ * EtdFile customization of Dublin Core XmlObject / Fedora Foxml datastream
+ * @category EtdFile
+ * @package Etd_Models
+ * @subpackage Etd
+ *
+ */
+class etd_dc_unversioned extends dublin_core {
+
+  // In order for the ETD to not be versioned, and workaround the fedora bug:
+  // https://jira.duraspace.org/browse/FCREPO-849
+  // the control_group MUST be set to Inline/XML_DATASTREAM for the DC datastream, 
+  // when versionable is set to false.
+  public $versionable = false;
+  
+}
