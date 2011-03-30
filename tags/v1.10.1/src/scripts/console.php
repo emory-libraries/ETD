1c79e81c93d527d0c12b5ea2fd76b6a055752774
athom09 <athom09@affb5729-703e-0410-bbc3-960ba87530bd>
2011-03-30 14:53:14 +0000

    Creating tag v1.10.1 for release - multi admin department fix

---
+++ tags/v1.10.1/src/scripts/console.php
@@ -0,0 +1,14 @@
+#!/usr/bin/env php 
+<?php
+/**
+ * 
+ * @category Etd
+ * @package Etd_Scripts
+ */
+// set up include path, fedora/db connections, load common models, etc.
+require_once("bootstrap.php");
+  
+// load interactive php shell
+require "php-shell-cmd.php";
+
+?>
