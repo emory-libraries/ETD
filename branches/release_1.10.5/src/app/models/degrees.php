da5faa167fe779f55d1ffd3e3452e27312c8e9a7
sconna2 <sconna2@affb5729-703e-0410-bbc3-960ba87530bd>
2011-06-08 15:32:02 +0000

    Create a branch to contain the current 1.10.4 bugfix release as 1.10.5  includes mads as local schema and url update to cs-xacml-schema-policy.

---
+++ branches/release_1.10.5/src/app/models/degrees.php
@@ -0,0 +1,37 @@
+<?
+/**
+ * minimal object interface to degree name & level configuration
+ * - used to set PQ degree codes for ProQuest submissions
+ * @category Etd
+ * @package Etd_Models
+ * @subpackage ProQuest_Submission
+ */
+
+class degrees {
+
+  private $dom;
+  private $xpath;
+  
+  public function __construct() {
+    $this->dom = new DOMDocument();
+    $this->dom->loadXML(file_get_contents("degrees.xml", FILE_USE_INCLUDE_PATH));
+
+    $this->xpath = new DOMXpath($this->dom);
+  }
+
+
+  public function codeByAbbreviation($name) {
+    $nodelist = $this->xpath->query("/degrees/level/degree[@name = '$name']");
+    if ($nodelist->length == 1) {	// we want one and only one match
+      $degree = $nodelist->item(0);
+      return $degree->getAttribute("pq_code");
+    } else {
+      // degree not found
+      trigger_error("Error! No degree code found for $name", E_USER_WARNING);
+    }
+  }
+
+  
+}
+
+?>
