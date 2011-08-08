14354b932148b6e957e16bee248a77edd11f6192
sconna2 <sconna2@affb5729-703e-0410-bbc3-960ba87530bd>
2011-08-08 12:11:31 +0000

    remove release_1.10.5 branch.

--- branches/release_1.10.5/tests/suites/console.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/php -q
-<?php
-
-// set up include path, fedora/db connections, load common models, etc.
-require_once("../bootstrap.php");
-  
-// load interactive php shell
-require "php-shell-cmd.php";
-?>
