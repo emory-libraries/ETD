c63c88c08da6f9a4daa2b490d7965054715cb9a2
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2012-07-12 16:22:45 +0000

    merge submit-without-initial-pdf feature branch r3422:3445 into trunk

--- src/public/js/jquery.local.js
+++ src/public/js/jquery.local.js
@@ -1,5 +1,68 @@
 var $j = jQuery.noConflict();  
-/* avoid conflict with prototype/scriptaculous while ETD has both*/
+/* avoid conflict with prototype/scriptaculous while ETD has both  */
+
+(function($j){
+    /**
+    jQuery plugin to conditionally display or hide dom content based on radio button
+    selected value.
+
+    Example usage:
+   
+        <input type="radio" name="foo" value="no" class="required-field"> No
+        <input type="radio" name="foo" value="yes" class="required-field"> Yes
+
+        <div id="foo_no">Display only when NO is selected</div>
+        <div id="foo_yes">Display only when YES is selected</div>
+
+        $('input[name=foo]').conditionalDisplay({
+            conditions: { yes: '#foo_yes', no: '#foo_no' }
+        });  
+
+        Recommended: leave conditional divs displayed by default and use javascript to 
+        hide them by triggering the `change` event:
+
+           $("input:radio").change();
+
+    */
+
+    var methods = {
+     init : function( options ) {
+        var settings = $j.extend({
+            conditions: {},
+            }, options);
+
+       return this.each(function(){
+         $j(this).data('conditionalDisplay', settings);
+         $j(this).bind('change', methods.update_display);
+       });
+
+     },
+    update_display : function() { 
+       var conditions = $j(this).data('conditionalDisplay').conditions;
+        // hide everything by default
+        for (var cond in conditions) {
+            $j(conditions[cond]).hide();
+        }
+        // show configured item matching current checked value, if any
+        var val = $j(this).filter(':checked').first().val();
+        if (conditions[val]) {
+            $j(conditions[val]).show();
+        }
+     }
+  };
+
+   $j.fn.conditionalDisplay = function( method ) {
+    
+    if ( methods[method] ) {
+      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.conditionalDisplay' );
+    }    
+}
+
+})(jQuery);
 
 $j(document).ready(function() {
 
