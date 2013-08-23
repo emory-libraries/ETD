ce84cbc5f54f131f5e07042d39af084993dbf44b
athom09 <athom09@affb5729-703e-0410-bbc3-960ba87530bd>
2013-08-23 16:30:44 +0000

    changed 'value' key in report fields array to 'field' so make it less confusing

--- branches/reporting/src/app/controllers/report_fields.php
+++ branches/reporting/src/app/controllers/report_fields.php
@@ -4,193 +4,193 @@
         global $all_report_fields;        
         $all_report_fields = array(
         array(
-          'value' => 'label', 
+          'field' => 'label', 
           'label' => 'Label', 
           'description' => 'Fedora object label (set from initial title)',
           'available' => false
         ),
         array(
-          'value' => 'ownerId', 
+          'field' => 'ownerId', 
           'label' => 'OwnerId', 
           'description' => 'netid of user who owns the object (author',
           'available' => false
         ),
         array(
-          'value' => 'state', 
+          'field' => 'state', 
           'label' => 'State', 
           'description' => 'Fedora object state indicating whether the objest is (A)ctive), (I)nactive), (D)eleted',
           'available' => false
         ),
         array(
-          'value' => 'author', 
+          'field' => 'author', 
           'label' => 'Author', 
           'description' => 'Full name of the ETD author as lastname, firstname',
           'available' => true
         ),
         array(
-          'value' => 'date_embargoedUntil', 
+          'field' => 'date_embargoedUntil', 
           'label' => 'DateEmbargoedUntil', 
           'description' => 'Date embargo expires in YYYYMMDD format; only available for published records',
           'available' => false
         ),
         array(
-          'value' => 'dateIssued', 
+          'field' => 'dateIssued', 
           'label' => 'DateIssued', 
           'description' => 'Date published in YYYYMMDD format; only available for published records',
           'available' => true
         ),
         array(
-          'value' => 'degree_level', 
+          'field' => 'degree_level', 
           'label' => 'DegreeLevel', 
           'description' => 'Level of the degree, e.g. bachelors, masters, doctoral',
           'available' => false
         ),
         array(
-          'value' => 'degree_name', 
+          'field' => 'degree_name', 
           'label' => 'DegreeName', 
           'description' => 'Actual degree received, e.g. PhD, MA, BA',
           'available' => true
         ),
         array(
-          'value' => 'document_type', 
+          'field' => 'document_type', 
           'label' => 'DocumentType', 
           'description' => 'Genre of the record, i.e. dissertation or thesis',
           'available' => true
         ),
         array(
-          'value' => 'registering_copyright', 
+          'field' => 'registering_copyright', 
           'label' => 'RegisteringCopyright', 
           'description' => 'yes/no flag indicating that the user is registering copyright',
           'available' => false
         ),
         array(
-          'value' => 'embargo_duration', 
+          'field' => 'embargo_duration', 
           'label' => 'EmbargoDuration', 
           'description' => 'Duration of embargo',
           'available' => true
         ),
         array(
-          'value' => 'embargo_notice', 
+          'field' => 'embargo_notice', 
           'label' => 'EmbargoNotice', 
           'description' => 'If set, indicates when 60 day embargo expiration was sent (e.g., "sent 2013-04-04")',
           'available' => false
         ),
         array(
-          'value' => 'embargo_requested', 
+          'field' => 'embargo_requested', 
           'label' => 'EmbargoRequested', 
           'description' => 'yes/no flag indicating whether the user requested an embargo',
           'available' => false
         ),
         array(
-          'value' => 'language', 
+          'field' => 'language', 
           'label' => 'Language', 
           'description' => 'Language in which the thesis is written',
           'available' => true
         ),
         array(
-          'value' => 'num_pages', 
+          'field' => 'num_pages', 
           'label' => 'NumPages', 
           'description' => 'Number of pages in the submitted PDF',
           'available' => false
         ),
         array(
-          'value' => 'program', 
+          'field' => 'program', 
           'label' => 'Program', 
           'description' => 'Name of program ETD is associated',
           'available' => true
         ),
         array(
-          'value' => 'program_id', 
+          'field' => 'program_id', 
           'label' => 'ProgramId', 
           'description' => 'ETD code for the associated program',
           'available' => true
         ),
         array(
-          'value' => 'subfield', 
+          'field' => 'subfield', 
           'label' => 'Subfield', 
           'description' => 'Name of sub-divison of program ETD is associated',
           'available' => true
         ),
         array(
-          'value' => 'subfield_id', 
+          'field' => 'subfield_id', 
           'label' => 'SubfieldId', 
           'description' => 'Code of sub-divison of program ETD is associated',
           'available' => true
         ),
         array(
-          'value' => 'subject', 
+          'field' => 'subject', 
           'label' => 'Subject', 
           'description' => 'Text list of subjects / research fields',
           'available' => true
         ),
         array(
-          'value' => 'year', 
+          'field' => 'year', 
           'label' => 'Year', 
           'description' => 'Year the record was published (only available for records that have been published)',
           'available' => true
         ),
         array(
-          'value' => 'abstract', 
+          'field' => 'abstract', 
           'label' => 'Abstract', 
           'description' => 'Text of the abstract, without any formatting',
           'available' => false
         ),
         array(
-          'value' => 'tableOfContents', 
+          'field' => 'tableOfContents', 
           'label' => 'TableOfContents', 
           'description' => 'Text of the table of contents, without any formatting',
           'available' => false
         ),
         array(
-          'value' => 'title', 
+          'field' => 'title', 
           'label' => 'Title', 
           'description' => 'Title of the record without any formatting',
           'available' => true
         ),
         array(
-          'value' => 'advisor', 
+          'field' => 'advisor', 
           'label' => 'Advisor', 
           'description' => 'List of full names for all committee chairs/advisors in lastname, first format',
           'available' => true
         ),
         array(
-          'value' => 'advisor_id', 
+          'field' => 'advisor_id', 
           'label' => 'AdvisorId', 
           'description' => 'List of netids for committee chair/advisor (Emory faculty only)',
           'available' => false
         ),
         array(
-          'value' => 'committee', 
+          'field' => 'committee', 
           'label' => 'Committee', 
           'description' => 'List of full names for all committee members in lastname, first format',
           'available' => true
         ),
         array(
-          'value' => 'committee_id', 
+          'field' => 'committee_id', 
           'label' => 'CommitteeId', 
           'description' => 'List of netids for committee members on the record (Emory faculty only)',
           'available' => false
         ),
         array(
-          'value' => 'keyword', 
+          'field' => 'keyword', 
           'label' => 'Keyword', 
           'description' => 'Text keyword terms entered by author',
           'available' => true
         ),
         array(
-          'value' => 'status', 
+          'field' => 'status', 
           'label' => 'Status', 
           'description' => 'Status of the record within the ETD system (draft, submitted, approved, reviewed, published, inactive)',
           'available' => true
         ),
         array(
-          'value' => 'partneringagencies', 
+          'field' => 'partneringagencies', 
           'label' => 'PartneringAgencies', 
           'description' => 'Text names of Rollins partnering agencies',
           'available' => true
         ),
         array(
-          'value' => 'collection', 
+          'field' => 'collection', 
           'label' => 'Collection', 
           'description' => 'Fedora identifier for the collection this ETD belongs to; use to associated ETD records with schools (i.e., Graduate School, Theology, Rollins, etc)',
           'available' => true
