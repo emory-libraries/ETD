0913d03068d2e817eee630dd0d5516b832ff0480
hkglove <hkglove@affb5729-703e-0410-bbc3-960ba87530bd>
2015-06-24 14:01:43 +0000

    Updated migration for new nursing program

--- branches/release_1.16/src/scripts/migrations/021-add-nursing-program.php
+++ branches/release_1.16/src/scripts/migrations/021-add-nursing-program.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php -q
 <?php
 /**
- * Update ETDs program listings to a new school, department or program.
+ * Update ETDs program listings to add School of Nursing.
  *
  * @category Etd
  * @package Etd_Scripts
@@ -18,7 +18,7 @@ $opts = new Zend_Console_Getopt($common_getopts);
 // extended usage information - based on option list above, but with explanation/examples
 $scriptname = basename($_SERVER{"SCRIPT_NAME"});
 $usage = $opts->getUsageMessage() . "
- $scriptname updates program list with new schools, departments and programs
+ $scriptname updates program list to add Neil Hodgson Woodruff School of Nursing with its subdivisions
 ";
 
 try {
@@ -30,114 +30,101 @@ try {
 // output logging - common setup function in bootstrap
 $logger = setup_logging($opts->verbose);
 
-// Get the pid from the config.xml file
-if (! Zend_Registry::isRegistered("config")) {
-  throw new FoxmlException("Configuration not registered, cannot retrieve pid");
-}
-$config = Zend_Registry::get("config");
-if (! isset($config->programs_collection->pid) || $config->programs_collection->pid == "") {
-  throw new FoxmlException("Configuration does not contain vocabulary pid, cannot initialize");
-}
-$pid = $config->programs_collection->pid;
 
-// programs
-$program_data = array(
-  // NURS - Neil Hodgson Woodruff School of Nursing
-  array("id"=>'nursing', "label"=>'Neil Hodgson Woodruff School of Nursing'),
-  array("level01"=>'nursing', "id"=>'nhwson-dnp', "label"=>'Doctor of Nursing Practice'),
+// Top level for School of Nursing school
+$nhwson_school = array("id" => "#nhwson",
+     "label" => "Neil Hodgson Woodruff School of Nursing");
+
+/**
+ * WARNING: There cannot be any duplicate id values.
+ */
+
+//departments / sub sections for School of Nursing
+$nhwson_departments= array(
+
 );
 
-  $programs = null;
-  $programs  = new foxmlPrograms(); // access existing or create new object.
 
-  if (!$programs) {
-    $logger->err("Failed to create/access fedora object [$pid]");
-    return;
-  }
-  $skos = $programs->skos;
+// programs for School of Nursing
+// department code does not have # because it is only referenced at this point and is referenced without the #
+$nhwson_programs = array(
+array('id' => '#nhwson-dnp', 'label' => 'Doctor of Nursing Practice', "identifiers" => array('NRHSLD','NRPOP')),
+);
 
-  foreach ($program_data as $pgm) {
 
-    if ($skos->findLabelbyId('#'.$pgm["id"]) == null) {
-      $logger->info("Adding collection " . '#' . $pgm["id"]);
+$programs = new foxmlPrograms();
+$skos = $programs->skos;
 
-      // Add the collection
-      if (isset($pgm["identifiers"])) {
-        /**
-         * @todo remove the dc identifiers not in this list or any duplicates.
-         */
-        $skos->addCollection('#'.$pgm["id"], $pgm["label"], $pgm["identifiers"]);
-      }
-      else {
-        $skos->addCollection('#'.$pgm["id"], $pgm["label"]);
-      }
-    }
+// add School of Nursing top-level item
+// - check if already present (don't add more than once)
+if ($skos->findLabelbyId($nhwson_school["id"]) == null) {
+  $logger->info("Adding School of Nursing");
+  // add School of Nursing as another top-level item
+  $skos->addCollection($nhwson_school["id"], $nhwson_school["label"]);
+  $skos->collection->addMember($nhwson_school["id"]);
+} else {
+  $logger->info("Not adding School of Nursing - already present in programs list");
+  // set/update label, just to make sure it is set properly
+  $skos->nhwson->label = $nhwson_school["label"];
+}
 
-    // Set the collection as a member of another collection
-    if (isset($pgm["level01"]) && isset($pgm["level02"]) && isset($pgm["level03"])) {
-      if (! $skos->{$pgm["level01"]}->{$pgm["level02"]}->{$pgm["level03"]}->collection->hasMember($pgm["id"])) {
-        $logger->info("Adding program {$pgm["id"]} to {$pgm["level01"]}->{$pgm["level02"]}->{$pgm["level03"]}");
-        $skos->{$pgm["level01"]}->{$pgm["level02"]}->{$pgm["level03"]}->collection->addMember('#'.$pgm["id"]);
-      }
-      if ($skos->{$pgm["level01"]}->{$pgm["level02"]}->{$pgm["level03"]}->{$pgm["id"]}->label != preg_replace("/&amp;/", "&", $pgm["label"])) {
-        $cur_label = $skos->{$pgm["level01"]}->{$pgm["level02"]}->{$pgm["level03"]}->{$pgm["id"]}->label;
-        $logger->info("Set label for [{$pgm["id"]}] old=[$cur_label]  new=[{$pgm["label"]}]\n");
-        $skos->{$pgm["level01"]}->{$pgm["level02"]}->{$pgm["level03"]}->{$pgm["id"]}->label = $pgm["label"];
-      }
-    }
-    elseif (isset($pgm["level01"]) && isset($pgm["level02"])) {
-      if (! $skos->{$pgm["level01"]}->{$pgm["level02"]}->collection->hasMember($pgm["id"])) {
-        $logger->info("Adding program {$pgm["id"]} to {$pgm["level01"]}->{$pgm["level02"]}");
-        $skos->{$pgm["level01"]}->{$pgm["level02"]}->collection->addMember('#'.$pgm["id"]);
-      }
-      if ($skos->{$pgm["level01"]}->{$pgm["level02"]}->{$pgm["id"]}->label != preg_replace("/&amp;/", "&", $pgm["label"])) {
-        $cur_label = $skos->{$pgm["level01"]}->{$pgm["level02"]}->{$pgm["id"]}->label;
-        $logger->info("Set label for [{$pgm["id"]}] old=[$cur_label]  new=[{$pgm["label"]}]\n");
-        $skos->{$pgm["level01"]}->{$pgm["level02"]}->{$pgm["id"]}->label = $pgm["label"];
-      }
-    }
-    elseif (isset($pgm["level01"])) {
-      if (! $skos->{$pgm["level01"]}->collection->hasMember($pgm["id"])) {
-        $logger->info("Adding program {$pgm["id"]} to {$pgm["level01"]}");
-        $skos->{$pgm["level01"]}->collection->addMember('#'.$pgm["id"]);
-      }
-      if ($skos->{$pgm["level01"]}->{$pgm["id"]}->label != preg_replace("/&amp;/", "&", $pgm["label"])) {
-        $cur_label = $skos->{$pgm["level01"]}->{$pgm["id"]}->label;
-        $logger->info("Set label for [{$pgm["id"]}] old=[$cur_label]  new=[{$pgm["label"]}]\n");
-        $skos->{$pgm["level01"]}->{$pgm["id"]}->label = $pgm["label"];
-      }
-    }
-    else {
-      if (! $skos->collection->hasMember($pgm["id"])) {
-        $logger->info("Adding program {$pgm["id"]} to programs collection.");
-        $skos->collection->addMember('#'.$pgm["id"]);
-      }
-      if ($skos->{$pgm["id"]}->label != preg_replace("/&amp;/", "&", $pgm["label"])) {
-        $cur_label = $skos->{$pgm["id"]}->label;
-        $logger->info("Set label for [{$pgm["id"]}] old=[$cur_label]  new=[{$pgm["label"]}]\n");
-        $skos->{$pgm["id"]}->label = $pgm["label"];
-      }
-    }
-  }
+$nhwson = $programs->skos->nhwson; //nhwson section of hierarchy
 
-  // if record has changed, save to Fedora; or create fedora object when not found.
-  if ($skos->hasChanged()){
-    if (!$opts->noact) {
-      try {
-        $result = $programs->save("updating program list to include new data.");
-      }
-      catch (FedoraObjectNotFound $e) {
-        $result = $programs->ingest("ingesting program list to include new data.");
-      }
-      if ($result) {
-        $logger->info("Successfully updated programs ($pid) at $result");
-      } else {
-        $logger->err("Error updating programs $pid)");
-      }
-    } else {  // no-act mode, simulate saving
-      $logger->info("Updating programs $pid (simulated)");
-    }
-
+ //add School of Nursing departments
+foreach ($nhwson_departments as $id => $label) {
+  // create collection item if not already present
+  if ($skos->findLabelbyId($id) == null) {
+    $skos->addCollection($id, $label);
+    $logger->info("Adding School of Nursing department $label ($id)");
   } else {
-    $logger->info("Program listing is unchanged, not saving.");
+    $logger->info("School of Nursing department $label ($id) is already present");
   }
+  // add to School of Nursing collection if not already included
+  if (! $skos->nhwson->collection->hasMember(preg_replace("/^#/", "", $id))) {
+    $logger->info("School of Nursing does not yet include $id, adding as member");
+    $skos->nhwson->collection->addMember($id);
+  }
+}
+
+
+//add School of Nursing programs to departments
+foreach ($nhwson_programs as $program) {
+  // create collection item if not already present
+  if ($skos->findLabelbyId($program['id']) == null) {
+    $skos->addCollection($program['id'], $program['label'], $program['identifiers']);
+    $logger->info("Adding School of Nursing program {$program['id']}, ({$program['label']})");
+  } else {
+    $logger->info("School of Nursing program {$program['id']}, ({$program['label']}) is already present");
+  }
+  // add to School of Nursing collection if not already included
+
+  //if department is nhwson add directly under nhwson else add under correct departemnt
+
+      if (! $skos->nhwson->{$program['department']}->collection->hasMember(preg_replace("/^#/", "", $program['id']))) {
+        $logger->info("nhwson->{$program['department']} does not yet include program {$program['id']}, adding as member");
+        $skos->nhwson->{$program['department']}->collection->addMember($program['id']);
+      }
+      else{
+          $logger->info("nhwson->{$program['department']} {$program['id']}, ({$program['label']}) is already present");
+      }
+
+
+}
+
+// if record has changed, save to Fedora
+if ($skos->hasChanged()){
+  if (!$opts->noact) {
+    $result = $programs->save("updating program list to include School of Nursing");
+    if ($result) {
+      $logger->info("Successfully updated progams (" . $programs->pid .
+        ") at $result");
+    } else {
+      $logger->err("Error updating programs (" . $programs->pid . ")");
+    }
+  } else {  // no-act mode, simulate saving
+    $logger->info("Updating programs " .  $programs->pid . " (simulated)");
+  }
+
+} else {
+  $logger->info("Program listing is unchanged, not saving.");
+}
