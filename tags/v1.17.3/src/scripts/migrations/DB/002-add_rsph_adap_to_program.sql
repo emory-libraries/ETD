9d67b3c94f8f76c9e91a50ec7b2351debdecbfce
hkglove <hkglove@affb5729-703e-0410-bbc3-960ba87530bd>
2015-08-25 20:07:34 +0000

    Mimetype param to etdfile

---
+++ tags/v1.17.3/src/scripts/migrations/DB/002-add_rsph_adap_to_program.sql
@@ -0,0 +1,6 @@
+-- Add Melissa Krancer to  Applied Public Health Informatics (APHI) program
+
+
+
+INSERT INTO `etd_admins` (`netid`, `schoolid`, `programid`) VALUES('MKRANCE', 'PUBH', 'RSPH-APHI');
+
