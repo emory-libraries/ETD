3f744a3ddafa82eb6e98bfc7ee26d85c7b2e2788
jsvarne <jsvarne@affb5729-703e-0410-bbc3-960ba87530bd>
2016-04-12 17:47:02 +0000

    I don't know how to merge :(

--- branches/release_1.18/src/scripts/migrations/DB/002-add_rsph_adap_to_program.sql
+++
@@ -1,6 +0,0 @@
--- Add Melissa Krancer to  Applied Public Health Informatics (APHI) program
-
-
-
-INSERT INTO `etd_admins` (`netid`, `schoolid`, `programid`) VALUES('MKRANCE', 'PUBH', 'RSPH-APHI');
-
