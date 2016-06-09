e3b5bb6e878b4d870baa65cc909ce55689bf04e6
Jay Varner <jayvarner@gmail.com>
2016-06-09 18:01:53 +0000

    I have butterfingers.

--- src/app/models/etd.php
+++ src/app/models/etd.php
@@ -1338,7 +1338,7 @@ class etd extends foxml implements etdInterface {
   }
 
   public function doiURI() {
-    return 'http://doi.org/ ' + split(":", $this->mods->doi)[1];     // want the resolvable version of the doi
+    return 'http://doi.org/ ' + split(":", $this->mods->doi)[1];    // want the resolvable version of the doi
   }
 
   // direct access to metadata disseminations for efficiency
